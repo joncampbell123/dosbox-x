4fdcba96c9e75ac98cc1555da45e3d029d12ca19
Jonathan Campbell <jonathan@castus.tv>
2013-10-20 00:59:21 -0700

    remove printer emulation

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -117,10 +117,6 @@ void INNOVA_Init(Section*);
 void SERIAL_Init(Section*); 
 void DONGLE_Init(Section*);
 
-#if C_PRINTER
-void PRINTER_Init(Section*);
-#endif
-
 #if C_IPX
 void IPX_Init(Section*);
 #endif
@@ -1089,45 +1085,6 @@ void DOSBOX_Init(void) {
 	Pstring->Set_values(serials);
 	Pstring = Pmulti_remain->GetSection()->Add_string("parameters",Property::Changeable::WhenIdle,"");
 	Pmulti_remain->Set_help("see serial1");
-
-#if C_PRINTER
-	secprop=control->AddSection_prop("printer",&PRINTER_Init); 
-	Pbool = secprop->Add_bool("printer",Property::Changeable::WhenIdle,true);
-	Pbool->Set_help("Enable printer emulation.");
-	//secprop->Add_string("fontpath","%%windir%%\\fonts");
-	Pint = secprop->Add_int("dpi",Property::Changeable::WhenIdle,360); 
-	Pint->Set_help("Resolution of printer (default 360).");
-	Pint = secprop->Add_int("width",Property::Changeable::WhenIdle,85);
-	Pint->Set_help("Width of paper in 1/10 inch (default 85 = 8.5'').");
-	Pint = secprop->Add_int("height",Property::Changeable::WhenIdle,110);
-	Pint->Set_help("Height of paper in 1/10 inch (default 110 = 11.0'').");
-#ifdef C_LIBPNG
-	Pstring = secprop->Add_string("printoutput",Property::Changeable::WhenIdle, "png");
-#else
-	Pstring = secprop->Add_string("printoutput",Property::Changeable::WhenIdle, "ps");
-#endif
-	Pstring->Set_help("Output method for finished pages: \n"
-#ifdef C_LIBPNG
-		"  png     : Creates PNG images (default)\n"
-#endif
-		"  ps      : Creates Postscript\n"
-		"  bmp     : Creates BMP images (very huge files, not recommend)\n"
-#if defined (WIN32)
-		"  printer : Send to an actual printer (Print dialog will appear)"
-#endif
-		);
-
-	Pbool = secprop->Add_bool("multipage",Property::Changeable::WhenIdle, false);
-	Pbool->Set_help("Adds all pages to one Postscript file or printer job until CTRL-F2 is pressed.");
-
-	Pstring = secprop->Add_string("docpath",Property::Changeable::WhenIdle,".");
-	Pstring->Set_help("The path where the output files are stored.");
-
-	Pint = secprop->Add_int("timeout",Property::Changeable::WhenIdle,0);
-	Pint->Set_help("(in milliseconds) if nonzero: the time the page will\n"
-					"be ejected automatically after when no more data\n"
-					"arrives at the printer.");
-#endif
 
 	// parallel ports
 	secprop=control->AddSection_prop("parallel",&PARALLEL_Init,true);
--- src/hardware/parport/parport.cpp
+++ src/hardware/parport/parport.cpp
@@ -264,10 +264,6 @@ public:
 	
 	PARPORTS (Section * configuration):Module_base (configuration) {
 
-#if C_PRINTER
-		bool printer_used = false;
-#endif
-
 		// default ports & interrupts
 		Bit8u defaultirq[] = { 7, 5, 12};
 		Section_prop *section = static_cast <Section_prop*>(configuration);
@@ -302,22 +298,6 @@ public:
 				}
 			}
 			else 
-#if C_PRINTER
-			if(str=="printer") {
-				if(printer_used) {
-					
-				}; // only one parallel port with printer
-				CPrinterRedir* cprd = new CPrinterRedir(i,defaultirq[i],&cmd);
-				if(cprd->InstallationSuccessful) {
-					parallelPortObjects[i]=cprd;
-					printer_used=true;
-				} else {
-					LOG_MSG("Error: printer is not enabled.");
-					delete cprd;
-					parallelPortObjects[i]=0;
-				}
-			} else
-#endif				
 			if(str=="disabled") {
 				parallelPortObjects[i] = 0;
 			} else {
