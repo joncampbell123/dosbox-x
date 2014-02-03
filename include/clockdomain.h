d2e736a04039c1046397fa3b98ac75eeab30a1a1
Jonathan Campbell <jonathan@castus.tv>
2014-02-02 16:41:02 -0800

    more clock domain work. master/slave relationships.

--- include/clockdomain.h
+++ include/clockdomain.h
@@ -8,9 +8,11 @@
  * (C) 2014 Jonathan Campbell */
 
 #include <stdint.h>
+#include <assert.h>
 #include <math.h>
 
 #include <string>
+#include <vector>
 
 class ClockDomain {
 public:
@@ -18,15 +20,18 @@ public:
 		freq = 0;
 		freq_div = 1;
 		master = true;
+		master_clock = NULL;
 	}
 	ClockDomain(unsigned long long freq_new) {
 		freq = freq_new;
 		freq_div = 1;
 		master = true;
+		master_clock = NULL;
 	}
 	/* we allow non-integer frequencies as integer fractions.
 	 * example: 33.3333333...MHz as 100,000,000Hz / 3 */
 	ClockDomain(unsigned long long freq_new,unsigned long long div) {
+		master_clock = NULL;
 		freq = freq_new;
 		freq_div = div;
 		master = true;
@@ -92,15 +97,42 @@ public:
 		}
 	}
 	virtual void notify_advance(unsigned long long wc) { /* override me! */
+		for (size_t i=0;i < slaves.size();i++) slaves[i]->advance(wc);
 	}
 	virtual void notify_rebase() { /* override me! */
+		for (size_t i=0;i < slaves.size();i++) slaves[i]->notify_rebase();
+	}
+	void add_slave(ClockDomain *s) {
+		assert(s != NULL);
+		if (s->master_clock != NULL) {
+			fprintf(stderr,"Clock domain %s warning: attempting to add slave clock %s when said clock is already slave to someone else\n",name.c_str(),s->name.c_str());
+			return;
+		}
+
+		for (size_t i=0;i < slaves.size();i++) {
+			if (slaves[i] == s) {
+				fprintf(stderr,"Clock domain %s warning: attempted to add slave clock %s again\n",name.c_str(),s->name.c_str());
+				return;
+			}
+		}
+
+		s->master = false;
+		s->master_clock = this;
+		slaves.push_back(s);
+	}
+	void snapshot() {
+		counter_whole_snapshot = counter_whole;
+		for (size_t i=0;i < slaves.size();i++) slaves[i]->snapshot();
 	}
 public:
 	unsigned long long		freq,freq_div;
 	double				base_f;		/* base time if driven by floating point time */
 	unsigned long long		counter;	/* in units of freq */
 	unsigned long long		counter_whole;	/* in units of freq / freq_div */
+	unsigned long long		counter_whole_snapshot;
 	std::string			name;
 	bool				master;
+	ClockDomain*			master_clock;
+	std::vector<ClockDomain*>	slaves;
 };
 
