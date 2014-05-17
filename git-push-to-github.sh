378a73402d1dc8ddde3bd47b17163b546b4bd518
Jonathan Campbell <jonathan@castus.tv>
2014-05-17 10:08:21 -0700

    one more tweak. stop setting GitHub up as default push target.

--- git-push-to-github.sh
+++ git-push-to-github.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git remote add github https://joncampbell123@github.com/joncampbell123/dosbox-x.git # might fail
 git remote set-url github https://joncampbell123@github.com/joncampbell123/dosbox-x.git # might fail
-git push -u github master
+git push github master
 
