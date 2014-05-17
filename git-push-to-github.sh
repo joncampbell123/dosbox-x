90499976b6bb6e6e45cb648eaf9bfd187f3eeb98
Jonathan Campbell <jonathan@castus.tv>
2014-05-17 10:07:17 -0700

    update github push script

--- git-push-to-github.sh
+++ git-push-to-github.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-git remote add github https://joncampbell123@github.com/joncampbell123/dosbox-x.git
+git remote add github https://joncampbell123@github.com/joncampbell123/dosbox-x.git # might fail
+git remote set-url github https://joncampbell123@github.com/joncampbell123/dosbox-x.git # might fail
 git push -u github master
 
