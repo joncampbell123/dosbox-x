20044fbcfb4ccb34e550c8b45a662b0b7ab63bea
Jonathan Campbell <jonathan@castus.tv>
2014-05-17 21:46:45 -0700

    update comments to reflect that Windows 95 also does not support anything but 32-bit APM BIOS entry point.

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -1023,8 +1023,8 @@ void DOSBOX_Init(void) {
 
 	Pbool = secprop->Add_bool("apmbios allow 32-bit protected mode",Property::Changeable::WhenIdle,true);
 	Pbool->Set_help("Allow guest OS to connect from 32-bit protected mode.\n"
-			"If you want power management in Windows 98/ME (beyond using the APM to shutdown the computer) you MUST enable this option,\n"
-			"else Windows 98 will ignore the APM BIOS and complain that it's \"not working properly\".");
+			"If you want power management in Windows 95/98/ME (beyond using the APM to shutdown the computer) you MUST enable this option.\n"
+			"Windows 95/98/ME does not support the 16-bit real and protected mode APM BIOS entry points.");
 
 	Pbool = secprop->Add_bool("isapnpbios",Property::Changeable::WhenIdle,true);
 	Pbool->Set_help("Emulate ISA Plug & Play BIOS. Enable if using DOSBox to run a PnP aware DOS program or if booting Windows 9x.\n"
