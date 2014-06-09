d1a01611634439ec904f864f8e0cda0839844731
Jonathan Campbell <jonathan@castus.tv>
2014-06-08 21:05:54 -0700

    sound blaster bugfix: check the DMA channel variable is non-NULL before checking length to detect goldplay mode.

--- src/hardware/sblaster.cpp
+++ src/hardware/sblaster.cpp
@@ -802,7 +802,7 @@ static void DSP_DoDMATransfer(DMA_MODES mode,Bitu freq,bool stereo) {
 	 *    Triton - Crystal Dream (1992) [SB and SB Pro modes] ----FIXME: Yes, but apparently the demo fails to acknowledge the IRQ and sound stops
 	 *    The Jungly (1992) [SB and SB Pro modes]
 	 */
-	if (sb.goldplay && sb.dma_dac_srcrate > 0 && sb.dma.chan->basecnt < 2/* && sb.dma.chan->autoinit && mode == DSP_DMA_8*/) {
+	if (sb.goldplay && sb.dma_dac_srcrate > 0 && sb.dma.chan != NULL && sb.dma.chan->basecnt < 2/* && sb.dma.chan->autoinit && mode == DSP_DMA_8*/) {
 		sb.dma_dac_mode=1;
 		PIC_AddEvent(DMA_DAC_Event,1000.0 / sb.dma_dac_srcrate);
 	}
