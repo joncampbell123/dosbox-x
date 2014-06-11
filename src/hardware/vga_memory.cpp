a51dd2ec0a7ac1c7370a1ad74d30769eff504d39
Jonathan Campbell <jonathan@castus.tv>
2014-06-11 01:55:10 -0700

    vga map reset updated to follow "adapter rom is ram" setting.

--- src/hardware/vga_memory.cpp
+++ src/hardware/vga_memory.cpp
@@ -991,6 +991,9 @@ void VGA_ChangedBank(void) {
 }
 
 void MEM_ResetPageHandler_Unmapped(Bitu phys_page, Bitu pages);
+void MEM_ResetPageHandler_RAM(Bitu phys_page, Bitu pages);
+
+extern bool adapter_rom_is_ram;
 
 void VGA_SetupHandlers(void) {
 	vga.svga.bank_read_full = vga.svga.bank_read*vga.svga.bank_size;
@@ -1114,21 +1117,34 @@ void VGA_SetupHandlers(void) {
 		vgapages.base = VGA_PAGE_A0;
 		vgapages.mask = 0xffff;
 		MEM_SetPageHandler( VGA_PAGE_A0, 16, newHandler );
-		MEM_ResetPageHandler_Unmapped( VGA_PAGE_B0, 16);
+		if (adapter_rom_is_ram) MEM_ResetPageHandler_RAM( VGA_PAGE_B0, 16);
+		else MEM_ResetPageHandler_Unmapped( VGA_PAGE_B0, 16);
 		break;
 	case 2:
 		vgapages.base = VGA_PAGE_B0;
 		vgapages.mask = 0x7fff;
 		MEM_SetPageHandler( VGA_PAGE_B0, 8, newHandler );
-		MEM_ResetPageHandler_Unmapped( VGA_PAGE_A0, 16 );
-		MEM_ResetPageHandler_Unmapped( VGA_PAGE_B8, 8 );
+		if (adapter_rom_is_ram) {
+			MEM_ResetPageHandler_RAM( VGA_PAGE_A0, 16 );
+			MEM_ResetPageHandler_RAM( VGA_PAGE_B8, 8 );
+		}
+		else {
+			MEM_ResetPageHandler_Unmapped( VGA_PAGE_A0, 16 );
+			MEM_ResetPageHandler_Unmapped( VGA_PAGE_B8, 8 );
+		}
 		break;
 	case 3:
 		vgapages.base = VGA_PAGE_B8;
 		vgapages.mask = 0x7fff;
 		MEM_SetPageHandler( VGA_PAGE_B8, 8, newHandler );
-		MEM_ResetPageHandler_Unmapped( VGA_PAGE_A0, 16 );
-		MEM_ResetPageHandler_Unmapped( VGA_PAGE_B0, 8 );
+		if (adapter_rom_is_ram) {
+			MEM_ResetPageHandler_RAM( VGA_PAGE_A0, 16 );
+			MEM_ResetPageHandler_RAM( VGA_PAGE_B0, 8 );
+		}
+		else {
+			MEM_ResetPageHandler_Unmapped( VGA_PAGE_A0, 16 );
+			MEM_ResetPageHandler_Unmapped( VGA_PAGE_B0, 8 );
+		}
 		break;
 	}
 	if(svgaCard == SVGA_S3Trio && (vga.s3.ext_mem_ctrl & 0x10))
