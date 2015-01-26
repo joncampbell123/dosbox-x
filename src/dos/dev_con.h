171451580136a78d066be0d1ac743d5d3015872a
Jonathan Campbell <jonathan@castus.tv>
2015-01-25 21:23:16 -0800

    svn mainline r3880 (ripsaw):
    "Change the console device to use the DOS default attribute of 7 when
    ANSI is not active. Fixes glitched text in Space Pilot, and generally
    improves compatibility of console text output in graphics modes."
    
    Adapted for DOSBox-X by TheGreatCodeholio

--- src/dos/dev_con.h
+++ src/dos/dev_con.h
@@ -267,8 +267,8 @@ bool device_CON::Write(Bit8u * data,Bit16u * size) {
 			} else { 
 				/* Some sort of "hack" now that '\n' doesn't set col to 0 (int10_char.cpp old chessgame) */
 				if((data[count] == '\n') && (lastwrite != '\r')) Real_INT10_TeletypeOutputAttr('\r',ansi.attr,ansi.enabled);
- 				/* pass attribute only if ansi is enabled */
-				Real_INT10_TeletypeOutputAttr(data[count],ansi.attr,ansi.enabled);
+				/* use ansi attribute if ansi is enabled, otherwise use DOS default attribute*/
+				Real_INT10_TeletypeOutputAttr(data[count],ansi.enabled?ansi.attr:7,true);
 				lastwrite = data[count++];
 				continue;
 		}
