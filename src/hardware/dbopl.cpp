b49d89bc6f19085acbac84f4cf873b663338e720
Jonathan Campbell <jonathan@castus.tv>
2015-01-31 01:05:22 -0800

    dbopl.cpp fix compiler warnings (switch case)

--- src/hardware/dbopl.cpp
+++ src/hardware/dbopl.cpp
@@ -883,6 +883,8 @@ Channel* Channel::BlockTemplate( Chip* chip, Bit32u samples, Bit32s* output ) {
 			return (this + 2);
 		}
 		break;
+	default:
+		break;
 	}
 	//Init the operators with the the current vibrato and tremolo values
 	Op( 0 )->Prepare( chip );
@@ -947,6 +949,8 @@ Channel* Channel::BlockTemplate( Chip* chip, Bit32u samples, Bit32s* output ) {
 		case sm3AMAM:
 			output[ i * 2 + 0 ] += sample & maskLeft;
 			output[ i * 2 + 1 ] += sample & maskRight;
+			break;
+		default:
 			break;
 		}
 	}
