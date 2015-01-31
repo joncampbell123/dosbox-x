0b29393ffae1a143206f49303c83b3030bc4b0b3
Jonathan Campbell <jonathan@castus.tv>
2015-01-31 00:28:51 -0800

    silence compiler warnings about unhandled switch cases

--- src/ints/int10_pal.cpp
+++ src/ints/int10_pal.cpp
@@ -83,6 +83,8 @@ void INT10_SetSinglePaletteRegister(Bit8u reg,Bit8u val) {
 		}
 		IO_Write(VGAREG_ACTL_ADDRESS,32);		//Enable output and protect palette
 		break;
+	default:
+		break;
 	}
 }
 
@@ -99,6 +101,8 @@ void INT10_SetOverscanBorderColor(Bit8u val) {
 		IO_Write(VGAREG_ACTL_ADDRESS,0x11);
 		IO_Write(VGAREG_ACTL_WRITE_DATA,val);
 		IO_Write(VGAREG_ACTL_ADDRESS,32);		//Enable output and protect palette
+		break;
+	default:
 		break;
 	}
 }
@@ -127,6 +131,8 @@ void INT10_SetAllPaletteRegisters(PhysPt data) {
 		IO_Write(VGAREG_ACTL_ADDRESS,0x11);
 		IO_Write(VGAREG_ACTL_WRITE_DATA,mem_readb(data));
 		IO_Write(VGAREG_ACTL_ADDRESS,32);		//Enable output and protect palette
+		break;
+	default:
 		break;
 	}
 }
@@ -362,6 +368,8 @@ void INT10_SetBackgroundBorder(Bit8u val) {
 		INT10_SetSinglePaletteRegister( 2, val );
 		val+=2;
 		INT10_SetSinglePaletteRegister( 3, val );
+		break;
+	default:
 		break;
 	}
 }
