57aa02dab585adf400ed4b324d7e1f952494e00c
Jonathan Campbell <jonathan@castus.tv>
2015-01-31 06:38:51 -0800

    signed/unsigned compare fixes

--- src/hardware/serialport/misc_util.cpp
+++ src/hardware/serialport/misc_util.cpp
@@ -223,7 +223,7 @@ bool TCPClientSocket::Putchar(Bit8u data) {
 }
 
 bool TCPClientSocket::SendArray(Bit8u* data, Bitu bufsize) {
-	if(SDLNet_TCP_Send(mysock, data, bufsize)!=bufsize) {
+	if((Bitu)SDLNet_TCP_Send(mysock, data, bufsize) != bufsize) {
 		isopen=false;
 		return false;
 	}
@@ -237,7 +237,7 @@ bool TCPClientSocket::SendByteBuffered(Bit8u data) {
 		sendbuffer[sendbufferindex]=data;
 		sendbufferindex=0;
 		
-		if(SDLNet_TCP_Send(mysock, sendbuffer, sendbuffersize)!=sendbuffersize) {
+		if((Bitu)SDLNet_TCP_Send(mysock, sendbuffer, sendbuffersize) != sendbuffersize) {
 			isopen=false;
 			return false;
 		}
@@ -268,8 +268,8 @@ bool TCPClientSocket::SendArrayBuffered(Bit8u* data, Bitu bufsize) {
 */
 void TCPClientSocket::FlushBuffer() {
 	if(sendbufferindex) {
-		if(SDLNet_TCP_Send(mysock, sendbuffer,
-			sendbufferindex)!=sendbufferindex) {
+		if((Bitu)SDLNet_TCP_Send(mysock, sendbuffer,
+			sendbufferindex) != sendbufferindex) {
 			isopen=false;
 			return;
 		}
