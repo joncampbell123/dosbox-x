e2e3a76a9a99a6027da675a9a8062a55ccc0fe15
Jonathan Campbell <jonathan@castus.tv>
2015-06-08 00:01:33 -0700

    now take stringop changes and apply to full core

--- src/cpu/core_full/string.h
+++ src/cpu/core_full/string.h
@@ -26,6 +26,7 @@
 
 	count_left=0;
 
+	add_index=cpu.direction;
 	if (inst.prefix & PREFIX_SEG) si_base=inst.seg.base;
 	else si_base=SegBase(ds);
 	di_base=SegBase(es);
@@ -45,250 +46,308 @@
 	if (!(inst.prefix & PREFIX_REP)) {
 		count=1;
 	} else {
-		CPU_Cycles++;
 		/* we allow the user to cap our count as a way of making REP string operations interruptable (and at what granularity) */
-		if (cpu_rep_max > 0 && count > (unsigned int)cpu_rep_max && (inst.code.op<R_SCASB)) {
+		/* NTS: This condition is less important now that the loops themselves break when CPU_Cycles <= 0. when this code was
+		 *      initially implemented the string ops stubbornly counted through the bytes regardless of pending interrupts and
+		 *      it caused problems with code that needed fine timing i.e. demos that played music through the LPT DAC while
+		 *      using REP OUTSB to the VGA palette suffered from audio quality problems. at this phase of implementation the
+		 *      "interruptible string ops" parameter is now merely a testing parameter that can be used to verify this code
+		 *      breaks and restarts string ops correctly. */
+		if (cpu_rep_max > 0 && count > (unsigned int)cpu_rep_max) {
 			count_left+=count-(unsigned int)cpu_rep_max;
 			count=(unsigned int)cpu_rep_max;
-			LoadIP();		//capping the count means (E)CX will be nonzero afterwards, we need CPU to restart it again
-		}
-
-		/* Calculate amount of ops to do before cycles run out */
-		if ((count>(Bitu)CPU_Cycles) && (inst.code.op<R_SCASB)) {
-			count_left+=count-CPU_Cycles;
-			count=CPU_Cycles;
-			CPU_Cycles=0;
-			LoadIP();		//capping the count means (E)CX will be nonzero afterwards, we need CPU to restart it again
-		} else {
-			/* Won't interrupt scas and cmps instruction since they can interrupt themselves */
-			if ((count<=1) && (CPU_Cycles<=1)) CPU_Cycles--;
-			else if (inst.code.op<R_SCASB) CPU_Cycles-=count;
 		}
 	}
-	add_index=cpu.direction;
-	if (count) {
+
+	if (count != 0) {
 		try {
-		switch (inst.code.op) {
-	case R_OUTSB:
-		for (;count>0;count--) {
-			IO_WriteB(reg_dx,LoadMb(si_base+si_index));
-			si_index=(si_index+add_index) & add_mask;
-		}
-		break;
-	case R_OUTSW:
-		add_index<<=1;
-		for (;count>0;count--) {
-			IO_WriteW(reg_dx,LoadMw(si_base+si_index));
-			si_index=(si_index+add_index) & add_mask;
-		}
-		break;
-	case R_OUTSD:
-		add_index<<=2;
-		for (;count>0;count--) {
-			IO_WriteD(reg_dx,LoadMd(si_base+si_index));
-			si_index=(si_index+add_index) & add_mask;
-		}
-		break;
-	case R_INSB:
-		for (;count>0;count--) {
-			SaveMb(di_base+di_index,IO_ReadB(reg_dx));
-			di_index=(di_index+add_index) & add_mask;
-		}
-		break;
-	case R_INSW:
-		add_index<<=1;
-		for (;count>0;count--) {
-			SaveMw(di_base+di_index,IO_ReadW(reg_dx));
-			di_index=(di_index+add_index) & add_mask;
-		}
-		break;
-	case R_INSD:
-		add_index<<=2;
-		for (;count>0;count--) {
-			SaveMd(di_base+di_index,IO_ReadD(reg_dx));
-			di_index=(di_index+add_index) & add_mask;
-		}
-		break;
-	case R_STOSB:
-		for (;count>0;count--) {
-			SaveMb(di_base+di_index,reg_al);
-			di_index=(di_index+add_index) & add_mask;
-		}
-		break;
-	case R_STOSW:
-		add_index<<=1;
-		for (;count>0;count--) {
-			SaveMw(di_base+di_index,reg_ax);
-			di_index=(di_index+add_index) & add_mask;
-		}
-		break;
-	case R_STOSD:
-		add_index<<=2;
-		for (;count>0;count--) {
-			SaveMd(di_base+di_index,reg_eax);
-			di_index=(di_index+add_index) & add_mask;
-		}
-		break;
-	case R_MOVSB:
-		for (;count>0;count--) {
-			SaveMb(di_base+di_index,LoadMb(si_base+si_index));
-			di_index=(di_index+add_index) & add_mask;
-			si_index=(si_index+add_index) & add_mask;
-		}
-		break;
-	case R_MOVSW:
-		add_index<<=1;
-		for (;count>0;count--) {
-			SaveMw(di_base+di_index,LoadMw(si_base+si_index));
-			di_index=(di_index+add_index) & add_mask;
-			si_index=(si_index+add_index) & add_mask;
-		}
-		break;
-	case R_MOVSD:
-		add_index<<=2;
-		for (;count>0;count--) {
-			SaveMd(di_base+di_index,LoadMd(si_base+si_index));
-			di_index=(di_index+add_index) & add_mask;
-			si_index=(si_index+add_index) & add_mask;
-		}
-		break;
-	case R_LODSB:
-		for (;count>0;count--) {
-			reg_al=LoadMb(si_base+si_index);
-			si_index=(si_index+add_index) & add_mask;
-		}
-		break;
-	case R_LODSW:
-		add_index<<=1;
-		for (;count>0;count--) {
-			reg_ax=LoadMw(si_base+si_index);
-			si_index=(si_index+add_index) & add_mask;
-		}
-		break;
-	case R_LODSD:
-		add_index<<=2;
-		for (;count>0;count--) {
-			reg_eax=LoadMd(si_base+si_index);
-			si_index=(si_index+add_index) & add_mask;
-		}
-		break;
-	case R_SCASB:
-		{
-			Bit8u val2;
-			for (;count>0;) {
-				val2=LoadMb(di_base+di_index);
-				di_index=(di_index+add_index) & add_mask;
-				count--;CPU_Cycles--;
-				if ((reg_al==val2)!=inst.repz) break;
-			}
-			CMPB(reg_al,val2,LoadD,0);
-		}
-		break;
-	case R_SCASW:
-		{
-			add_index<<=1;Bit16u val2;
-			for (;count>0;) {
-				val2=LoadMw(di_base+di_index);
-				di_index=(di_index+add_index) & add_mask;
-				count--;CPU_Cycles--;
-				if ((reg_ax==val2)!=inst.repz) break;
-			}
-			CMPW(reg_ax,val2,LoadD,0);
-		}
-		break;
-	case R_SCASD:
-		{
-			add_index<<=2;Bit32u val2;
-			for (;count>0;) {
-				val2=LoadMd(di_base+di_index);
-				di_index=(di_index+add_index) & add_mask;
-				count--;CPU_Cycles--;
-				if ((reg_eax==val2)!=inst.repz) break;
-			}
-			CMPD(reg_eax,val2,LoadD,0);
-		}
-		break;
-	case R_CMPSB:
-		{
-			Bit8u val1,val2;
-			for (;count>0;) {
-				val1=LoadMb(si_base+si_index);
-				val2=LoadMb(di_base+di_index);
-				si_index=(si_index+add_index) & add_mask;
-				di_index=(di_index+add_index) & add_mask;
-				count--;CPU_Cycles--;
-				if ((val1==val2)!=inst.repz) break;
-			}
-			CMPB(val1,val2,LoadD,0);
-		}
-		break;
-	case R_CMPSW:
-		{
-			add_index<<=1;Bit16u val1,val2;
-			for (;count>0;) {
-				val1=LoadMw(si_base+si_index);
-				val2=LoadMw(di_base+di_index);
-				si_index=(si_index+add_index) & add_mask;
-				di_index=(di_index+add_index) & add_mask;
-				count--;CPU_Cycles--;
-				if ((val1==val2)!=inst.repz) break;
-			}
-			CMPW(val1,val2,LoadD,0);
-		}
-		break;
-	case R_CMPSD:
-		{
-			add_index<<=2;Bit32u val1,val2;
-			for (;count>0;) {
-				val1=LoadMd(si_base+si_index);
-				val2=LoadMd(di_base+di_index);
-				si_index=(si_index+add_index) & add_mask;
-				di_index=(di_index+add_index) & add_mask;
-				count--;CPU_Cycles--;
-				if ((val1==val2)!=inst.repz) break;
-			}
-			CMPD(val1,val2,LoadD,0);
-		}
-		break;
-	default:
-		LOG(LOG_CPU,LOG_ERROR)("Unhandled string op %d",inst.code.op);
-	}
-	/* Clean up after certain amount of instructions */
-	reg_esi&=(~add_mask);
-	reg_esi|=(si_index & add_mask);
-	reg_edi&=(~add_mask);
-	reg_edi|=(di_index & add_mask);
-	if (inst.prefix & PREFIX_REP) {
-		count+=count_left;
-		reg_ecx&=(~add_mask);
-		reg_ecx|=(count & add_mask);
-	}
-	}
-	catch (GuestPageFaultException &pf) {
-		LOG_MSG("Strip op #%d interrupted si_index=%x di_index=%x count=%x+%x=%x esi=%x edi=%x ecx=%x",
-			(unsigned int)inst.code.op,
-			(unsigned int)si_index,
-			(unsigned int)di_index,
-			(unsigned int)count,
-			(unsigned int)count_left,
-			(unsigned int)(count+count_left),
-			(unsigned int)reg_esi,
-			(unsigned int)reg_edi,
-			(unsigned int)reg_ecx);
+			switch (inst.code.op) {
+				case R_OUTSB:
+					do {
+						IO_WriteB(reg_dx,LoadMb(si_base+si_index));
+						si_index=(si_index+add_index) & add_mask;
+						count--;
 
-		/* Clean up after certain amount of instructions */
-		reg_esi&=(~add_mask);
-		reg_esi|=(si_index & add_mask);
-		reg_edi&=(~add_mask);
-		reg_edi|=(di_index & add_mask);
-		if (inst.prefix & PREFIX_REP) {
-			count+=count_left;
-			reg_ecx&=(~add_mask);
-			reg_ecx|=(count & add_mask);
-		}
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+				case R_OUTSW:
+					add_index<<=1;
+					do {
+						IO_WriteW(reg_dx,LoadMw(si_base+si_index));
+						si_index=(si_index+add_index) & add_mask;
+						count--;
 
-		throw;
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+				case R_OUTSD:
+					add_index<<=2;
+					do {
+						IO_WriteD(reg_dx,LoadMd(si_base+si_index));
+						si_index=(si_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+
+				case R_INSB:
+					do {
+						SaveMb(di_base+di_index,IO_ReadB(reg_dx));
+						di_index=(di_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+				case R_INSW:
+					add_index<<=1;
+					do {
+						SaveMw(di_base+di_index,IO_ReadW(reg_dx));
+						di_index=(di_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+				case R_INSD:
+					add_index<<=2;
+					do {
+						SaveMd(di_base+di_index,IO_ReadD(reg_dx));
+						di_index=(di_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+
+				case R_STOSB:
+					do {
+						SaveMb(di_base+di_index,reg_al);
+						di_index=(di_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+				case R_STOSW:
+					add_index<<=1;
+					do {
+						SaveMw(di_base+di_index,reg_ax);
+						di_index=(di_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+				case R_STOSD:
+					add_index<<=2;
+					do {
+						SaveMd(di_base+di_index,reg_eax);
+						di_index=(di_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+
+				case R_MOVSB:
+					do {
+						SaveMb(di_base+di_index,LoadMb(si_base+si_index));
+						di_index=(di_index+add_index) & add_mask;
+						si_index=(si_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+				case R_MOVSW:
+					add_index<<=1;
+					do {
+						SaveMw(di_base+di_index,LoadMw(si_base+si_index));
+						di_index=(di_index+add_index) & add_mask;
+						si_index=(si_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+				case R_MOVSD:
+					add_index<<=2;
+					do {
+						SaveMd(di_base+di_index,LoadMd(si_base+si_index));
+						di_index=(di_index+add_index) & add_mask;
+						si_index=(si_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+
+				case R_LODSB:
+					do {
+						reg_al=LoadMb(si_base+si_index);
+						si_index=(si_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+				case R_LODSW:
+					add_index<<=1;
+					do {
+						reg_ax=LoadMw(si_base+si_index);
+						si_index=(si_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+				case R_LODSD:
+					add_index<<=2;
+					do {
+						reg_eax=LoadMd(si_base+si_index);
+						si_index=(si_index+add_index) & add_mask;
+						count--;
+
+						if ((--CPU_Cycles) <= 0) break;
+					} while (count != 0); break;
+
+				case R_SCASB:
+					{
+						Bit8u val2;
+						do {
+							val2=LoadMb(di_base+di_index);
+							di_index=(di_index+add_index) & add_mask;
+							count--;
+
+							if ((--CPU_Cycles) <= 0) break;
+							if ((reg_al==val2)!=inst.repz) break;
+						} while (count != 0);
+						CMPB(reg_al,val2,LoadD,0);
+					}
+					break;
+				case R_SCASW:
+					add_index<<=1;
+					{
+						Bit16u val2;
+						do {
+							val2=LoadMw(di_base+di_index);
+							di_index=(di_index+add_index) & add_mask;
+							count--;
+
+							if ((--CPU_Cycles) <= 0) break;
+							if ((reg_ax==val2)!=inst.repz) break;
+						} while (count != 0);
+						CMPW(reg_ax,val2,LoadD,0);
+					}
+					break;
+				case R_SCASD:
+					add_index<<=2;
+					{
+						Bit32u val2;
+						do {
+							val2=LoadMd(di_base+di_index);
+							di_index=(di_index+add_index) & add_mask;
+							count--;
+
+							if ((--CPU_Cycles) <= 0) break;
+							if ((reg_eax==val2)!=inst.repz) break;
+						} while (count != 0);
+						CMPD(reg_eax,val2,LoadD,0);
+					}
+					break;
+
+				case R_CMPSB:
+					{
+						Bit8u val1,val2;
+						do {
+							val1=LoadMb(si_base+si_index);
+							val2=LoadMb(di_base+di_index);
+							si_index=(si_index+add_index) & add_mask;
+							di_index=(di_index+add_index) & add_mask;
+							count--;
+
+							if ((--CPU_Cycles) <= 0) break;
+							if ((val1==val2)!=inst.repz) break;
+						} while (count != 0);
+						CMPB(val1,val2,LoadD,0);
+					}
+					break;
+				case R_CMPSW:
+					add_index<<=1;
+					{
+						Bit16u val1,val2;
+						do {
+							val1=LoadMw(si_base+si_index);
+							val2=LoadMw(di_base+di_index);
+							si_index=(si_index+add_index) & add_mask;
+							di_index=(di_index+add_index) & add_mask;
+							count--;
+
+							if ((--CPU_Cycles) <= 0) break;
+							if ((val1==val2)!=inst.repz) break;
+						} while (count != 0);
+						CMPW(val1,val2,LoadD,0);
+					}
+					break;
+				case R_CMPSD:
+					add_index<<=2;
+					{
+						Bit32u val1,val2;
+						do {
+							val1=LoadMd(si_base+si_index);
+							val2=LoadMd(di_base+di_index);
+							si_index=(si_index+add_index) & add_mask;
+							di_index=(di_index+add_index) & add_mask;
+							count--;
+
+							if ((--CPU_Cycles) <= 0) break;
+							if ((val1==val2)!=inst.repz) break;
+						} while (count != 0);
+						CMPD(val1,val2,LoadD,0);
+					}
+					break;
+
+				default:
+					LOG(LOG_CPU,LOG_ERROR)("Unhandled string op %d",inst.code.op);
+			}
+
+			/* Clean up after certain amount of instructions */
+			reg_esi&=(~add_mask);
+			reg_esi|=(si_index & add_mask);
+			reg_edi&=(~add_mask);
+			reg_edi|=(di_index & add_mask);
+			if (inst.prefix & PREFIX_REP) {
+				count+=count_left;
+				reg_ecx&=(~add_mask);
+				reg_ecx|=(count & add_mask);
+
+				/* if the count is still nonzero, then there is still work to do and the
+				 * instruction has not finished executing---it needs to be restarted.
+				 * if the string op was REP CMPSB or REP SCASB then it also matters
+				 * whether the ZF flag matches the REP condition on whether or not we
+				 * restart the instruction. */
+				if (count != 0) {
+					if (inst.code.op < R_SCASB) {
+						/* if count != 0 then restart the instruction */
+						LoadIP();
+					}
+					else {
+						/* if ZF matches the REP condition, restart the instruction */
+						if ((get_ZF()?1:0) == (inst.repz?1:0)) {
+							LoadIP();
+						}
+					}
+				}
+			}
+		}
+		catch (GuestPageFaultException &pf) {
+			/* Clean up after certain amount of instructions */
+			reg_esi&=(~add_mask);
+			reg_esi|=(si_index & add_mask);
+			reg_edi&=(~add_mask);
+			reg_edi|=(di_index & add_mask);
+			if (inst.prefix & PREFIX_REP) {
+				count+=count_left;
+				reg_ecx&=(~add_mask);
+				reg_ecx|=(count & add_mask);
+			}
+
+			/* rethrow the exception.
+			 * NOTE: this means the normal core has no chance to execute SAVEIP, therefore
+			 *       when the guest OS has finished handling the page fault the instruction
+			 *       pointer will come right back to the string op that caused the fault
+			 *       and the string op will restart where it left off. */
+			throw;
+		}
 	}
-	}// /count
 }
 
