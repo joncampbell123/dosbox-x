b1b1c02ce4e66308003cdc39c3a8804a6b9db350
Jonathan Campbell <jonathan@castus.tv>
2015-06-20 16:53:05 -0700

    update header

--- src/cpu/lazyflags.h
+++ src/cpu/lazyflags.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2002-2013  The DOSBox Team
+ *  Copyright (C) 2002-2015  The DOSBox Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
  */
 
 #ifndef DOSBOX_LAZYFLAGS_H
-#define DOSBOX_LAZYFLAGS_h
+#define DOSBOX_LAZYFLAGS_H
 
 //Flag Handling
 Bit32u get_CF(void);
