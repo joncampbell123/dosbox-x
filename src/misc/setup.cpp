ded1931e4154273227deca877dfe849bd1709bab
Jonathan Campbell <jonathan@castus.tv>
2015-10-13 08:59:35 +0000

    announce config files we try to load

--- src/misc/setup.cpp
+++ src/misc/setup.cpp
@@ -851,6 +851,8 @@ Section* Config::GetSectionFromProperty(char const * const prop) const{
 
 
 bool Config::ParseConfigFile(char const * const configfilename){
+	LOG(LOG_MISC,LOG_DEBUG)("Attempting to load config file #%zu from %s",configfiles.size(),configfilename);
+
 	//static bool first_configfile = true;
 	ifstream in(configfilename);
 	if (!in) return false;
@@ -858,7 +860,7 @@ bool Config::ParseConfigFile(char const * const configfilename){
 	settings_type = (configfiles.size() == 0)? "primary":"additional";
 	configfiles.push_back(configfilename);
 	
-	LOG_MSG("CONFIG:Loading %s settings from config file %s", settings_type,configfilename);
+	LOG(LOG_MISC,LOG_NORMAL)("Loading %s settings from config file %s", settings_type,configfilename);
 
 	//Get directory from configfilename, used with relative paths.
 	current_config_dir=configfilename;
