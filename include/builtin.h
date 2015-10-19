53417ff401b49caa47609d3abe37fd4b45fb3642
Jonathan Campbell <jonathan@castus.tv>
2015-10-19 13:40:17 +0000

    move out DOS32A.EXE image

--- include/builtin.h
+++ include/builtin.h
@@ -1,5 +1,6 @@
 
 #include "dos_inc.h"
 
-extern struct BuiltinFileBlob bfb_CWSDPMI_EXE;
+extern struct BuiltinFileBlob bfb_CWSDPMI_EXE;		// CWSDPMI.EXE
+extern struct BuiltinFileBlob bfb_DOS32A_EXE;		// DOS32A.EXE
 
