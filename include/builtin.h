7df3fea2963b444c8ced02f6e461d967d9828e4a
Jonathan Campbell <jonathan@castus.tv>
2015-10-19 13:47:25 +0000

    DOS4GW.EXE move out

--- include/builtin.h
+++ include/builtin.h
@@ -3,4 +3,5 @@
 
 extern struct BuiltinFileBlob bfb_CWSDPMI_EXE;		// CWSDPMI.EXE
 extern struct BuiltinFileBlob bfb_DOS32A_EXE;		// DOS32A.EXE
+extern struct BuiltinFileBlob bfb_DOS4GW_EXE;		// DOS4GW.EXE
 
