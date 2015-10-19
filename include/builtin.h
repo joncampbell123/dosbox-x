f12e48a490f8044485588a6c3082fca73571c63d
Jonathan Campbell <jonathan@castus.tv>
2015-10-19 20:33:30 +0000

    DEBUG.EXE (aka DEBUG.COM) move out

--- include/builtin.h
+++ include/builtin.h
@@ -6,4 +6,5 @@ extern struct BuiltinFileBlob bfb_DOS32A_EXE;		// DOS32A.EXE
 extern struct BuiltinFileBlob bfb_DOS4GW_EXE;		// DOS4GW.EXE
 extern struct BuiltinFileBlob bfb_HEXMEM16_EXE;		// HEXMEM16.EXE
 extern struct BuiltinFileBlob bfb_HEXMEM32_EXE;		// HEXMEM32.EXE
+extern struct BuiltinFileBlob bfb_DEBUG_EXE;		// DEBUG.EXE
 
