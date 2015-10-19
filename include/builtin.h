decd6df4392af94ae1825eb433bbddee3a876573
Jonathan Campbell <jonathan@castus.tv>
2015-10-19 20:47:20 +0000

    mem.com move out

--- include/builtin.h
+++ include/builtin.h
@@ -9,4 +9,5 @@ extern struct BuiltinFileBlob bfb_HEXMEM32_EXE;		// HEXMEM32.EXE
 extern struct BuiltinFileBlob bfb_DEBUG_EXE;		// DEBUG.EXE
 extern struct BuiltinFileBlob bfb_TREE_EXE;		// TREE.EXE
 extern struct BuiltinFileBlob bfb_MOVE_EXE;		// MOVE.EXE
+extern struct BuiltinFileBlob bfb_MEM_COM;		// MEM.COM
 
