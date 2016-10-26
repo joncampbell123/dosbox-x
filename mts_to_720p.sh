eaeaf5e66f9b4ed0327bb73e4f906054fe02fc58
Jonathan Campbell <jonathan@castus.tv>
2016-10-26 13:21:33 -0700

    720 conversion generalization

--- mts_to_720p.sh
+++ mts_to_720p.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-file=cma_shrt_000.mts
+file="$1"
 bitrate=15000000
 aspect_ratio=4:3
-ffmpeg -i "$file" -acodec copy -vcodec libx264 -pix_fmt yuv422p -r 300000/1001 -vsync vfr -bsf:a aac_adtstoasc -vf 'scale=864:648,pad=width=960:height=720:x=(ow-iw)/2:y=(oh-ih)/2' -vb "$bitrate" -minrate "$bitrate" -maxrate "$bitrate" -bufsize 8000000 -g 15 -bf 2 -threads 0 -aspect "$aspect_ratio" -y -f mp4 "$file.mp4" || exit 1
+overscan_percent=10
+final_height=720
+final_width=960
+
+# non-editable part
+render_width=$((($final_width * (100-$overscan_percent))/100))
+render_height=$((($final_height * (100-$overscan_percent))/100))
+
+# announce
+echo "Rendering as $render_width x $render_height encoding to $final_width x $final_height"
+
+# go
+ffmpeg -i "$file" -acodec copy -vcodec libx264 -pix_fmt yuv422p -r 300000/1001 -vsync vfr -bsf:a aac_adtstoasc -vf "scale=$render_width:$render_height,pad=width=$final_width:height=$final_height:"'x=(ow-iw)/2:y=(oh-ih)/2' -vb "$bitrate" -minrate "$bitrate" -maxrate "$bitrate" -bufsize 8000000 -g 15 -bf 2 -threads 0 -aspect "$aspect_ratio" -y -f mp4 "$file.mp4" || exit 1
 
