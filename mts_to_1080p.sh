574ac36025bce97c85c66d6112b98f96ef3e3d49
Jonathan Campbell <jonathan@castus.tv>
2016-10-26 13:23:00 -0700

    the scripts are meant for rendering to video edit, so set the colorspace to 4:2:0 for better compatability.

--- mts_to_1080p.sh
+++ mts_to_1080p.sh
@@ -14,5 +14,5 @@ render_height=$((($final_height * (100-$overscan_percent))/100))
 echo "Rendering as $render_width x $render_height encoding to $final_width x $final_height"
 
 # go
-ffmpeg -i "$file" -acodec copy -vcodec libx264 -pix_fmt yuv422p -r 300000/1001 -vsync vfr -bsf:a aac_adtstoasc -vf "scale=$render_width:$render_height,pad=width=$final_width:height=$final_height:"'x=(ow-iw)/2:y=(oh-ih)/2' -vb "$bitrate" -minrate "$bitrate" -maxrate "$bitrate" -bufsize 8000000 -g 15 -bf 2 -threads 0 -aspect "$aspect_ratio" -y -f mp4 "$file.mp4" || exit 1
+ffmpeg -i "$file" -acodec copy -vcodec libx264 -pix_fmt yuv420p -r 300000/1001 -vsync vfr -bsf:a aac_adtstoasc -vf "scale=$render_width:$render_height,pad=width=$final_width:height=$final_height:"'x=(ow-iw)/2:y=(oh-ih)/2' -vb "$bitrate" -minrate "$bitrate" -maxrate "$bitrate" -bufsize 8000000 -g 15 -bf 2 -threads 0 -aspect "$aspect_ratio" -y -f mp4 "$file.mp4" || exit 1
 
