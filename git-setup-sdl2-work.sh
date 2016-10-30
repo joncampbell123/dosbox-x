0c60178484bbe0706d64a4e6d35a4d744c7dadb4
Jonathan Campbell <jonathan@castus.tv>
2016-10-30 00:05:40 -0700

    more

--- git-setup-sdl2-work.sh
+++ git-setup-sdl2-work.sh
@@ -3,4 +3,5 @@ url="https://github.com/wcp16/dosbox-xe.git"
 branch="dosbox-sdl2-upstream"
 git remote add "$branch" "$url" || git remote set-url "$branch" "$url" || exit 1
 git fetch "$branch" || exit 1
+git checkout -t "$branch" || exit 1
 
