e3d5290a95077234d24dea4e1c5b5816ba93486c
Jonathan Campbell <jonathan@castus.tv>
2016-10-29 23:55:18 -0700

    begin SDL2 port work, first by adding script to refer to other repository

---
+++ git-setup-sdl2-work.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+url="https://github.com/wcp16/dosbox-xe.git"
+branch="dosbox-sdl2-upstream"
+git remote add "$branch" "$url" || git remote set-url "$branch" "$url" || exit 1
+git fetch "$branch" || exit 1
+
