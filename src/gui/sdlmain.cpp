672bfd58aef50a159587ae5eb55a0df637a3e595
Jonathan Campbell <jonathan@castus.tv>
2017-04-01 14:41:49 -0700

    fix fresh reboot of guest os

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -4914,6 +4914,7 @@ fresh_boot:
             throw;
         }
 
+fresh_dos_exit:
         if (dos_kernel_shutdown) {
             /* NTS: we take different paths depending on whether we're just shutting down DOS
              *      or doing a hard reboot. */
@@ -4991,17 +4992,28 @@ fresh_boot:
 				while (1/*execute until some other part of DOSBox throws exception*/)
 					DOSBOX_RunMachine();
 			}
-			catch (int x) {
-				if (x == 3) { /* reboot the machine */
-					LOG(LOG_MISC,LOG_DEBUG)("Emulation threw a signal to reboot the system");
+            catch (int x) {
+                if (x == 2) { /* booting a guest OS. "boot" has already done the work to load the image and setup CPU registers */
+                    LOG(LOG_MISC,LOG_DEBUG)("Emulation threw a signal to boot guest OS");
 
-					reboot_machine = true;
-				}
-				else {
-					LOG(LOG_MISC,LOG_DEBUG)("Emulation threw DOSBox kill switch signal");
+                    run_machine = false;
+                    reboot_machine = false;
+                    dos_kernel_shutdown = false;
 
-					// kill switch (see instances of throw(0) and throw(1) elsewhere in DOSBox)
-				}
+                    run_machine = true; /* make note. don't run the whole shebang from an exception handler! */
+                    dos_kernel_shutdown = true;
+                    goto fresh_dos_exit;
+                }
+                else if (x == 3) { /* reboot the machine */
+                    LOG(LOG_MISC,LOG_DEBUG)("Emulation threw a signal to reboot the system");
+
+                    reboot_machine = true;
+                }
+                else {
+                    LOG(LOG_MISC,LOG_DEBUG)("Emulation threw DOSBox kill switch signal");
+
+                    // kill switch (see instances of throw(0) and throw(1) elsewhere in DOSBox)
+                }
 			}
 			catch (...) {
 				throw;
