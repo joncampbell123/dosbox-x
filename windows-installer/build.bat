16de3cd99ee5cad36702510ae5779713f416818a
Aybe <aybe.one@hotmail.com>
2017-08-24 05:13:18 +0200

    major overhaul for installer: optimized, command line, inconsistencies

---
+++ windows-installer/build.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" DOSBox-X-setup-win32.iss
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" DOSBox-X-setup-win64.iss
---
+++ windows-installer/readme.txt
@@ -0,0 +1,3 @@
+- install innosetup first
+- build.bat assumes we are on 64-bit windows
+
