8e7dfaf43c512303034f9f82897caeafb0ac3b57
Aybe <aybe.one@hotmail.com>
2017-09-14 11:36:36 +0200

    Enhanced package build for Windows : added standalone archives, both installers and standalone have same build date
    TODO allow independent installer/standalone build ? (i.e. currently one must have InnoSetup installed for it to work)

--- windows-installer/build.bat
+++ windows-installer/build.bat
@@ -1,3 +1,55 @@
 @echo off
-"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" DOSBox-X-setup-win32.iss
-"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" DOSBox-X-setup-win64.iss
+
+cls
+
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set DOSBOXDATE=%%I
+set DOSBOXDATE=%DOSBOXDATE:~0,8%-%DOSBOXDATE:~8,6%
+set DOSBOX32="..\bin\Win32\Release\dosbox-x.exe"
+set DOSBOX64="..\bin\x64\Release\dosbox-x.exe"
+set ISCC="C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
+
+if not exist %DOSBOX32% (
+	echo Couldn't find 32-bit DOSBox-X at %DOSBOX32%
+	goto error
+)
+
+if not exist %DOSBOX64% (
+	echo Couldn't find 64-bit DOSBox-X at %DOSBOX64%
+	goto error
+)
+
+if not exist %ISCC% (
+	echo Couldn't find Inno Setup 5 at %ISCC%
+	goto error
+)
+
+echo ************************************
+echo * Building DOSBox-X standalone ... *
+echo ************************************
+del dosbox-x-standalone-win*.7z
+7za a -mx9 dosbox-x-standalone-win32-%DOSBOXDATE% %DOSBOX32%
+7za a -mx9 dosbox-x-standalone-win64-%DOSBOXDATE% %DOSBOX64%
+
+echo ************************************
+echo * Building DOSBox-X installers ... *
+echo ************************************
+del dosbox-x-setup-win*.exe
+%ISCC% DOSBox-X-setup-win32.iss
+%ISCC% DOSBox-X-setup-win64.iss
+ren DOSBox-X-setup-win32.exe DOSBox-X-setup-win32-%DOSBOXDATE%.exe
+ren DOSBox-X-setup-win64.exe DOSBox-X-setup-win64-%DOSBOXDATE%.exe
+goto success
+
+:error
+echo An error has occurred, aborting ...
+goto end
+
+:success
+echo Success !
+goto end
+
+:end
+set DOSBOXDATE=
+set DOSBOX32=
+set DOSBOX64=
+set ISCC=
