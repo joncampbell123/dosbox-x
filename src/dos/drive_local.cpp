3ec4f475f05e7673d93b73e64b3f20cf88fc197b
Jonathan Campbell <jonathan@castus.tv>
2017-11-29 06:17:58 -0800

    more

--- src/dos/drive_local.cpp
+++ src/dos/drive_local.cpp
@@ -74,7 +74,7 @@ typedef char host_cnv_char_t;
 
 static host_cnv_char_t cpcnv_temp[4096];
 
-template <class MT> bool String_SBCS_TO_HOST(char *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/,const MT *map,const size_t map_max) {
+template <class MT> bool String_SBCS_TO_HOST(host_cnv_char_t *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/,const MT *map,const size_t map_max) {
     const char *sf = s + CROSS_LEN - 1;
     char *df = d + CROSS_LEN - 1;
     unsigned char ic;
@@ -95,7 +95,7 @@ template <class MT> bool String_SBCS_TO_HOST(char *d/*CROSS_LEN*/,const char *s/
     return true;
 }
 
-template <class MT> bool String_DBCS_TO_HOST_SHIFTJIS(char *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/,const MT *hitbl,const MT *rawtbl,const size_t rawtbl_max) {
+template <class MT> bool String_DBCS_TO_HOST_SHIFTJIS(host_cnv_char_t *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/,const MT *hitbl,const MT *rawtbl,const size_t rawtbl_max) {
     const char *sf = s + CROSS_LEN - 1;
     char *df = d + CROSS_LEN - 1;
     uint16_t ic;
@@ -156,7 +156,7 @@ template <class MT> int DBCS_SHIFTJIS_From_Host_Find(int c,const MT *hitbl,const
     return -1;
 }
 
-template <class MT> bool String_HOST_TO_DBCS_SHIFTJIS(char *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/,const MT *hitbl,const MT *rawtbl,const size_t rawtbl_max) {
+template <class MT> bool String_HOST_TO_DBCS_SHIFTJIS(char *d/*CROSS_LEN*/,const host_cnv_char_t *s/*CROSS_LEN*/,const MT *hitbl,const MT *rawtbl,const size_t rawtbl_max) {
     const char *sf = s + CROSS_LEN - 1;
     char *df = d + CROSS_LEN - 1;
     int ic;
@@ -195,7 +195,7 @@ template <class MT> bool String_HOST_TO_DBCS_SHIFTJIS(char *d/*CROSS_LEN*/,const
     return true;
 }
 
-template <class MT> bool String_HOST_TO_SBCS(char *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/,const MT *map,const size_t map_max) {
+template <class MT> bool String_HOST_TO_SBCS(char *d/*CROSS_LEN*/,const host_cnv_char_t *s/*CROSS_LEN*/,const MT *map,const size_t map_max) {
     const char *sf = s + CROSS_LEN - 1;
     char *df = d + CROSS_LEN - 1;
     int ic;
@@ -221,7 +221,7 @@ template <class MT> bool String_HOST_TO_SBCS(char *d/*CROSS_LEN*/,const char *s/
 
 bool cpwarn_once = false;
 
-bool CodePageHostToGuest(char *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/) {
+bool CodePageHostToGuest(char *d/*CROSS_LEN*/,const host_cnv_char_t *s/*CROSS_LEN*/) {
     switch (dos.loaded_codepage) {
         case 437:
             return String_HOST_TO_SBCS<uint16_t>(d,s,cp437_to_unicode,sizeof(cp437_to_unicode)/sizeof(cp437_to_unicode[0]));
@@ -241,7 +241,7 @@ bool CodePageHostToGuest(char *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/) {
     return false;
 }
 
-bool CodePageGuestToHost(char *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/) {
+bool CodePageGuestToHost(host_cnv_char_t *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/) {
     switch (dos.loaded_codepage) {
         case 437:
             return String_SBCS_TO_HOST<uint16_t>(d,s,cp437_to_unicode,sizeof(cp437_to_unicode)/sizeof(cp437_to_unicode[0]));
