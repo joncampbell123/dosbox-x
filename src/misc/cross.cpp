fea5b6d2da04c5df2ab11cdd099e54adef2da057
Jonathan Campbell <jonathan@castus.tv>
2017-11-29 08:58:34 -0800

    more

--- src/misc/cross.cpp
+++ src/misc/cross.cpp
@@ -162,7 +162,7 @@ dir_information* open_directoryw(const wchar_t* dirname) {
 dir_information* open_directory(const char* dirname) {
 	if (dirname == NULL) return NULL;
 
-	size_t len = strlen(dirname);
+	size_t len = wcslen(dirname);
 	if (len == 0) return NULL;
 
 	static dir_information dir;
