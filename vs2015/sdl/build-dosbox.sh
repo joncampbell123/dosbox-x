1f932a602c3ff9fc5fc92c150291795b6d8aca75
Jonathan Campbell <jonathan@castus.tv>
2017-12-13 22:50:53 -0800

    I have no idea if this fixes it, but, try NOT dynamically loading pulseaudio's shared library.

--- vs2015/sdl/build-dosbox.sh
+++ vs2015/sdl/build-dosbox.sh
@@ -2,7 +2,8 @@
 rm -Rfv linux-host || exit 1
 mkdir -p linux-host || exit 1
 chmod +x configure || exit 1
-ac_cv_header_iconv_h=no ac_cv_func_iconv=no ac_cv_lib_iconv_libiconv_open=no ./configure "--prefix=`pwd`/linux-host" --enable-static --disable-shared || exit 1
+# NTS: We use --disable-pulseaudio-shared because the dynamic loading seems to be slightly unstable
+ac_cv_header_iconv_h=no ac_cv_func_iconv=no ac_cv_lib_iconv_libiconv_open=no ./configure "--prefix=`pwd`/linux-host" --enable-static --disable-shared --disable-pulseaudio-shared || exit 1
 make -j || exit 1
 make install || exit 1  # will install into ./linux-host
 cp -vf include/SDL_config.h.default include/SDL_config.h || exit 1
