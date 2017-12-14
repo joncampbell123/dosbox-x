a4a2c15fbd8342cedecd48a3c94ae370bf79ce15
Jonathan Campbell <jonathan@castus.tv>
2017-12-14 02:08:52 -0800

    Revert "Let's just disable PulseAudio entirely in our SDL builds and be done"
    This reverts commit b70a024a0806429b49ede0b86969be9aa74ce790.
    
    Okay fine PulseAudio. I pulled in SDL 1.x for a reason, and here it is.
    The audio thread handling needs to be fixed, just as the Windows builds
    need to fix the SDL 1.x issue with relative mouse motion. This is why.
    Time to start hacking. If it makes it incompatible with the main SDL 1.x
    library at this point I don't care, it's time to fix this nonsense.

--- vs2015/sdl/build-dosbox.sh
+++ vs2015/sdl/build-dosbox.sh
@@ -2,7 +2,7 @@
 rm -Rfv linux-host || exit 1
 mkdir -p linux-host || exit 1
 chmod +x configure || exit 1
-ac_cv_header_iconv_h=no ac_cv_func_iconv=no ac_cv_lib_iconv_libiconv_open=no ./configure "--prefix=`pwd`/linux-host" --enable-static --disable-shared --disable-pulseaudio || exit 1
+ac_cv_header_iconv_h=no ac_cv_func_iconv=no ac_cv_lib_iconv_libiconv_open=no ./configure "--prefix=`pwd`/linux-host" --enable-static --disable-shared || exit 1
 make -j || exit 1
 make install || exit 1  # will install into ./linux-host
 cp -vf include/SDL_config.h.default include/SDL_config.h || exit 1
