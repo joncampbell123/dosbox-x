99fb17ff6aa40bd45fbc991a73bd0402c89f1617
Jonathan Campbell <jonathan@castus.tv>
2017-12-19 11:43:42 -0800

    split some code out to a header

--- src/hardware/vga.cpp
+++ src/hardware/vga.cpp
@@ -2148,47 +2148,9 @@ void SVGA_Setup_Driver(void) {
 	}
 }
 
-// GLUE TYPEDEFS
-// WARNING: Windows targets will want to IFDEF some of these out as they will
-//          conflict with the typedefs in windows.h
-typedef uint32_t UINT32;
-typedef int32_t SINT32;
-typedef uint16_t UINT16;
-typedef int16_t SINT16;
-typedef uint8_t UINT8;
-typedef int8_t SINT8;
-typedef uint32_t UINT;
-typedef uint32_t REG8; /* GLIB guint32 -> UINT -> REG8 */
-#ifndef WIN32
-typedef uint8_t BOOL;
-#endif
-typedef char OEMCHAR;
-typedef void* NEVENTITEM;
-typedef void* NP2CFG;
-#define OEMTEXT(x) (x)
-#define SOUNDCALL
-
-#define LOADINTELWORD(x) host_readw((HostPt)(x))
-#define STOREINTELWORD(x,y) host_writew((HostPt)(x),(y))
-
-#define TRUE 1
-#define FALSE 0
+#include "np2glue.h"
 
 MixerChannel *pc98_mixer = NULL;
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-#ifndef WIN32
-static inline void ZeroMemory(void *p,size_t l) {
-    memset(p,0,l);
-}
-
-static inline void FillMemory(void *p,size_t l,unsigned char c) {
-    memset(p,c,l);
-}
-#endif
 
 static inline void pcm86io_bind(void) {
     /* dummy */
