a02be3b7d44e497894aaaafdcbdbe896bdf34a17
Jonathan Campbell <jonathan@castus.tv>
2017-12-24 03:05:35 -0800

    IMGMOUNT/FAT: When searching for a good partition, make sure to pick one with a type ID that indicates the FAT filesystem. Announce the type ID when indicating the partition chosen.

--- src/dos/drive_fat.cpp
+++ src/dos/drive_fat.cpp
@@ -719,8 +719,12 @@ fatDrive::fatDrive(const char *sysFilename, Bit32u bytesector, Bit32u cylsector,
 		int m;
 		for(m=0;m<4;m++) {
 			/* Pick the first available partition */
-			if(mbrData.pentry[m].partSize != 0x00) {
-				LOG_MSG("Using partition %d on drive; skipping %d sectors", m, mbrData.pentry[m].absSectStart);
+			if(mbrData.pentry[m].partSize != 0x00 &&
+                (mbrData.pentry[m].parttype == 0x01 || mbrData.pentry[m].parttype == 0x04 ||
+                 mbrData.pentry[m].parttype == 0x06 || mbrData.pentry[m].parttype == 0x0B ||
+                 mbrData.pentry[m].parttype == 0x0C || mbrData.pentry[m].parttype == 0x0D ||
+                 mbrData.pentry[m].parttype == 0x0E || mbrData.pentry[m].parttype == 0x0F)) {
+				LOG_MSG("Using partition %d on drive (type 0x%02x); skipping %d sectors", m, mbrData.pentry[m].parttype, mbrData.pentry[m].absSectStart);
 				startSector = mbrData.pentry[m].absSectStart;
 				break;
 			}
