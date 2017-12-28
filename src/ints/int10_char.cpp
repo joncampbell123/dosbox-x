7c76d07a260d61a91d3bbb30cf17868254db0cd2
Jonathan Campbell <jonathan@castus.tv>
2017-12-28 10:48:42 -0800

    Separate ShiftJISDecoder into C++ class header and source file parts.

--- src/ints/int10_char.cpp
+++ src/ints/int10_char.cpp
@@ -527,79 +527,86 @@ void INT10_PC98_CurMode_Relocate(void) {
 }
 
 struct ShiftJISDecoder {
-    ShiftJISDecoder() {
-        reset();
-    }
-    void reset(void) {
-        fullwidth = false;
-        b1 = b2 = 0;
-    }
-    bool take(unsigned char c) {
-        /* JIS sequence j1, j2
-         * ShiftJIS bytes s1, s2
-         *
-         * if (33 <= j1 && j1 <= 94)
-         *   s1 = ((j1 + 1) / 2) + 112
-         * else if (95 <= j1 && j1 <= 126)
-         *   s1 = ((j1 + 1) / 2) + 176
-         * 
-         * if (j1 & 1)    <- is odd
-         *   s2 = j2 + 31 + (j2 / 96)
-         * else           <- is even
-         *   s2 = j2 + 126
-         */ 
-        if (!fullwidth) {
-            if (c >= 0x81 && c <= 0x9F) {
-                /* Reverse:
-                 *
-                 *    s1 = ((j1 + 1) / 2) + 112
-                 *    s1 - 112 = (j1 + 1) / 2
-                 *    (s1 - 112) * 2 = j1 + 1
-                 *    ((s1 - 112) * 2) - 1 = j1 */
-                fullwidth = true;
-                b1 = (c - 112) * 2;
-                return false;
-            }
-            else if (c >= 0xE0 && c <= 0xEF) {
-                /* Reverse:
-                 *
-                 *    s1 = ((j1 + 1) / 2) + 176
-                 *    s1 - 176 = (j1 + 1) / 2
-                 *    (s1 - 176) * 2 = j1 + 1
-                 *    ((s1 - 176) * 2) - 1 = j1 */
-                fullwidth = true;
-                b1 = (c - 176) * 2;
-                return false;
-            }
-            else {
-                b2 = 0;
-                b1 = c;
-            }
-        }
-        else { // fullwidth, 2nd byte
-            if (c >= 0x9F) { /* j1 is even */
-                b2 = c - 126;
-            }
-            else if (c >= 0x40 && c != 0x7F) { /* j1 is odd */
-                b1--; /* (j1 + 1) / 2 */
-                b2 = c - 31;
-                if (c >= 0x80) b2--; /* gap at 0x7F */
-            }
-            else {
-                // ???
-                b1 = 0x7F;
-                b2 = 0x7F;
-            }
+                        ShiftJISDecoder();
 
-            fullwidth = false;
-        }
-
-        return true;
-    }
+    void                reset(void);
+    bool                take(unsigned char c);
 public:
     unsigned char       b1,b2;
     bool                fullwidth;
 };
+
+ShiftJISDecoder::ShiftJISDecoder() {
+    reset();
+}
+
+void ShiftJISDecoder::reset(void) {
+    fullwidth = false;
+    b1 = b2 = 0;
+}
+
+bool ShiftJISDecoder::take(unsigned char c) {
+    /* JIS sequence j1, j2
+     * ShiftJIS bytes s1, s2
+     *
+     * if (33 <= j1 && j1 <= 94)
+     *   s1 = ((j1 + 1) / 2) + 112
+     * else if (95 <= j1 && j1 <= 126)
+     *   s1 = ((j1 + 1) / 2) + 176
+     * 
+     * if (j1 & 1)    <- is odd
+     *   s2 = j2 + 31 + (j2 / 96)
+     * else           <- is even
+     *   s2 = j2 + 126
+     */ 
+    if (!fullwidth) {
+        if (c >= 0x81 && c <= 0x9F) {
+            /* Reverse:
+             *
+             *    s1 = ((j1 + 1) / 2) + 112
+             *    s1 - 112 = (j1 + 1) / 2
+             *    (s1 - 112) * 2 = j1 + 1
+             *    ((s1 - 112) * 2) - 1 = j1 */
+            fullwidth = true;
+            b1 = (c - 112) * 2;
+            return false;
+        }
+        else if (c >= 0xE0 && c <= 0xEF) {
+            /* Reverse:
+             *
+             *    s1 = ((j1 + 1) / 2) + 176
+             *    s1 - 176 = (j1 + 1) / 2
+             *    (s1 - 176) * 2 = j1 + 1
+             *    ((s1 - 176) * 2) - 1 = j1 */
+            fullwidth = true;
+            b1 = (c - 176) * 2;
+            return false;
+        }
+        else {
+            b2 = 0;
+            b1 = c;
+        }
+    }
+    else { // fullwidth, 2nd byte
+        if (c >= 0x9F) { /* j1 is even */
+            b2 = c - 126;
+        }
+        else if (c >= 0x40 && c != 0x7F) { /* j1 is odd */
+            b1--; /* (j1 + 1) / 2 */
+            b2 = c - 31;
+            if (c >= 0x80) b2--; /* gap at 0x7F */
+        }
+        else {
+            // ???
+            b1 = 0x7F;
+            b2 = 0x7F;
+        }
+
+        fullwidth = false;
+    }
+
+    return true;
+}
 
 static ShiftJISDecoder int10_sjis;
 
