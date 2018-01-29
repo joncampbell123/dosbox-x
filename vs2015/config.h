081ba266dc467372826c0bbff86e65bd41329bff
Jonathan Campbell <jonathan@castus.tv>
2018-01-28 22:43:14 -0800

    configure.ac write PACKAGE autoconf variables to a header file that VS2015 can include so PACKAGE information is kept in sync across Linux and Windows

--- vs2015/config.h
+++ vs2015/config.h
@@ -187,9 +187,6 @@
 /* Compiling on OS/2 EMX */
 #undef OS2
 
-/* Name of package */
-#define PACKAGE					"dosbox"
-
 /* Define to the address where bug reports for this package should be sent. */
 
 /* Define to the full name of this package. */
@@ -335,9 +332,7 @@ typedef         double     Real64;
 #if defined(WIN32)
 # pragma warning(disable:4996)
 #endif
-#define PACKAGE_BUGREPORT "https://github.com/joncampbell123/dosbox-x/issues"
-#define PACKAGE_NAME "dosbox-x"
-#define PACKAGE_STRING "dosbox-x 0.82.1"
-#define PACKAGE_TARNAME "dosbox-x"
-#define PACKAGE_URL "http://dosbox-x.software"
-#define PACKAGE_VERSION "0.82.1"
+
+/* Linux-side configure script will write/rewrite this file so both Windows and Linux builds carry the same information --J.C. */
+#include "config_package.h"
+
