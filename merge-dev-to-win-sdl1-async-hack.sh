2c5a2403e0a35ee23969ef6b390b90b30448c5ed
Jonathan Campbell <jonathan@castus.tv>
2018-02-01 18:59:07 -0800

    more

--- merge-dev-to-win-sdl1-async-hack.sh
+++ merge-dev-to-win-sdl1-async-hack.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 # do NOT merge develop-win-sdl1-async-hack-201802 back into develop. I will do that manually when it is time.
+git checkout develop-win-sdl1-async-hack-201802
+git pull
+git push
+
+git checkout develop
+git pull
+git push
+
+git checkout master
+git pull
+git push
+
 git checkout develop-win-sdl1-async-hack-201802 && git merge develop && git checkout develop && git fetch --all && git push --all && make -j
