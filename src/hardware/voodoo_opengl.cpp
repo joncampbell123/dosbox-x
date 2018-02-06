be8ad076effa0474ada00495fa2a48ffdc98bb4e
Jonathan Campbell <jonathan@castus.tv>
2018-02-05 17:13:27 -0800

    3Dfx Voodoo OpenGL: OGL width/height report functions need to check to make sure the Voodoo C++ object is initialized.

--- src/hardware/voodoo_opengl.cpp
+++ src/hardware/voodoo_opengl.cpp
@@ -69,15 +69,21 @@ std::map <const UINT32, ogl_texmap> textures[2];
 void GFX_PreventFullscreen(bool lockout);
 
 int Voodoo_OGL_GetWidth() {
-    return v->fbi.width;
+	if (v != NULL)
+		return v->fbi.width;
+	else
+		return 0;
 }
 
 int Voodoo_OGL_GetHeight() {
-    return v->fbi.height;
+	if (v != NULL)
+		return v->fbi.height;
+	else
+		return 0;
 }
 
 bool Voodoo_OGL_Active() {
-	return (v->clock_enabled && v->output_on);
+	return (v != NULL && v->clock_enabled && v->output_on);
 }
 
 static void ogl_get_depth(voodoo_state* VV, INT32 ITERZ, INT64 ITERW, INT32 *depthval, INT32 *out_wfloat)
