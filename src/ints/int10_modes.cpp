b9f7577f88343e39f9105e26c595a501b762d079
Jonathan Campbell <jonathan@castus.tv>
2018-02-10 12:36:39 -0800

    INT 10h: Make sure VGA DAC currenet index is reset to 0 after modeset (fixes color palette errors in Second Reality)

--- src/ints/int10_modes.cpp
+++ src/ints/int10_modes.cpp
@@ -1531,6 +1531,8 @@ dac_text16:
 			/* check if gray scale summing is enabled */
 			if (modeset_ctl & 2) INT10_PerformGrayScaleSumming(0,256);
 		}
+        /* make sure the DAC index is reset on modeset */
+		IO_Write(0x3c8,0);
 	} else {
 		for (Bit8u ct=0x10;ct<ATT_REGS;ct++) {
 			if (ct==0x11) continue;	// skip overscan register
