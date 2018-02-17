2149d4cb8666c5a997ecc4a90915318f232e491c
Jonathan Campbell <jonathan@castus.tv>
2018-02-17 10:03:36 -0800

    another source file

--- src/hardware/pc98_fm.cpp
+++ src/hardware/pc98_fm.cpp
@@ -1172,72 +1172,7 @@ void keydisp_fmkeyon(UINT8 ch, UINT8 value) {
 	}
 }
 
-TMS3631CFG	tms3631cfg;
-
-static const UINT16 tms3631_freqtbl[] = {
-			0,	0x051B, 0x0569, 0x05BB, 0x0613, 0x066F, 0x06D1,
-				0x0739, 0x07A7, 0x081B, 0x0897, 0x091A, 0x09A4, 0,0,0,
-			0,	0x0A37, 0x0AD3, 0x0B77, 0x0C26, 0x0CDF, 0x0DA3,
-				0x0E72, 0x0F4E, 0x1037, 0x112E, 0x1234, 0x1349, 0,0,0,
-			0,	0x146E, 0x15A6, 0x16EF, 0x184C, 0x19BE, 0x1B46,
-				0x1CE5, 0x1E9D, 0x206F, 0x225D, 0x2468, 0x2692, 0,0,0,
-			0,	0x28DD, 0x2B4C, 0x2DDF, 0x3099, 0x337D, 0x368D,
-				0x39CB, 0x3D3B, 0x40DF, 0x44BA, 0x48D1, 0x4D25, 0x51BB, 0,0};
-
-
-void tms3631_initialize(UINT rate) {
-
-	UINT	sft;
-
-	ZeroMemory(&tms3631cfg, sizeof(tms3631cfg));
-	sft = 0;
-	if (rate == 11025) {
-		sft = 0;
-	}
-	else if (rate == 22050) {
-		sft = 1;
-	}
-	else if (rate == 44100) {
-		sft = 2;
-	}
-	tms3631cfg.ratesft = sft;
-}
-
-void tms3631_setvol(const UINT8 *vol) {
-
-	UINT	i;
-	UINT	j;
-	SINT32	data;
-
-	tms3631cfg.left = (vol[0] & 15) << 5;
-	tms3631cfg.right = (vol[1] & 15) << 5;
-	vol += 2;
-	for (i=0; i<16; i++) {
-		data = 0;
-		for (j=0; j<4; j++) {
-			data += (vol[j] & 15) * ((i & (1 << j))?1:-1);
-		}
-		tms3631cfg.feet[i] = data << 5;
-	}
-}
-
-
 // ----
-
-void tms3631_reset(TMS3631 tms) {
-
-	ZeroMemory(tms, sizeof(_TMS3631));
-}
-
-void tms3631_setkey(TMS3631 tms, REG8 ch, REG8 key) {
-
-	tms->ch[ch & 7].freq = tms3631_freqtbl[key & 0x3f] >> tms3631cfg.ratesft;
-}
-
-void tms3631_setenable(TMS3631 tms, REG8 enable) {
-
-	tms->enable = enable;
-}
 
 void fmboard_extreg(void (*ext)(REG8 enable)) {
 
@@ -1541,23 +1476,6 @@ const UINT8	*reg;
 	rhythm_setreg(rhy, 0x1c, reg[0x1c]);
 	rhythm_setreg(rhy, 0x1d, reg[0x1d]);
 }
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void tms3631_initialize(UINT rate);
-void tms3631_setvol(const UINT8 *vol);
-
-void tms3631_reset(TMS3631 tms);
-void tms3631_setkey(TMS3631 tms, REG8 ch, REG8 key);
-void tms3631_setenable(TMS3631 tms, REG8 enable);
-
-void SOUNDCALL tms3631_getpcm(TMS3631 tms, SINT32 *pcm, UINT count);
-
-#ifdef __cplusplus
-}
-#endif
 
 void fmboard_extreg(void (*ext)(REG8 enable));
 void fmboard_extenable(REG8 enable);
@@ -2505,48 +2423,6 @@ void SOUNDCALL psggen_getpcm(PSGGEN psg, SINT32 *pcm, UINT count) {
 		} while(++tone < toneterm);
 		pcm += 2;
 	} while(--count);
-}
-
-void SOUNDCALL tms3631_getpcm(TMS3631 tms, SINT32 *pcm, UINT count) {
-
-	UINT	ch;
-	SINT32	data;
-	UINT	i;
-
-	if (tms->enable == 0) {
-		return;
-	}
-	while(count--) {
-		ch = 0;
-		data = 0;
-		do {									// centre
-			if ((tms->enable & (1 << ch)) && (tms->ch[ch].freq)) {
-				for (i=0; i<4; i++) {
-					tms->ch[ch].count += tms->ch[ch].freq;
-					data += (tms->ch[ch].count & 0x10000)?1:-1;
-				}
-			}
-		} while(++ch < 2);
-		pcm[0] += data * tms3631cfg.left;
-		pcm[1] += data * tms3631cfg.right;
-		do {									// left
-			if ((tms->enable & (1 << ch)) && (tms->ch[ch].freq)) {
-				for (i=0; i<4; i++) {
-					tms->ch[ch].count += tms->ch[ch].freq;
-					pcm[0] += tms3631cfg.feet[(tms->ch[ch].count >> 16) & 15];
-				}
-			}
-		} while(++ch < 5);
-		do {									// right
-			if ((tms->enable & (1 << ch)) && (tms->ch[ch].freq)) {
-				for (i=0; i<4; i++) {
-					tms->ch[ch].count += tms->ch[ch].freq;
-					pcm[1] += tms3631cfg.feet[(tms->ch[ch].count >> 16) & 15];
-				}
-			}
-		} while(++ch < 8);
-		pcm += 2;
-	}
 }
 
 static void pc98_mix_CallBack(Bitu len) {
