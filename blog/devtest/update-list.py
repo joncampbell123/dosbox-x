f8077ecfb4461e31b08d0abb59145c371afd0fe5
Jonathan Campbell <jonathan@castus.tv>
2018-04-21 19:04:09 -0700

    more

--- blog/devtest/update-list.py
+++ blog/devtest/update-list.py
@@ -5,9 +5,13 @@ from lxml import etree
 # open the template page file.
 # Python will blow up with an exception here on failure.
 htmt_tree = etree.parse("_page.html")
+htmt_tree_root = htmt_tree.getroot()
+
+# find the LIST_PLACEHOLDER and remove it from the tree
+list_placeholder = htmt_tree_root.find("./body/LIST_PLACEHOLDER")
+if not list_placeholder == None:
+    list_placeholder.getparent().remove(list_placeholder)
 
 # write the final result
-indexhtml = open("index.html","wb")
-htmt_tree.write(indexhtml)
-indexhtml.close();
+htmt_tree.write("index.html")
 
