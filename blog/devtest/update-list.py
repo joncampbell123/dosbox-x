d5fe3888fb40fcb215e98a1f5f1df2fe717dab07
Jonathan Campbell <jonathan@castus.tv>
2018-04-21 19:13:10 -0700

    more

--- blog/devtest/update-list.py
+++ blog/devtest/update-list.py
@@ -10,7 +10,13 @@ htmt_tree_root = htmt_tree.getroot()
 # find the LIST_PLACEHOLDER and remove it from the tree
 list_placeholder = htmt_tree_root.find("./body/LIST_PLACEHOLDER")
 if not list_placeholder == None:
-    list_placeholder.getparent().remove(list_placeholder)
+    list_placeholder_index = list_placeholder.getparent().index(list_placeholder)
+    list_placeholder_parent = list_placeholder.getparent()
+    list_placeholder_parent.remove(list_placeholder)
+    #
+    list_tbl = etree.SubElement(list_placeholder_parent, "table", attrib={"width":"100%"})
+    #
+    list_placeholder_parent.insert(list_placeholder_index,list_tbl)
 
 # write the final result
 htmt_tree.write("index.html")
