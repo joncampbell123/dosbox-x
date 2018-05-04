8b2867156d6fb8021b60586d5752dde5a5337cae
Jonathan Campbell <jonathan@castus.tv>
2018-05-03 18:54:09 -0700

    more

--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,3 +1,3 @@
 /*auto-generated*/
-#define UPDATED_STR "May 3, 2018 6:49:33pm"
+#define UPDATED_STR "May 3, 2018 6:53:44pm"
 #define COPYRIGHT_END_YEAR "2018"
--- merge-dev-to-master.sh
+++ merge-dev-to-master.sh
@@ -6,8 +6,14 @@ git checkout develop
 git pull
 git push
 
+rm -f include/build_timestamp.h
+git checkout include/build_timestamp.h
+
 git checkout master
 git pull
 git push
+
+rm -f include/build_timestamp.h
+git checkout include/build_timestamp.h
 
 git checkout master && git merge develop && git checkout develop && git merge master && git fetch --all && git push --all && make -j
