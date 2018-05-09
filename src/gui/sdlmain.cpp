dceed2ba36f8e6656b84e37eec5d5412ba619adf
Jonathan Campbell <jonathan@castus.tv>
2018-05-09 13:02:12 -0700

    Show arrow for items with submenus

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -1279,6 +1279,9 @@ void DOSBoxMenu::item::drawMenuItem(DOSBoxMenu &menu) {
     if (shortBox.w != 0 && shortBox.h != 0)
         MenuDrawText(screenBox.x+shortBox.x, screenBox.y+shortBox.y, shortcut_text.c_str(), fgshortcolor);
 
+    if (type == submenu_type_id && borderTop/*not toplevel*/)
+        MenuDrawText(screenBox.x+screenBox.w - DOSBoxMenu::fontCharWidth - 1, screenBox.y+textBox.y, "\x10", fgcheckcolor);
+
     if (type >= separator_type_id)
         MenuDrawRect(screenBox.x, screenBox.y + (screenBox.h/2), screenBox.w, 1, fgcolor);
 
