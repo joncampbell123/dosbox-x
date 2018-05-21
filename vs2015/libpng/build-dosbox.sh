470cb3302124f57dd778d9c1ce60f4dd8409822f
Jonathan Campbell <jonathan@castus.tv>
2018-05-20 19:10:30 -0700

    CRLF filtering needed for libpng to compile on MinGW

--- vs2015/libpng/build-dosbox.sh
+++ vs2015/libpng/build-dosbox.sh
@@ -16,6 +16,12 @@ opts=
 
 ../configure "--srcdir=$srcdir" "--prefix=$instdir" --enable-static --disable-shared $opts || exit 1
 
+# mingw: need to remove ^M from the header file.
+# first make the file exist, then filter it.
+make pnglibconf.h || exit 1
+(cat pnglibconf.h | sed -e 's/\x0D//' >x) || exit 1
+mv x pnglibconf.h || exit 1
+
 make -j || exit 1
 make install || exit 1  # will install into ./linux-host
 
