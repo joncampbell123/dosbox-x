48c1c2be707d04ebf03748ab9013cf260bbaf232
Jonathan Campbell <jonathan@castus.tv>
2018-05-29 13:56:15 -0700

    qcow2 write prototype fix

--- src/ints/qcow2_disk.cpp
+++ src/ints/qcow2_disk.cpp
@@ -454,6 +454,6 @@ using namespace std;
 
 
 //Public function to a write a sector.
-	Bit8u QCow2Disk::Write_AbsoluteSector(Bit32u sectnum, void* data){
+	Bit8u QCow2Disk::Write_AbsoluteSector(Bit32u sectnum,const void* data){
 		return qcowImage.write_sector(sectnum, (Bit8u*)data);
 	}
