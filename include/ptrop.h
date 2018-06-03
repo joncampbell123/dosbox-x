877b839a4bfa0c6ab043142e16efe470837a0d73
Jonathan Campbell <jonathan@castus.tv>
2018-06-03 12:37:59 -0700

    ptrop by direct integer value rather than typeof

--- include/ptrop.h
+++ include/ptrop.h
@@ -3,12 +3,25 @@
 
 namespace ptrop {
 
+template <const uintptr_t A> static inline constexpr uintptr_t misalignment(const uintptr_t p) {
+    return p % (uintptr_t)A;
+}
+
 template <typename A> static inline constexpr uintptr_t misalignment(const uintptr_t p) {
-    return p % (uintptr_t)sizeof(A);
+    return misalignment<(uintptr_t)sizeof(A)>(p);
 }
 
 template <typename T=unsigned char,typename A=T> static inline constexpr uintptr_t misalignment(T* const p) {
     return misalignment<A>((uintptr_t)((unsigned char*)p));
+}
+
+template <typename T=unsigned char,const uintptr_t A> static inline constexpr uintptr_t misalignment(T* const p) {
+    return misalignment<A>((uintptr_t)((unsigned char*)p));
+}
+
+
+template <const uintptr_t A> static inline constexpr bool isaligned(const uintptr_t p) {
+    return misalignment<A>(p) == 0;
 }
 
 template <typename A> static inline constexpr bool isaligned(const uintptr_t p) {
@@ -19,19 +32,41 @@ template <typename T=unsigned char,typename A=T> static inline constexpr bool is
     return misalignment<T,A>(p) == 0;
 }
 
-template <typename A> static inline constexpr uintptr_t aligndown(const uintptr_t p) {
+template <typename T=unsigned char,const uintptr_t A> static inline constexpr bool isaligned(T* const p) {
+    return misalignment<T,A>(p) == 0;
+}
+
+
+template <const uintptr_t A> static inline constexpr uintptr_t aligndown(const uintptr_t p) {
     return p - misalignment<A>(p);
+}
+
+template <typename A> static inline constexpr uintptr_t aligndown(const uintptr_t p) {
+    return aligndown<(uintptr_t)sizeof(A)>(p);
 }
 
 template <typename T=unsigned char,typename A=T> static inline constexpr T* aligndown(T* const p) {
     return (T*)aligndown<A>((uintptr_t)((unsigned char*)p));
 }
 
-template <typename A> static inline constexpr uintptr_t alignup(const uintptr_t p) {
+template <typename T=unsigned char,const uintptr_t A> static inline constexpr T* aligndown(T* const p) {
+    return (T*)aligndown<A>((uintptr_t)((unsigned char*)p));
+}
+
+
+template <const uintptr_t A> static inline constexpr uintptr_t alignup(const uintptr_t p) {
     return aligndown<A>(p + (uintptr_t)sizeof(A) - (uintptr_t)1u);
 }
 
+template <typename A> static inline constexpr uintptr_t alignup(const uintptr_t p) {
+    return alignup<(uintptr_t)sizeof(A)>(p);
+}
+
 template <typename T=unsigned char,typename A=T> static inline constexpr T* alignup(T* const p) {
+    return (T*)alignup<A>((uintptr_t)((unsigned char*)p));
+}
+
+template <typename T=unsigned char,const uintptr_t A> static inline constexpr T* alignup(T* const p) {
     return (T*)alignup<A>((uintptr_t)((unsigned char*)p));
 }
 
