e5a9d9054f536f3407235ee423e5d833947020bd
Jonathan Campbell <jonathan@castus.tv>
2018-06-04 08:01:40 -0700

    commenting

--- include/ptrop.h
+++ include/ptrop.h
@@ -3,6 +3,7 @@
 
 namespace ptrop {
 
+/* return the misalignment in bytes of 'p' in the context of a data type of size 'A' */
 static inline constexpr uintptr_t misalignment(const uintptr_t p,const uintptr_t A) {
     return p % A;
 }
@@ -24,6 +25,7 @@ template <typename T=unsigned char,const uintptr_t A> static inline constexpr ui
 }
 
 
+/* indicate whether pointer 'p' is aligned to type of size 'A' */
 static inline constexpr bool isaligned(const uintptr_t p,const uintptr_t A) {
     return misalignment(p,A) == (uintptr_t)0;
 }
@@ -45,6 +47,7 @@ template <typename T=unsigned char,const uintptr_t A> static inline constexpr bo
 }
 
 
+/* take pointer 'p' and align downward to type of size 'A' */
 static inline constexpr uintptr_t aligndown(const uintptr_t p,const uintptr_t A) {
     return p - misalignment(p,A);
 }
@@ -66,6 +69,7 @@ template <typename T=unsigned char,const uintptr_t A> static inline constexpr T*
 }
 
 
+/* take pointer 'p' and align upward to type of size 'A' */
 static inline constexpr uintptr_t alignup(const uintptr_t p,const uintptr_t A) {
     return aligndown(p + (uintptr_t)A - (uintptr_t)1u,A);
 }
