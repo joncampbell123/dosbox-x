aed7f15e23dcead730d9351adad92efd267aea1b
Jonathan Campbell <jonathan@castus.tv>
2018-06-05 01:25:06 -0700

    We are not going to align pointers to 3, 5, or anything not a power of 2, so change the code to just use ANDing by (A - 1) for performance

--- include/ptrop.h
+++ include/ptrop.h
@@ -3,12 +3,10 @@
 
 namespace ptrop {
 
-/* return the misalignment in bytes of 'p' in the context of a data type of size 'A'
- * for best performance, enable optimizations in your C++ compiler and use a constant power of 2 for parameter 'A'.
- * If you meet that constraint, your C++ compiler should know to convert "p % A" into "p & (A - 1)" to use
- * bit masking instead of slower division. */
+/* return the misalignment in bytes of 'p' in the context of a data type of size 'A',
+ * 'A' must be a power of 2, or else this code will not work. */
 static inline constexpr uintptr_t misalignment(const uintptr_t p,const uintptr_t A) {
-    return p % A;
+    return p & (A - (uintptr_t)1u);
 }
 
 template <const uintptr_t A> static inline constexpr uintptr_t misalignment(const uintptr_t p) { // DEFER
