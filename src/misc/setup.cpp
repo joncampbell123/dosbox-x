cbd2f8bbd487986a9e89af0ba40c8ff5de9db9b4
Aybe <aybe@users.noreply.github.com>
2018-06-19 22:03:40 +0200

    Added single quote support

--- src/misc/setup.cpp
+++ src/misc/setup.cpp
@@ -1335,7 +1335,7 @@ CommandLine::CommandLine(char const * const name,char const * const cmdline,enum
     opt_style = opt;
     while ((c=*c_cmdline)!=0) {
         if (inquote) {
-            if (c!='"') str+=c;
+            if (c!='"'&&c!='\'') str+=c;
             else {
                 inquote=false;
                 cmds.push_back(str);
@@ -1349,7 +1349,7 @@ CommandLine::CommandLine(char const * const name,char const * const cmdline,enum
                 str.erase();
             }
         } 
-        else if (c=='"') { inquote=true;}
+        else if (c=='"'||c=='\'') { inquote=true;}
         else if (c!=' ') { str+=c;inword=true;}
         c_cmdline++;
     }
