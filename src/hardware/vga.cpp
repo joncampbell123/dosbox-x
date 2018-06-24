6158ec8f6d57bae94ffd43813314a27b8b99e54e
Aybe <aybe@users.noreply.github.com>
2018-06-24 05:11:39 +0200

    Added help and improved output.

--- src/hardware/vga.cpp
+++ src/hardware/vga.cpp
@@ -409,6 +409,15 @@ void VGA_SetCGA4Table(Bit8u val0,Bit8u val1,Bit8u val2,Bit8u val3) {
 class VFRCRATE : public Program {
 public:
     void Run(void) {
+		if (cmd->FindExist("/?", false)) {
+			WriteOut("Video refresh rate.\n\n");
+			WriteOut("VFRCRATE [SET [OFF|PAL|NTSC|rate]\n");
+			WriteOut("  SET OFF   unlock\n");
+			WriteOut("  SET PAL   lock to PAL frame rate\n");
+			WriteOut("  SET NTSC  lock to NTSC frame rate\n");
+			WriteOut("  SET rate  lock to integer or decimal frame rate, e.g. 15, 29.97\n");
+			return;
+		}
         if (cmd->FindString("SET",temp_line,false)) {
             char *x = (char*)temp_line.c_str();
 
@@ -437,11 +446,11 @@ public:
             VGA_SetupHandlers();
             VGA_StartResize();
         }
-        
+
         if (vga_force_refresh_rate > 0)
-            WriteOut("Video refresh rate locked to %.3ffps\n",vga_force_refresh_rate);
+            WriteOut("Locked to %.3f fps\n",vga_force_refresh_rate);
         else
-            WriteOut("Video refresh rate unlocked\n");
+            WriteOut("Unlocked\n");
     }
 };
 
