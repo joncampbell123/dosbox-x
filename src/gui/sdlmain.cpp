0923fc45d19f7b751f3bbddd433079e5dc0a22e8
Jonathan Campbell <jonathan@castus.tv>
2018-06-29 16:01:54 -0700

    MinGW does not have ppl.h

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -120,7 +120,7 @@ void RENDER_xBRZ_Early_Init(); // early initialization function defined in rende
 #endif
 #if C_XBRZ || C_SURFACE_POSTRENDER_ASPECT
 #include <xBRZ/xbrz_tools.h>
-# if defined(WIN32)
+# if defined(WIN32) && !defined(__MINGW32__)
 #  define XBRZ_PPL 1
 #  include <ppl.h>
 # endif
