c5cc82ffc6dc3d4365f756c7222f69703a99abcf
Jonathan Campbell <jonathan@castus.tv>
2018-07-06 10:10:24 -0700

    VGA attribute controller: Fix how Color Select is applied, both for standard VGA emulation and for the Tseng ET4000AX behavior used in COPPER.EXE

--- src/hardware/vga_attr.cpp
+++ src/hardware/vga_attr.cpp
@@ -80,11 +80,33 @@ void VGA_ATTR_SetPalette(Bit8u index, Bit8u val) {
         // apply the plane mask
         val = vga.attr.palette[index & vga.attr.color_plane_enable];
 
-        // replace bits 4-5 if configured, 6-7 if not 256-color mode
-        if (vga.attr.mode_control & 0x80)
-            val = (val&0xf) | (vga.attr.color_select << 4);
-        else if (!(vga.mode == M_VGA || vga.mode == M_LIN8))
-            val |= (vga.attr.color_select & 0xc) << 4;
+        // Tseng ET4000AX behavior (according to how COPPER.EXE treats the hardware):
+        // - if P54S (palette select bits 5-4) are enabled, replace bits 7-4 of the
+        //   color index with the entire color select register. COPPER.EXE line fading
+        //   tricks will not work correctly otherwise.
+        // - (guess) apply bits 3-2 of the color select register to bits 7-6 as
+        //   normally expected of VGA hardware in any video mode other than 256-color
+        //   mode.
+        if (VGA_AC_remap == AC_first16) {
+            if (vga.attr.mode_control & 0x80)
+                val = (val&0xf) | (vga.attr.color_select << 4);
+            else if (!(vga.mode == M_VGA || vga.mode == M_LIN8))
+                val |= (vga.attr.color_select & 0xc) << 4;
+        }
+        // normal VGA/SVGA behavior:
+        // - ignore color select in 256-color mode entirely
+        // - otherwise, if P54S is enabled, replace bits 5-4 with bits 1-0 of color select.
+        // - always replace bits 7-6 with bits 3-2 of color select.
+        else {
+            if (!(vga.mode == M_VGA || vga.mode == M_LIN8)) {
+                // replace bits 5-4 if P54S is enabled
+                if (vga.attr.mode_control & 0x80)
+                    val = (val&0xf) | ((vga.attr.color_select & 0x3) << 4);
+
+                // always replace bits 7-6
+                val |= (vga.attr.color_select & 0xc) << 4;
+            }
+        }
 
         // apply
         VGA_DAC_CombineColor(index,val);
