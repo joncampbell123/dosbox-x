f7760d3e1498313b07ae3e24e4f0a899b3365808
Jonathan Campbell <jonathan@castus.tv>
2018-07-06 10:32:10 -0700

    A quick modification of the AC_low4 DAC case causes COPPER.EXE to display correctly. Add reminder to self that AC_first16 may be unnecessary

--- src/hardware/vga_dac.cpp
+++ src/hardware/vga_dac.cpp
@@ -105,10 +105,18 @@ void VGA_DAC_UpdateColor( Bitu index ) {
                 case AC_low4:
                     /* Tseng ET4000 behavior, according to the SVGA card I have where only the low 4 bits are translated. --J.C. */
                     maskIndex  =  vga.dac.combine[index&0xF] & 0x0F;
-                    maskIndex +=  index & 0xF0;
+
+                    /* FIXME: TEST THIS ON THE ACTUAL ET4000. This seems to make COPPER.EXE work correctly without using AC_first16 */
+                    if (vga.attr.mode_control & 0x80)
+                        maskIndex += vga.attr.color_select << 4;
+                    else
+                        maskIndex += index & 0xF0;
+
                     maskIndex &=  vga.dac.pel_mask;
                     break;
                 case AC_first16:
+                    // TODO: It may be possible to remove AC_first16 and have only the low4 and 4x4 modes.
+                    //       The AC_low4 case above now correctly renders COPPER.EXE line-fading effects without error.
                     /* Tseng ET4000 behavior, according to the democoders that wrote COPPER.EXE (line-fading effects require this).
                      * only the first 16 colors are translated. */
                     if (index >= 0x10)
