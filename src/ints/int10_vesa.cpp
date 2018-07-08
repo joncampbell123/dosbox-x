6d06104fe32d75f70904410818fb9558ec36f4bb
Jonathan Campbell <jonathan@castus.tv>
2018-07-08 16:29:42 -0700

    more

--- src/ints/int10_vesa.cpp
+++ src/ints/int10_vesa.cpp
@@ -61,7 +61,7 @@ void CALLBACK_DeAllocate(Bitu in);
 static char string_oem[]="S3 Incorporated. Trio64";
 static char string_vendorname[]="DOSBox Development Team";
 static char string_productname[]="DOSBox - The DOS Emulator";
-static char string_productrev[]="DOSBox SVN-X";
+static char string_productrev[]="2";
 
 #ifdef _MSC_VER
 #pragma pack (1)
