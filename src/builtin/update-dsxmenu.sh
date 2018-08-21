ac873a3ec548094b015068ac6b216b2087a4b9cd
Jonathan Campbell <jonathan@castus.tv>
2018-08-20 20:32:09 -0700

    more

--- src/builtin/update-dsxmenu.sh
+++ src/builtin/update-dsxmenu.sh
@@ -5,15 +5,16 @@ filename="DSXMENU.EXE"
 bfb_sym="bfb_DSXMENU_EXE_PC"
 src_sym="_usr_src_doslib_tool_dsxmenu_dos86s_dsxmenu_exe"
 dst_sym="bin_dsxmenu_exe_pc"
+cppfile="dsxmenu_exe_pc.cpp"
 
-cat >dsxmenu_exe_pc.cpp <<_EOF
+cat >$cppfile <<_EOF
 
 #include "dos_inc.h"
 
 _EOF
-xxd -i /usr/src/doslib/tool/dsxmenu/dos86s/dsxmenu.exe | sed -e 's/unsigned char/static const unsigned char/' | sed -e 's/unsigned int/static const unsigned int/' | sed -e "s/$src_sym/$dst_sym/" | grep -v "$dst_sym"_len >>dsxmenu_exe_pc.cpp || exit 1
+xxd -i /usr/src/doslib/tool/dsxmenu/dos86s/dsxmenu.exe | sed -e 's/unsigned char/static const unsigned char/' | sed -e 's/unsigned int/static const unsigned int/' | sed -e "s/$src_sym/$dst_sym/" | grep -v "$dst_sym"_len >>$cppfile || exit 1
 
-cat >>dsxmenu_exe_pc.cpp <<_EOF
+cat >>$cppfile <<_EOF
 
 struct BuiltinFileBlob $bfb_sym = {
     /*recommended file name*/	"$filename",
