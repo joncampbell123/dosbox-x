6854ba11bc41fb3556328bb014507708b8ef6f94
Jonathan Campbell <jonathan@castus.tv>
2018-09-09 23:27:32 -0700

    Handle unhandled IRQs by mask/ack if asked to do so

--- src/ints/bios.cpp
+++ src/ints/bios.cpp
@@ -5323,11 +5323,29 @@ void gdc_16color_enable_update_vars(void) {
     }
 }
 
-static Bitu Default_IRQ_Handler(void) {
-    if (unhandled_irq_method == UNHANDLED_IRQ_MASK_ISR) {
-        /* loosely adapted from em-dosbox */
-        LOG_MSG("MASK ISR");
+void Default_IRQ_Handler_Mask_ISR(void) {
+    /* loosely adapted from em-dosbox */
+    IO_WriteB(IS_PC98_ARCH ? 0x00 : 0x20,0x0B); // ask the PIC for the ISR register
+    Bit8u master_isr = IO_ReadB(IS_PC98_ARCH ? 0x00 : 0x20);
+    if (master_isr) {
+        IO_WriteB(IS_PC98_ARCH ? 0x08 : 0xA0,0x0B); // ask the PIC for the ISR register
+        Bit8u slave_isr = IO_ReadB(IS_PC98_ARCH ? 0x08 : 0xA0);
+        if (slave_isr) {
+            IO_WriteB(IS_PC98_ARCH ? 0x0A : 0xA1,IO_ReadB(IS_PC98_ARCH ? 0x0A : 0xA1)|slave_isr);
+            IO_WriteB(IS_PC98_ARCH ? 0x08 : 0xA0,0x20);//ACK
+        }
+        else {
+            IO_WriteB(IS_PC98_ARCH ? 0x02 : 0x21,IO_ReadB(IS_PC98_ARCH ? 0x02 : 0x21)|(master_isr&(~4)));
+        }
+        IO_WriteB(IS_PC98_ARCH ? 0x00 : 0x20,0x20);//ACK
+
+        LOG_MSG("Unhandled IRQ, ISR master=0x%02x slave=0x%02x",master_isr,slave_isr);
     }
+}
+
+static Bitu Default_IRQ_Handler(void) {
+    if (unhandled_irq_method == UNHANDLED_IRQ_MASK_ISR)
+        Default_IRQ_Handler_Mask_ISR();
 
     return CBRET_NONE;
 }
@@ -5625,8 +5643,14 @@ private:
         }
 
         if (IS_PC98_ARCH) {
-            CALLBACK_Setup(call_irq07default,NULL,CB_IRET_EOI_PIC1,Real2Phys(BIOS_DEFAULT_IRQ07_DEF_LOCATION),"bios irq 0-7 default handler");
-            CALLBACK_Setup(call_irq815default,NULL,CB_IRET_EOI_PIC2,Real2Phys(BIOS_DEFAULT_IRQ815_DEF_LOCATION),"bios irq 8-15 default handler");
+            if (unhandled_irq_method == UNHANDLED_IRQ_MASK_ISR) {
+                CALLBACK_Setup(call_irq07default,Default_IRQ_Handler,CB_IRET,Real2Phys(BIOS_DEFAULT_IRQ07_DEF_LOCATION),"bios irq 0-7 default handler");
+                CALLBACK_Setup(call_irq815default,Default_IRQ_Handler,CB_IRET,Real2Phys(BIOS_DEFAULT_IRQ815_DEF_LOCATION),"bios irq 8-15 default handler");
+            }
+            else {
+                CALLBACK_Setup(call_irq07default,NULL,CB_IRET_EOI_PIC1,Real2Phys(BIOS_DEFAULT_IRQ07_DEF_LOCATION),"bios irq 0-7 default handler");
+                CALLBACK_Setup(call_irq815default,NULL,CB_IRET_EOI_PIC2,Real2Phys(BIOS_DEFAULT_IRQ815_DEF_LOCATION),"bios irq 8-15 default handler");
+            }
 
             BIOS_UnsetupKeyboard();
             BIOS_UnsetupDisks();
@@ -6910,11 +6934,17 @@ public:
 
         /* Irq 2-7 */
         call_irq07default=CALLBACK_Allocate();
-        CALLBACK_Setup(call_irq07default,NULL,CB_IRET_EOI_PIC1,Real2Phys(BIOS_DEFAULT_IRQ07_DEF_LOCATION),"bios irq 0-7 default handler");
+        if (unhandled_irq_method == UNHANDLED_IRQ_MASK_ISR)
+            CALLBACK_Setup(call_irq07default,Default_IRQ_Handler,CB_IRET,Real2Phys(BIOS_DEFAULT_IRQ07_DEF_LOCATION),"bios irq 0-7 default handler");
+        else
+            CALLBACK_Setup(call_irq07default,NULL,CB_IRET_EOI_PIC1,Real2Phys(BIOS_DEFAULT_IRQ07_DEF_LOCATION),"bios irq 0-7 default handler");
 
         /* Irq 8-15 */
         call_irq815default=CALLBACK_Allocate();
-        CALLBACK_Setup(call_irq815default,NULL,CB_IRET_EOI_PIC2,Real2Phys(BIOS_DEFAULT_IRQ815_DEF_LOCATION),"bios irq 8-15 default handler");
+        if (unhandled_irq_method == UNHANDLED_IRQ_MASK_ISR)
+            CALLBACK_Setup(call_irq815default,Default_IRQ_Handler,CB_IRET,Real2Phys(BIOS_DEFAULT_IRQ815_DEF_LOCATION),"bios irq 8-15 default handler");
+        else
+            CALLBACK_Setup(call_irq815default,NULL,CB_IRET_EOI_PIC2,Real2Phys(BIOS_DEFAULT_IRQ815_DEF_LOCATION),"bios irq 8-15 default handler");
 
         /* BIOS boot stages */
         cb_bios_post.Install(&cb_bios_post__func,CB_RETF,"BIOS POST");
