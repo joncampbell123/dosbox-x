6a6d01d10d6231c0bd687d07297f84bee195699f
Jonathan Campbell <jonathan@castus.tv>
2018-09-14 19:28:44 -0700

    Emscripten: Pause is also problematic

--- src/gui/menu.cpp
+++ src/gui/menu.cpp
@@ -649,8 +649,10 @@ static const char *def_menu_main[] = {
     "--",
     "mapper_capmouse",
 	"auto_lock_mouse",
+#if !defined(C_EMSCRIPTEN)//FIXME: Reset causes problems with Emscripten
 	"--",
 	"mapper_pause",
+#endif
 #if !defined(C_EMSCRIPTEN)//FIXME: Reset causes problems with Emscripten
     "--",
     "mapper_reset",
