514643f3749e56c62f5b1a98849baa1dffd63088
Jonathan Campbell <jonathan@castus.tv>
2018-10-19 12:08:18 -0700

    BOOT: In PC-98 mode, when unable to determine the disk type, say so

--- src/dos/dos_programs.cpp
+++ src/dos/dos_programs.cpp
@@ -1333,6 +1333,9 @@ public:
                 }
                 else {
                     // FIXME
+                    LOG_MSG("PC-98 boot: Unable to determine boot drive type for ssize=%u heads=%u cyls=%u sects=%u",
+                        ssize,heads,cyls,sects);
+
                     mem_writeb(0x584,0x00);
                     mem_writew(0x55C,disk_equip);   /* disk equipment (drive 0 is present) */
                     mem_writew(0x5AE,disk_equip_144);   /* disk equipment (drive 0 is present, 1.44MB) */
