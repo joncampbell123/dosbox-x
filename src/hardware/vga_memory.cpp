ce6c409f44aedfe50ecbf25124a57b5c1b2a0a4c
Jonathan Campbell <jonathan@castus.tv>
2018-11-08 22:32:18 -0800

    PC-98: It turns out the CG memory region at A4000-A401F, actually responds to A4000-A4FFF on real hardware. Adjust emulation to match

--- src/hardware/vga_memory.cpp
+++ src/hardware/vga_memory.cpp
@@ -1316,7 +1316,7 @@ public:
             return pc98_mem_msw((addr >> 2) & 7);
         }
 
-        /* 0xA4000-0xA401F is word-sized access to the character generator.
+        /* 0xA4000-0xA4FFF is word-sized access to the character generator.
          *
          * Some games, though not many, appear to prefer this memory-mapped I/O
          * rather than the I/O ports.
@@ -1328,12 +1328,9 @@ public:
          *             PC9821 laptop reveals that the BIOS also uses this method,
          *             using REP MOVSW
          *
-         * TODO: It's not clear if only 0xA4000-0xA401F is involved or if a wider
-         *       range is involved (meaning that the hardware latches A4000-A5FFF
-         *       to this function and then decodes only the low bits).
-         *
-         *       CHECK REAL HARDWARE to answer this question. */
-        if ((addr & (~0x1F)) == 0xA4000) {
+         * Also noted: On real hardware, A4000-A4FFF seems to latch to the CG.
+         *             A5000-A5FFF seems to latch to nothing. */
+        if ((addr & (~0xFFF)) == 0xA4000) {
             extern uint16_t a1_font_load_addr;
 
             // TODO: Does the memory address update the char offset value written to the I/O port version?
@@ -1433,7 +1430,7 @@ public:
         else
             addr &= 0x1FFFF;
 
-        /* 0xA4000-0xA401F is word-sized access to the character generator.
+        /* 0xA4000-0xA4FFF is word-sized access to the character generator.
          *
          * Some games, though not many, appear to prefer this memory-mapped I/O
          * rather than the I/O ports.
@@ -1445,12 +1442,9 @@ public:
          *             PC9821 laptop reveals that the BIOS also uses this method,
          *             using REP MOVSW
          *
-         * TODO: It's not clear if only 0xA4000-0xA401F is involved or if a wider
-         *       range is involved (meaning that the hardware latches A4000-A5FFF
-         *       to this function and then decodes only the low bits).
-         *
-         *       CHECK REAL HARDWARE to answer this question. */
-        if ((addr & (~0x1F)) == 0xA4000) {
+         * Also noted: On real hardware, A4000-A4FFF seems to latch to the CG.
+         *             A5000-A5FFF seems to latch to nothing. */
+        if ((addr & (~0xFFF)) == 0xA4000) {
             extern uint16_t a1_font_load_addr;
 
             // TODO: Does the memory address update the char offset value written to the I/O port version?
