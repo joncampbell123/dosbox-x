dcd598ea33722f20834c368370ff1f8183c69771
Jonathan Campbell <jonathan@castus.tv>
2018-11-16 11:15:12 -0800

    MPU-401: Follow recommendation by others, make MPU-401 default to IRQ 6 to match factory setting

--- src/hardware/mpu401.cpp
+++ src/hardware/mpu401.cpp
@@ -708,7 +708,7 @@ public:
 		mpu.mode=M_UART;
 
         if (IS_PC98_ARCH)
-            mpu.irq=5;  /* So far this seems to be the IRQ that games expect it to be at */
+            mpu.irq=6;  /* This is said to be the MIDI card's factory default on PC-98 */
         else
     		mpu.irq=9;	/* Princess Maker 2 wants it on irq 9 */
 
