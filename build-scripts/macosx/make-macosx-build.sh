a7479df3d37d8b2f5c565c4fc4db22f8f8b37323
Jonathan Campbell <jonathan@castus.tv>
2018-11-21 22:11:36 -0800

    more

---
+++ build-scripts/macosx/make-macosx-build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# 
+# Setup:
+#   git clone https://github.com/joncampbell123/dosbox-x dosbox-x
+#   git clone https://github.com/joncampbell123/dosbox-x dosbox-x-sdl2
+#
+# Then run this script
+name=`date +%F-%T | sed -e 's/:/-/g' | sed -e 's/-//g'`
+name="dosbox-x-macosx-x64-$name.zip"
+
+echo "Will pack to $name"
+sleep 1
+
+top=`pwd`
+cd "$top" || exit 1
+
+cd "$top/dosbox-x" || exit 1
+git clean -dfx
+git reset --hard
+./build-debug || exit 1
+make dosbox-x.app || exit 1
+cp CHANGELOG CHANGELOG.txt || exit 1
+
+cd "$top/dosbox-x-sdl2" || exit 1
+git clean -dfx
+git reset --hard
+./build-debug-sdl2 || exit 1
+make dosbox-x.app || exit 1
+cp CHANGELOG CHANGELOG.txt || exit 1
+
+cd "$top" || exit 1
+echo "Packing up now..."
+
+zip -r -9 "$name" dosbox-x/dosbox-x.app dosbox-x/CHANGELOG.txt dosbox-x-sdl2/dosbox-x.app dosbox-x-sdl2/CHANGELOG.txt || exit 1
+
+exit 0
+
