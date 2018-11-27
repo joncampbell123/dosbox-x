d466c855d6d1829c6c2d5980af2704fce7c9a066
Jonathan Campbell <jonathan@castus.tv>
2018-11-26 17:23:37 -0800

    SDL1: Show/hide mouse cursor causes strange mouse pointer warping bugs in fullscreen mode (whether or not the mouse cursor is captured), so reinstate fullscreen condition to hide the cursor

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -3399,9 +3399,13 @@ static void HandleMouseMotion(SDL_MouseMotionEvent * motion) {
         emu               = sdl.mouse.locked;
         const auto isdown = Mouse_GetButtonState() != 0;
 
+#if defined(C_SDL2)
         if (!sdl.mouse.locked)
+#else
+        /* Linux/X11 has some sort of weird mouse warping bug in fullscreen mode no matter whether the mouse is captured or not */
+        if (!sdl.mouse.locked && !sdl.desktop.fullscreen)
+#endif
             SDL_ShowCursor((isdown || inside) ? SDL_DISABLE : SDL_ENABLE);
-        /* TODO: If guest has not read mouse cursor position within 250ms show cursor again */
     }
     else if (!user_cursor_locked)
     {
@@ -3410,7 +3414,12 @@ static void HandleMouseMotion(SDL_MouseMotionEvent * motion) {
         bool MOUSE_IsHidden();
         /* Show only when DOS app is not using mouse */
 
+#if defined(C_SDL2)
         if (!sdl.mouse.locked)
+#else
+        /* Linux/X11 has some sort of weird mouse warping bug in fullscreen mode no matter whether the mouse is captured or not */
+        if (!sdl.mouse.locked && !sdl.desktop.fullscreen)
+#endif
             SDL_ShowCursor(((!inside) || ((MOUSE_IsHidden()) && !(MOUSE_IsBeingPolled() || MOUSE_HasInterruptSub()))) ? SDL_ENABLE : SDL_DISABLE);
     }
     Mouse_CursorMoved(xrel, yrel, x, y, emu);
