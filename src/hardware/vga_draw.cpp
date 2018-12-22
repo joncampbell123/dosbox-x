e0fb0aab6f004fceb6d8d6ca88b44b57d65e5ef8
Jonathan Campbell <jonathan@castus.tv>
2018-12-22 12:12:37 -0800

    Apply CGA/Hercules interlace mapping for Alt VGA emulation if compatible chain 4 OR the SVGA card is Tseng ET3000/ET4000. Emulation of those SVGA cards do not set compatible_chain4 because of the funky 256-color mode of the ET4000

--- src/hardware/vga_draw.cpp
+++ src/hardware/vga_draw.cpp
@@ -189,9 +189,12 @@ void VGA_Draw2_Recompute_CRTC_MaskAdd(void) {
         size_t new_mask = vga.mem.memmask >> (2ul + vga.config.addr_shift);
         size_t new_add = 0;
 
-        if (vga.config.compatible_chain4 || svgaCard == SVGA_None) {
+        if (vga.config.compatible_chain4 || svgaCard == SVGA_None)
             new_mask &= 0xFFFFul; /* 64KB planar (256KB linear when byte mode) */
 
+        /* CGA/Hercules compatible interlacing, unless SVGA graphics mode.
+         * Note that ET4000 and ET3000 emulation will NOT set compatible_chain4 */
+        if (vga.config.compatible_chain4 || svgaCard == SVGA_None ||svgaCard == SVGA_TsengET3K || svgaCard == SVGA_TsengET4K) {
             /* MAP13: If zero, bit 13 is taken from bit 0 of row scan counter (CGA compatible) */
             /* MAP14: If zero, bit 14 is taken from bit 1 of row scan counter (Hercules compatible) */
             if ((vga.crtc.mode_control & 3u) != 3u) {
