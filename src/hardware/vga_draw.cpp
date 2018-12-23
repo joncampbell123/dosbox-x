ef01b2118acc42d83f04c0b3a675d4edd5155706
Jonathan Campbell <jonathan@castus.tv>
2018-12-23 15:16:31 -0800

    Alt VGA 256-color 8BIT=0 mode cleanup and make into function per char clock

--- src/hardware/vga_draw.cpp
+++ src/hardware/vga_draw.cpp
@@ -583,6 +583,56 @@ static Bit8u * Alt_VGA_256color_Draw_Line(Bitu /*vidstart*/, Bitu /*line*/) {
     return TempLine;
 }
 
+#define LOAD_NEXT_PIXEL(n)  nex = pixels.b[n]
+#define SHIFTED_PIXEL       *temps++ = vga.dac.xlat32[((cur << 4u) + (nex >> 4u)) & 0xFFu]
+#define UNSHIFTED_PIXEL     *temps++ = vga.dac.xlat32[nex]; cur = nex
+
+template <const unsigned int pixelcount> static inline void Alt_VGA_256color_2x4bit_Draw_CharClock(Bit32u* &temps,const VGA_Latch &pixels,unsigned char &cur,unsigned char &nex) {
+/* NTS:
+ *   pixels == 7 for first char clock on the line
+ *   pixels == 8 for the rest of the char clocks
+ *   pixels == 1 for the char clock on the end */
+
+/* Real VGA hardware appears to have the first 8-bit pixel fully latched for
+ * the first pixel on the scanline when display enable starts. In that case,
+ * pixels == 7.
+ *
+ * After that, intermediate states are visible across the scan line,
+ * pixels == 8.
+ *
+ * The first pixel past end of active display (normally not visible), it's
+ * top nibble can be seen as the last clocked out pixel before end of
+ * active display, pixels == 1 */
+
+    if (pixelcount == 1 || pixelcount == 7) {
+        LOAD_NEXT_PIXEL(0);
+        UNSHIFTED_PIXEL;
+    }
+    else {
+        LOAD_NEXT_PIXEL(0);
+        SHIFTED_PIXEL;
+        UNSHIFTED_PIXEL;
+    }
+
+    if (pixelcount >= 7) {
+        LOAD_NEXT_PIXEL(1);
+        SHIFTED_PIXEL;
+        UNSHIFTED_PIXEL;
+
+        LOAD_NEXT_PIXEL(2);
+        SHIFTED_PIXEL;
+        UNSHIFTED_PIXEL;
+
+        LOAD_NEXT_PIXEL(3);
+        SHIFTED_PIXEL;
+        UNSHIFTED_PIXEL;
+    }
+}
+
+#undef LOAD_NEXT_PIXEL
+#undef SHIFTED_PIXEL
+#undef UNSHIFTED_PIXEL
+
 /* 256-color mode with 8BIT=0, in which the intermediate shift states are visible between
  * each 8-bit pixel, producing a weird 640x200 256-color mode.
  *
@@ -593,61 +643,27 @@ static Bit8u * Alt_VGA_256color_2x4bit_Draw_Line(Bitu /*vidstart*/, Bitu /*line*
     Bitu count = vga.draw.blocks;
     unsigned char cur,nex;
 
-#define LOAD_NEXT_PIXEL(n)  nex = pixels.b[n]
-#define SHIFTED_PIXEL       *temps++ = vga.dac.xlat32[((cur << 4u) + (nex >> 4u)) & 0xFFu]
-#define UNSHIFTED_PIXEL     *temps++ = vga.dac.xlat32[nex]; cur = nex
-
     if (count > 0u) {
         /* on VGA hardware I've seen, the first pixel is the full 8-bit pixel value of the FIRST pixel in memory. */
         const unsigned int addr = vga.draw_2[0].crtc_addr_fetch_and_advance();
         VGA_Latch pixels(*vga.draw_2[0].drawptr<Bit32u>(addr << vga.config.addr_shift));
-
-        /* NTS: Emits 7 pixels, not 8. On VGA hardware the first visible pixel is the full pixel */
-        LOAD_NEXT_PIXEL(0);
-        UNSHIFTED_PIXEL;
-
-        LOAD_NEXT_PIXEL(1);
-        SHIFTED_PIXEL;
-        UNSHIFTED_PIXEL;
-
-        LOAD_NEXT_PIXEL(2);
-        SHIFTED_PIXEL;
-        UNSHIFTED_PIXEL;
-
-        LOAD_NEXT_PIXEL(3);
-        SHIFTED_PIXEL;
-        UNSHIFTED_PIXEL;
-
+        Alt_VGA_256color_2x4bit_Draw_CharClock<7>(temps,pixels,cur,nex);
         count--;
     }
 
     while (count > 0u) {
         const unsigned int addr = vga.draw_2[0].crtc_addr_fetch_and_advance();
         VGA_Latch pixels(*vga.draw_2[0].drawptr<Bit32u>(addr << vga.config.addr_shift));
-
-        /* NTS: Emits 8 pixels, including intermediate from last loop */
-        LOAD_NEXT_PIXEL(0);
-        SHIFTED_PIXEL;
-        UNSHIFTED_PIXEL;
-
-        LOAD_NEXT_PIXEL(1);
-        SHIFTED_PIXEL;
-        UNSHIFTED_PIXEL;
-
-        LOAD_NEXT_PIXEL(2);
-        SHIFTED_PIXEL;
-        UNSHIFTED_PIXEL;
-
-        LOAD_NEXT_PIXEL(3);
-        SHIFTED_PIXEL;
-        UNSHIFTED_PIXEL;
-
+        Alt_VGA_256color_2x4bit_Draw_CharClock<8>(temps,pixels,cur,nex);
         count--;
     }
 
-#undef LOAD_NEXT_PIXEL
-#undef SHIFTED_PIXEL
-#undef UNSHIFTED_PIXEL
+    /* the top nibble of the first pixel past the end is visible on real hardware */
+    {
+        const unsigned int addr = vga.draw_2[0].crtc_addr_fetch_and_advance();
+        VGA_Latch pixels(*vga.draw_2[0].drawptr<Bit32u>(addr << vga.config.addr_shift));
+        Alt_VGA_256color_2x4bit_Draw_CharClock<1>(temps,pixels,cur,nex);
+    }
 
     return TempLine;
 }
