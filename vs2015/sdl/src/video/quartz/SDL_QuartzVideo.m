befafcdc84810dbf9382b88eae7701befb7b2e90
Jonathan Campbell <jonathan@castus.tv>
2018-12-24 01:57:07 -0800

    Revert "SDL1 Mac OS X: Nope, OpenGL demands the BGRA we had before the change"
    This reverts commit 1a9f7970997c4feb4ed619ab224872b9f07677de.

--- vs2015/sdl/src/video/quartz/SDL_QuartzVideo.m
+++ vs2015/sdl/src/video/quartz/SDL_QuartzVideo.m
@@ -1242,16 +1242,9 @@ static SDL_Surface* QZ_SetVideoModeInternal (_THIS, SDL_Surface *current,
                     bmask = 0x000000FF;
                 } else {
 #if SDL_BYTEORDER == SDL_LIL_ENDIAN
-		    if (flags & SDL_OPENGL) {
-			    rmask = 0x0000FF00;
-			    gmask = 0x00FF0000;
-			    bmask = 0xFF000000;
-		    }
-		    else {
-			    rmask = 0x000000FF;
-			    gmask = 0x0000FF00;
-			    bmask = 0x00FF0000;
-		    }
+                    rmask = 0x000000FF;
+                    gmask = 0x0000FF00;
+                    bmask = 0x00FF0000;
 #else
                     rmask = 0x00FF0000;
                     gmask = 0x0000FF00;
