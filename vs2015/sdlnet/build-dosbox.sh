102289b4dc31789c8c6bd379df51e382a439e9d4
Jonathan Campbell <jonathan@castus.tv>
2019-02-09 23:48:23 -0800

    more

---
+++ vs2015/sdlnet/build-dosbox.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+rm -Rfv linux-host || exit 1
+mkdir -p linux-host || exit 1
+
+rm -Rfv linux-build || exit 1
+mkdir -p linux-build || exit 1
+
+chmod +x configure || exit 1
+
+srcdir="`pwd`"
+instdir="`pwd`/linux-host"
+
+cd linux-build || exit 1
+
+opts=
+
+sys=`uname -s`
+
+../configure "--srcdir=$srcdir" "--prefix=$instdir" --enable-static --disable-shared $opts || exit 1
+
+# SDL is having concurrency problems with Brew compiles, help it out
+# https://jenkins.brew.sh/job/Homebrew%20Core%20Pull%20Requests/35627/version=sierra/testReport/junit/brew-test-bot/sierra/install_dosbox_x/
+mkdir -p linux-host || exit 1
+mkdir -p linux-build || exit 1
+mkdir -p linux-build/build || exit 1
+mkdir -p linux-build/include || exit 1
+
+make -j || exit 1
+make install || exit 1  # will install into ./linux-host
+
