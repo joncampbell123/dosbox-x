5b4cfa6ccff327a1010854b2580e7936299fdcc5
Jonathan Campbell <jonathan@castus.tv>
2019-02-13 09:38:41 -0800

    Move out vector2

--- src/gui/sdl_mapper.cpp
+++ src/gui/sdl_mapper.cpp
@@ -105,45 +105,6 @@ bool                                    mapper_addhandler_create_buttons = false
 
 bool                                    isJPkeyboard = false;
 
-//! \brief Floating-point vector with 2 components.
-struct DOSBox_Vector2
-{
-	float X, Y;
-
-	DOSBox_Vector2(const float x, const float y) : X(x), Y(y) { }
-	DOSBox_Vector2() : X(0.0f), Y(0.0f) { }
-
-	DOSBox_Vector2 clamp(const DOSBox_Vector2 min, const DOSBox_Vector2 max) const {
-		float x = this->X;
-		float y = this->Y;
-		float xmin = min.X;
-		float xmax = max.X;
-		float ymin = min.Y;
-		float ymax = max.Y;
-		x = x < xmin ? xmin : x > xmax ? xmax : x;
-		y = y < ymin ? ymin : y > ymax ? ymax : y;
-		DOSBox_Vector2 clamp = DOSBox_Vector2(x, y);
-		return clamp;
-	}
-
-	inline float magnitude(void) const {
-		return sqrt(sqrMagnitude());
-	}
-
-	inline float sqrMagnitude(void) const {
-		return X * X + Y * Y;
-	}
-
-	DOSBox_Vector2 normalized(void) const {
-		float m = this->magnitude();
-		return m > 0.0f ? DOSBox_Vector2(this->X / m, this->Y / m) : DOSBox_Vector2();
-	}
-
-	DOSBox_Vector2 operator*(const float f) const {
-		return DOSBox_Vector2(this->X * f, this->Y * f);
-	}
-};
-
 class CEvent;
 class CHandlerEvent;
 class CButton;
