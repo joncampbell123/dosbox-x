0f5ec99b988f0c3fe39239727199391055e25afe
Jonathan Campbell <jonathan@castus.tv>
2019-02-16 13:21:49 -0800

    Keyboard layout determination on Linux requires XKBlib and XKBrules.h

--- src/gui/sdlmain_linux.cpp
+++ src/gui/sdlmain_linux.cpp
@@ -264,6 +264,7 @@ unsigned int Linux_GetKeyboardLayout(void) {
     if (SDL_GetWMInfo(&wminfo) >= 0) {
 # if C_X11
 #  if C_X11_XKB
+#   if C_X11_EXT_XKBRULES
         if (wminfo.subsystem == SDL_SYSWM_X11 && wminfo.info.x11.display != NULL) {
             XkbRF_VarDefsRec vd = {0};
             XkbStateRec state = {0};
@@ -300,6 +301,7 @@ unsigned int Linux_GetKeyboardLayout(void) {
             if (group) XFree(group);
             if (desc) XFree(desc);
         }
+#   endif
 #  endif
 # endif
     }
