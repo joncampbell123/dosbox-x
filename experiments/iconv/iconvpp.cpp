1fcd87e13feafcf8ed930809978ec114a416395c
Jonathan Campbell <jonathan@castus.tv>
2019-03-17 18:32:30 -0700

    more

--- experiments/iconv/iconvpp.cpp
+++ experiments/iconv/iconvpp.cpp
@@ -18,7 +18,7 @@ public:
         close();
     }
 public:
-    void cancel_conversion(void) {
+    void finish(void) {
         dst_adv = 0;
         dst_ptr = NULL;
         dst_ptr_fence = NULL;
@@ -95,6 +95,10 @@ public:
         }
 
         return err_noinit;
+    }
+    int cstring_convert(const char *src) {
+        set_src(src);
+        return cstring_convert();
     }
     int cstring_convert(void) {
         int err = raw_convert();
@@ -185,6 +189,29 @@ int main() {
         cout << "   Res: " << tmp << endl;
         cout << "  Read: " << x->get_src_last_read() << endl;
         cout << " Wrote: " << x->get_dest_last_written() << endl;
+
+        x->finish();
+    }
+
+    {
+        char tmp[512];
+        const char *src = "\xC8\xC9\xCA Hello world \xC0\xC1\xC2";
+
+        x->set_dest(tmp,sizeof(tmp));
+
+        int err = x->cstring_convert(src);
+
+        if (err < 0) {
+            cerr << "Conversion failed, " << Iconv::errstring(err) << endl;
+            return 1;
+        }
+
+        cout << "Test 1: " << src << endl;
+        cout << "   Res: " << tmp << endl;
+        cout << "  Read: " << x->get_src_last_read() << endl;
+        cout << " Wrote: " << x->get_dest_last_written() << endl;
+
+        x->finish();
     }
 
     delete x;
