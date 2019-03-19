4f542833120434c960973961544875095dc39fc3
Jonathan Campbell <jonathan@castus.tv>
2019-03-18 18:36:51 -0700

    more

--- experiments/iconv/iconvpp.hpp
+++ experiments/iconv/iconvpp.hpp
@@ -10,24 +10,46 @@
 
 #include <iconv.h>
 
-class Iconv {
+template <typename srcT,typename dstT> class _Iconv {
 public:
-    explicit Iconv(const iconv_t &ctx); /* takes ownership of ctx */
-    Iconv(const Iconv *p) = delete;
-    Iconv(const Iconv &other) = delete; /* no copying */
-    Iconv(const Iconv &&other) = delete; /* no moving */
-    Iconv() = delete;
-    ~Iconv();
+    explicit _Iconv(const iconv_t &ctx) : context(ctx) {/* takes ownership of ctx */
+    }
+    _Iconv(const _Iconv *p) = delete;
+    _Iconv(const _Iconv &other) = delete; /* no copying */
+    _Iconv(const _Iconv &&other) = delete; /* no moving */
+    _Iconv() = delete;
+    ~_Iconv() {
+        close();
+    }
 public:
-    void finish(void);
+    void finish(void) {
+        dst_ptr = NULL;
+        dst_ptr_fence = NULL;
+        src_ptr = NULL;
+        src_ptr_fence = NULL;
+    }
 
-    void set_dest(char * const dst,char * const dst_fence);
+    void set_dest(char * const dst,char * const dst_fence) {
+        if (dst == NULL || dst_fence == NULL || dst > dst_fence)
+            throw std::invalid_argument("Iconv set_dest pointer out of range");
+
+        dst_adv = 0;
+        dst_ptr = dst;
+        dst_ptr_fence = dst_fence;
+    }
     void set_dest(char * const dst,const size_t len) {
         set_dest(dst,dst+len);
     }
     void set_dest(char * const dst) = delete; /* <- NO! Prevent C-string calls to std::string &dst function! */
 
-    void set_src(const char * const src,const char * const src_fence);
+    void set_src(const char * const src,const char * const src_fence) {
+        if (src == NULL || src_fence == NULL || src > src_fence)
+            throw std::invalid_argument("Iconv set_src pointer out of range");
+
+        src_adv = 0;
+        src_ptr = src;
+        src_ptr_fence = src_fence;
+    }
     void set_src(const char * const src,const size_t len) {
         set_src(src,src+len);
     }
@@ -35,8 +57,61 @@ public:
         set_src(src,strlen(src));
     }
 public:
-    int string_convert(std::string &dst,const std::string &src);
-    int string_convert(void);
+    int string_convert(std::string &dst,const std::string &src) {
+        dst.resize(std::max(dst.size(),((src.length()+4u)*4u)+2u)); // maximum 4 bytes/char expansion UTF-8 or bigger if caller resized already
+        set_dest(dst); /* will apply new size to dst/fence pointers */
+
+        int err = string_convert_src(src);
+
+        dst.resize(get_dest_last_written());
+
+        finish();
+        return err;
+    }
+    int string_convert(void) {
+        if (context != notalloc) {
+            if (dst_ptr == NULL || src_ptr == NULL)
+                return err_notvalid;
+            if (dst_ptr > dst_ptr_fence)
+                return err_notvalid;
+            if (src_ptr > src_ptr_fence)
+                return err_notvalid;
+
+            char *i_dst = dst_ptr;
+            const char *i_src = src_ptr;
+            size_t src_left = (size_t)((uintptr_t)src_ptr_fence - (uintptr_t)src_ptr);
+            size_t dst_left = (size_t)((uintptr_t)dst_ptr_fence - (uintptr_t)dst_ptr);
+
+            iconv(context,NULL,NULL,NULL,NULL);
+
+            int ret = iconv(context,(char**)(&src_ptr),&src_left,&dst_ptr,&dst_left);
+
+            src_adv = (size_t)(src_ptr - i_src);
+            dst_adv = (size_t)(dst_ptr - i_dst);
+
+            if (ret < 0) {
+                if (errno == E2BIG)
+                    return err_noroom;
+                else if (errno == EILSEQ)
+                    return err_notvalid;
+                else if (errno == EINVAL)
+                    return err_incomplete;
+
+                return err_notvalid;
+            }
+            else {
+                /* add NUL */
+                if (dst_ptr >= dst_ptr_fence)
+                    return err_noroom;
+
+                *dst_ptr++ = 0;
+            }
+
+            return ret;
+        }
+
+        return err_noinit;
+    }
     int string_convert_dest(std::string &dst) {
         size_t srcl = (size_t)((uintptr_t)src_ptr_fence - (uintptr_t)src_ptr);
 
@@ -83,10 +158,32 @@ public:
         return dst_adv;
     }
 public:
-    static const char *errstring(int x);
-    static Iconv *create(const char *to,const char *from);
+    static const char *errstring(int x) {
+        if (x >= 0)
+            return "no error";
+        else if (x == err_noinit)
+            return "not initialized";
+        else if (x == err_noroom)
+            return "out of room";
+        else if (x == err_notvalid)
+            return "illegal multibyte sequence or invalid state";
+        else if (x == err_incomplete)
+            return "incomplete multibyte sequence";
+
+        return "?";
+    }
+    static _Iconv<srcT,dstT> *create(const char *to,const char *from) { /* factory function */
+        iconv_t ctx = iconv_open(to,from);
+        if (ctx != notalloc) return new(std::nothrow) _Iconv<srcT,dstT>(ctx);
+        return NULL;
+    }
 private:
-    void close(void);
+    void close(void) {
+        if (context != notalloc) {
+            iconv_close(context);
+            context = notalloc;
+        }
+    }
 private:
     void set_dest(std::string &dst) { /* <- use string::resize() first before calling this */
         /* this is PRIVATE to avoid future bugs and problems where set_dest() is given a std::string
@@ -126,4 +223,6 @@ private:
     size_t                      src_adv = 0;
     iconv_t                     context = notalloc;
 };
+
+typedef _Iconv<char,char> Iconv;
 
