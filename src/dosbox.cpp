1855a783b0f2466e2fc7aea098ddc36469e01765
Jonathan Campbell <jonathan@castus.tv>
2019-04-10 16:20:38 -0700

    remove auto cpu type entirely

--- src/dosbox.cpp
+++ src/dosbox.cpp
@@ -800,7 +800,7 @@ void DOSBOX_SetupConfigSections(void) {
     const char* capturechromaformats[] = { "auto", "4:4:4", "4:2:2", "4:2:0", 0};
     const char* controllertypes[] = { "auto", "at", "xt", "pcjr", "pc98", 0}; // Future work: Tandy(?) and USB
     const char* auxdevices[] = {"none","2button","3button","intellimouse","intellimouse45",0};
-    const char* cputype_values[] = {"auto", "8086", "8086_prefetch", "80186", "80186_prefetch", "286", "286_prefetch", "386", "386_prefetch", "486old", "486old_prefetch", "486", "486_prefetch", "pentium", "pentium_mmx", "ppro_slow", 0};
+    const char* cputype_values[] = {"8086", "8086_prefetch", "80186", "80186_prefetch", "286", "286_prefetch", "386", "386_prefetch", "486old", "486old_prefetch", "486", "486_prefetch", "pentium", "pentium_mmx", "ppro_slow", 0};
     const char* rates[] = {  "44100", "48000", "32000","22050", "16000", "11025", "8000", "49716", 0 };
     const char* oplrates[] = {   "44100", "49716", "48000", "32000","22050", "16000", "11025", "8000", 0 };
     const char* devices[] = { "default", "win32", "alsa", "oss", "coreaudio", "coremidi", "mt32", "synth", "timidity", "none", 0}; // FIXME: add some way to offer the actually available choices.
@@ -1692,9 +1692,9 @@ void DOSBOX_SetupConfigSections(void) {
             "also causes problems with 32-bit protected mode DOS games and reduces the performance\n"
             "of the dynamic core.\n");
 
-    Pstring = secprop->Add_string("cputype",Property::Changeable::Always,"auto");
+    Pstring = secprop->Add_string("cputype",Property::Changeable::Always,"pentium");
     Pstring->Set_values(cputype_values);
-    Pstring->Set_help("CPU Type used in emulation. auto emulates a 486 which tolerates Pentium instructions.");
+    Pstring->Set_help("CPU Type used in emulation");
 
     Pmulti_remain = secprop->Add_multiremain("cycles",Property::Changeable::Always," ");
     Pmulti_remain->Set_help(
