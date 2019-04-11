3713ad04ff9c72df3110675f06ef835c7ba05232
Jonathan Campbell <jonathan@castus.tv>
2019-04-10 20:06:34 -0700

    Fix normal core indicator

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -208,7 +208,9 @@ void menu_update_core(void) {
     allow_dynamic = (strstr(cpu_sec_type.c_str(),"_prefetch") == NULL);
 
     mainMenu.get_item("mapper_normal").
-        check(cpudecoder == &CPU_Core_Normal_Run || cpudecoder == &CPU_Core_Prefetch_Run).
+        check(cpudecoder == &CPU_Core_Normal_Run ||
+              cpudecoder == &CPU_Core_Prefetch_Run ||
+              cpudecoder == &CPU_Core286_Prefetch_Run).
         refresh_item(mainMenu);
 }
 
