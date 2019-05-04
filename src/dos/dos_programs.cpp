cee20e97963659424350d8798016c7d257590ee1
Allofich <Allofich@users.noreply.github.com>
2019-05-04 17:00:52 +0900

    SVN r3911
    Introduce mount -pr to mount paths relative to last loaded configuration file.

--- src/dos/dos_programs.cpp
+++ src/dos/dos_programs.cpp
@@ -178,6 +178,8 @@ public:
             WriteOut(MSG_Get("PROGRAM_CONFIG_SECURE_DISALLOW"));
             return;
         }
+        bool path_relative_to_last_config = false;
+        if (cmd->FindExist("-pr",true)) path_relative_to_last_config = true;
 
         if (cmd->FindExist("-q",false))
             quiet = true;
@@ -335,11 +337,18 @@ public:
 
             if (!cmd->FindCommand(2,temp_line)) goto showusage;
             if (!temp_line.size()) goto showusage;
+            if(path_relative_to_last_config && control->configfiles.size() && !Cross::IsPathAbsolute(temp_line)) {
+		        std::string lastconfigdir(control->configfiles[control->configfiles.size()-1]);
+                std::string::size_type pos = lastconfigdir.rfind(CROSS_FILESPLIT);
+                if(pos == std::string::npos) pos = 0; //No directory then erase string
+                lastconfigdir.erase(pos);
+                if (lastconfigdir.length())	temp_line = lastconfigdir + CROSS_FILESPLIT + temp_line;
+            }
             bool is_physfs = temp_line.find(':',((temp_line[0]|0x20) >= 'a' && (temp_line[0]|0x20) <= 'z')?2:0) != std::string::npos;
             struct stat test;
             //Win32 : strip tailing backslashes
             //os2: some special drive check
-            //rest: substiture ~ for home
+            //rest: substitute ~ for home
             bool failed = false;
 
             (void)failed;// MAY BE UNUSED
