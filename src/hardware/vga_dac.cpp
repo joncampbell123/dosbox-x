4f5d8b32940590a43ba553f6a47ab1e6a2ba5033
Jonathan Campbell <jonathan@castus.tv>
2019-05-17 22:24:52 -0700

    Follow up Allofich SVN patch with one that restores noted Paradise DAC behavior if machine=svga_paradise

--- src/hardware/vga_dac.cpp
+++ src/hardware/vga_dac.cpp
@@ -224,7 +224,8 @@ void write_p3c8(Bitu port,Bitu val,Bitu iolen) {
     vga.dac.pel_index=0;
     vga.dac.state=DAC_WRITE;
     vga.dac.write_index=val;        /* NTS: Paradise SVGA behavior, this affects write index, but not read index */
-    vga.dac.read_index= val - 1;
+    if (svgaCard != SVGA_ParadisePVGA1A)
+        vga.dac.read_index = val - 1;
 }
 
 Bitu read_p3c8(Bitu port, Bitu iolen){
