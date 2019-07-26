ebb804da4b8f4e3cc8d77ac055323e95eb034417
Jonathan Campbell <jonathan@castus.tv>
2019-07-26 12:23:50 -0700

    Fill out FM Towns stub by tying it into VGA emulation for now, with reminder to self that it is a hack/stub to be removed later

--- src/ints/bios.cpp
+++ src/ints/bios.cpp
@@ -8193,6 +8193,9 @@ private:
                 case MCH_AMSTRAD:
                     card = "Amstrad graphics";
                     break;
+                case MCH_FM_TOWNS:
+                    card = "FM Towns (STUB)";
+                    break;
                 default:
                     abort(); // should not happen
                     break;
