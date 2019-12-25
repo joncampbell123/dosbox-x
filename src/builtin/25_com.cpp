6faeca5a39c1c5f545e9d87467b7756e725ec98a
Jonathan Campbell <jonathan@castus.tv>
2019-12-25 01:46:41 -0800

    Change 25.COM to use INT 10h AX=1114h (set VGA 8x16 font) instead of INT AX=3

--- src/builtin/25_com.cpp
+++ src/builtin/25_com.cpp
@@ -1,7 +1,7 @@
 
 #include "dos_inc.h"
 
-static const unsigned char bin_25_com[] = { 0xB8, 0x03, 0x00, 0xCD, 0x10, 0xC3 };
+static const unsigned char bin_25_com[] = { 0xB8, 0x14, 0x11, 0x30, 0xDB, 0xCD, 0x10, 0xC3 };
 
 struct BuiltinFileBlob bfb_25_COM = {
 	/*recommended file name*/	"25.COM",
