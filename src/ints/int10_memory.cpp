82e7154757a1a195df511b366a3615cfe797759f
Jonathan Campbell <jonathan@castus.tv>
2019-12-25 01:37:52 -0800

    INT 10h font setting: When changing the number of rows, clip the cursor row to within range. This keeps the cursor in range when calling INT 10h AX=1111h (set 8x14 font) and AX=1113h (set 8x8 font). Issue #1324 and #1325.

--- src/ints/int10_memory.cpp
+++ src/ints/int10_memory.cpp
@@ -117,6 +117,19 @@ void INT10_LoadFont(PhysPt font,bool reload,Bit16u count,Bitu offset,Bitu map,Bi
 		//Cursor shape
 		if (height>=14) height--; // move up one line on 14+ line fonts
 		INT10_SetCursorShape(height-2,height-1);
+        //Clip the cursor within range in case it is now beyond the new height. To fix 28.COM and 50.COM (issue #1324 and #1325).
+        //FIXME: Remove this code *IF* it turns out BIOSes do not clip the row value when calling INT 10h AX=1111h/1113h
+        //       If BIOSes are inconsistent about it, note which ones do and don't and try to match behavior with machine= type.
+        //       At the very least, if the IBM PS/2 VGA BIOS does not, then this code should be coded NOT to clip the cursor
+        //       when machine=vgaonly.
+        {
+            Bit8u page=real_readb(BIOSMEM_SEG,BIOSMEM_CURRENT_PAGE);
+            Bit8u cur_row=CURSOR_POS_ROW(page);
+            Bit8u cur_col=CURSOR_POS_COL(page);
+
+            if (cur_row >= rows)
+                INT10_SetCursorPos(rows-1,cur_col,page);
+        }
 	}
 }
 
