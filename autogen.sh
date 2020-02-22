c75ba4bb378c38fb5c7f8def886b30e4039e3f01
Michiel Sikma <michiel@sikma.org>
2020-02-22 16:36:52 +0100

    Turn e back on afterwards, and only echo the error message if the script actually exited prematurely

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-echo "Generating build information using aclocal, autoheader, automake and autoconf"
-echo "This may take a while ..."
-
 # If an error occurs, quit the script and inform the user. This ensures scripts
 # like ./build-macosx etc. don't continue on if Autotools isn't installed.
 function finish {
-  echo 'autogen.sh failed to complete: verify that GNU Autotools is installed on the system and try again'
+  if [ $success -eq 0 ]; then
+    echo 'autogen.sh failed to complete: verify that GNU Autotools is installed on the system and try again'
+  fi
 }
+
+success=0
 trap finish EXIT
 set -e
+
+echo "Generating build information using aclocal, autoheader, automake and autoconf"
+echo "This may take a while ..."
 
 # Regenerate configuration files.
 
@@ -20,3 +24,7 @@ autoconf
 
 echo "Now you are ready to run ./configure."
 echo "You can also run  ./configure --help for extra features to enable/disable."
+
+# Don't quit on errors again from here on out (for calling scripts).
+set +e
+success=1
