7bef93d470dd6c79c0442826aae028add34b4483
Michiel Sikma <michiel@sikma.org>
2020-02-22 16:13:48 +0100

    Ensure that Autotools is installed, error with help message otherwise

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,14 @@
 echo "Generating build information using aclocal, autoheader, automake and autoconf"
 echo "This may take a while ..."
 
+# If an error occurs, quit the script and inform the user. This ensures scripts
+# like ./build-macosx etc. don't continue on if Autotools isn't installed.
+function finish {
+  echo 'autogen.sh failed to complete: verify that GNU Autotools is installed on the system and try again'
+}
+trap finish EXIT
+set -e
+
 # Regenerate configuration files.
 
 aclocal
