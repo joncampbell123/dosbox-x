758fb287a6f95096217b71753156dfb92fbc2227
Jonathan Campbell <jonathan@castus.tv>
2020-03-22 20:31:49 -0700

    Increase sname to 15 chars, just in case

--- src/hardware/hardware.cpp
+++ src/hardware/hardware.cpp
@@ -514,7 +514,7 @@ std::string GetCaptureFilePath(const char * type,const char * ext) {
 	lowcase(file_start);
 	strcat(file_start,"_");
 	bool is_directory;
-    char tempname[CROSS_LEN], sname[12];
+    char tempname[CROSS_LEN], sname[15];
     bool testRead = read_directory_first(dir, tempname, sname, is_directory );
     for ( ; testRead; testRead = read_directory_next(dir, tempname, sname, is_directory) ) {
 		char * test=strstr(tempname,ext);
@@ -555,7 +555,7 @@ FILE * OpenCaptureFile(const char * type,const char * ext) {
 	lowcase(file_start);
 	strcat(file_start,"_");
 	bool is_directory;
-    char tempname[CROSS_LEN], sname[12];
+    char tempname[CROSS_LEN], sname[15];
     bool testRead = read_directory_first(dir, tempname, sname, is_directory );
     for ( ; testRead; testRead = read_directory_next(dir, tempname, sname, is_directory) ) {
 		char * test=strstr(tempname,ext);
