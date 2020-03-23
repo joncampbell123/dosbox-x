bee97a7122efcd7877fdadcba8dcff44d47d47d8
Jonathan Campbell <jonathan@castus.tv>
2020-03-22 20:30:57 -0700

    Fix OpenCaptureFile to detect sequences properly again. LFN patch seems to have broken it

--- src/hardware/hardware.cpp
+++ src/hardware/hardware.cpp
@@ -555,10 +555,9 @@ FILE * OpenCaptureFile(const char * type,const char * ext) {
 	lowcase(file_start);
 	strcat(file_start,"_");
 	bool is_directory;
-	char tempname[CROSS_LEN];
-    bool testRead = read_directory_first(dir, tempname, tempname, is_directory );
-    for ( ; testRead; testRead = read_directory_next(dir, tempname, tempname, is_directory) ) {
-        // didn't adapt this for long file names
+    char tempname[CROSS_LEN], sname[12];
+    bool testRead = read_directory_first(dir, tempname, sname, is_directory );
+    for ( ; testRead; testRead = read_directory_next(dir, tempname, sname, is_directory) ) {
 		char * test=strstr(tempname,ext);
 		if (!test || strlen(test)!=strlen(ext)) 
 			continue;
