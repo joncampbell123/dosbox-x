f490e72b7cf6e7d39f7d230e8e0ba9e5b7a8cadd
Jonathan Campbell <jonathan@castus.tv>
2020-04-18 02:50:33 -0700

    Fix Tandy 3-voice emulation to respond to 0xC0 for both 8-bit and 16-bit I/O because Prince of Persia likes to use 16-bit I/O for frequency values

--- src/hardware/tandy_sound.cpp
+++ src/hardware/tandy_sound.cpp
@@ -289,6 +289,14 @@ public:
 		if (machine == MCH_PCJR) activeDevice = &device_sn76496;
 		else activeDevice = &device_ncr8496;
 
+		/* ports from second DMA controller conflict with tandy ports at 0xC0.
+		 * Furthermore, the default I/O handlers after de-registration are needed
+		 * to ensure the SN76496 is writeable at port 0xC0 whether you're doing
+		 * normal 8-bit I/O or your a weirdo like Prince of Persia using 16-bit
+		 * I/O to write frequency values. (bugfix for Tandy mode of Prince of
+		 * Persia). */
+		CloseSecondDMAController();
+
 		if (IS_TANDY_ARCH) {
 			/* enable tandy sound if tandy=true/auto */
 			if ((strcmp(section->Get_string("tandy"),"true")!=0) &&
@@ -298,9 +306,6 @@ public:
 			/* only enable tandy sound if tandy=true */
 			if ((strcmp(section->Get_string("tandy"),"true")!=0) &&
 				(strcmp(section->Get_string("tandy"),"on")!=0)) return;
-
-			/* ports from second DMA controller conflict with tandy ports */
-			CloseSecondDMAController();
 
 			if (enable_hw_tandy_dac) {
 				WriteHandler[2].Install(0x1e0,SN76496Write,IO_MB,2);
