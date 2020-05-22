d23e01cb5f479ba0e783712e8062f6d0cc7fca97
Wengier <wengierwu@yahoo.com>
2020-05-22 17:58:43 -0400

    support REM, BREAK and LASTDRIVE in [config]

--- src/dos/dos_classes.cpp
+++ src/dos/dos_classes.cpp
@@ -24,6 +24,7 @@
 #include "dos_inc.h"
 #include "support.h"
 #include "drives.h"
+#include "control.h"
 
 Bit8u sattr[260], fattr;
 char sname[260][LFN_NAMELENGTH+1],storect[CTBUF];
@@ -73,6 +74,17 @@ void DOS_InfoBlock::SetLocation(Bit16u segment) {
 	/* Clear the initial Block */
 	for(Bit8u i=0;i<sizeof(sDIB);i++) mem_writeb(pt+i,0xff);
 	for(Bit8u i=0;i<14;i++) mem_writeb(pt+i,0);
+	
+	Bit8u drives=1;
+	Section_prop *section = static_cast<Section_prop *>(control->GetSection("config"));
+	if (section !=NULL) {
+		char *lastdrive = (char *)section->Get_string("LASTDRIVE");
+		if (strlen(lastdrive)==1&&lastdrive[0]>='a'&&lastdrive[0]<='z')
+			drives=lastdrive[0]-'a'+1;
+		char *dosbreak = (char *)section->Get_string("BREAK");
+		if (!strcasecmp(dosbreak, "on"))
+			dos.breakcheck=true;
+	}
 
 	sSave(sDIB,regCXfrom5e,(Bit16u)0);
 	sSave(sDIB,countLRUcache,(Bit16u)0);
@@ -81,7 +93,7 @@ void DOS_InfoBlock::SetLocation(Bit16u segment) {
 	sSave(sDIB,protFCBs,(Bit16u)0);
 	sSave(sDIB,specialCodeSeg,(Bit16u)0);
 	sSave(sDIB,joindedDrives,(Bit8u)0);
-	sSave(sDIB,lastdrive,(Bit8u)0x01);//increase this if you add drives to cds-chain
+	sSave(sDIB,lastdrive,(Bit8u)drives);//increase this if you add drives to cds-chain
 
 	sSave(sDIB,diskInfoBuffer,RealMake(segment,offsetof(sDIB,diskBufferHeadPt)));
 	sSave(sDIB,setverPtr,(Bit32u)0);
