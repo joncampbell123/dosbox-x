8150136637aba50b0e289f1bbb1f32a738b472c7
Jonathan Campbell <jonathan@castus.tv>
2020-05-29 03:31:12 -0700

    Remove duplicate #define safe_strncpy from support.h

--- include/support.h
+++ include/support.h
@@ -107,6 +107,4 @@ char * safe_strcat(char (& dst)[N], const char * src) noexcept {
         return & dst[0];
 }
 
-#define safe_strncpy(a,b,n) do { strncpy((a),(b),(n)-1); (a)[(n)-1] = 0; } while (0)
-
 #endif
