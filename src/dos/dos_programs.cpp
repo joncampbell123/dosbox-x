fc657b7385adf6ccf806142b47019e1c0933a472
Wengier <wengierwu@yahoo.com>
2020-06-20 19:56:34 -0400

    Add copyright header

--- src/dos/dos_programs.cpp
+++ src/dos/dos_programs.cpp
@@ -14,6 +14,9 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA.
+ *
+ *  Heavy improvements by the DOSBox-X Team - joncampbell123, Wengier, rderooy
+ *  AUTOTYPE Copyright (C) 2020 The dosbox-staging team
  */
 
 
