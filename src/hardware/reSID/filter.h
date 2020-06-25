234a9996fcb8f74eda601cec15ea42db0040bb2f
rderooy <robert.de.rooy@gmail.com>
2020-06-25 09:41:35 +0200

    Update FSF address

--- src/hardware/reSID/filter.h
+++ src/hardware/reSID/filter.h
@@ -12,9 +12,9 @@
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335 USA  USA
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 //  ---------------------------------------------------------------------------
 
 #ifndef __FILTER_H__
