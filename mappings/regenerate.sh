186c07a3eacd934c094d2616c3a497ad29fd3293
Wengier <wengierwu@yahoo.com>
2020-07-29 18:18:20 -0400

    CP 872

--- mappings/regenerate.sh
+++ mappings/regenerate.sh
@@ -5,8 +5,8 @@
 ./sb2u.pl cp855_to_unicode <CP855.TXT >../include/cp855_uni.h || exit 1
 ./sb2u.pl cp857_to_unicode <CP857.TXT >../include/cp857_uni.h || exit 1
 ./sb2u.pl cp858_to_unicode <CP858.TXT >../include/cp858_uni.h || exit 1
-./sb2u.pl cp863_to_unicode <CP863.TXT >../include/cp863_uni.h || exit 1
 ./sb2u.pl cp866_to_unicode <CP866.TXT >../include/cp866_uni.h || exit 1
 ./sb2u.pl cp869_to_unicode <CP869.TXT >../include/cp869_uni.h || exit 1
+./sb2u.pl cp872_to_unicode <CP872.TXT >../include/cp872_uni.h || exit 1
 ./sjis2u.pl cp932_to_unicode <CP932.TXT >../include/cp932_uni.h || exit 1
 
