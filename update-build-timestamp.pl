f4f8df92a7af080e5a3a308d10e8f33d004b477c
Wengier <wengierwu@yahoo.com>
2020-08-15 17:15:45 -0400

    VER /R to show the Git commit SHA1

--- update-build-timestamp.pl
+++ update-build-timestamp.pl
@@ -12,9 +12,13 @@ my @months = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
 
 my $tmp = sprintf("%s %u, %u %u:%02u:%02u%s",$months[$mon-1],$mday,$year,(($hour + 11) % 12) + 1,$min,$sec,$hour >= 12 ? "pm" : "am");
 
+my $commit = `git rev-parse --short HEAD`;
+chomp($commit);
+
 open(X,">","include/build_timestamp.h") || die;
 print X "/*auto-generated*/\n";
 print X "#define UPDATED_STR \"$tmp\"\n";
+print X "#define GIT_COMMIT_HASH \"$commit\"\n";
 print X "#define COPYRIGHT_END_YEAR \"$year\"\n";
 close(X);
 
