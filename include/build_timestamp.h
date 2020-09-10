e88ec27e7393c87c62d0cc3ca08f7e5cf85068b4
Wengier <wengierwu@yahoo.com>
2020-09-10 12:57:40 -0400

    update ref and timestamp

--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,4 +1,4 @@
 /*auto-generated*/
-#define UPDATED_STR "Sep 8, 2020 3:01:07pm"
-#define GIT_COMMIT_HASH "8c8a334"
+#define UPDATED_STR "Sep 10, 2020 12:52:49pm"
+#define GIT_COMMIT_HASH "ee03f93"
 #define COPYRIGHT_END_YEAR "2020"
