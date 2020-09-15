db4683443cf2d653c1fb65b97ed7d441b908f605
Wengier <wengierwu@yahoo.com>
2020-09-15 14:59:41 -0400

    update conf and timestamp

--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,4 +1,4 @@
 /*auto-generated*/
-#define UPDATED_STR "Sep 13, 2020 12:08:40pm"
-#define GIT_COMMIT_HASH "46f829b"
+#define UPDATED_STR "Sep 15, 2020 2:51:42pm"
+#define GIT_COMMIT_HASH "ec85c68"
 #define COPYRIGHT_END_YEAR "2020"
