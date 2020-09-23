9cfd8554c7aaaecb99343eb34c317c00f723cbd8
Wengier <wengierwu@yahoo.com>
2020-09-23 16:41:00 -0400

    update info

--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,4 +1,4 @@
 /*auto-generated*/
-#define UPDATED_STR "Sep 22, 2020 5:41:29pm"
-#define GIT_COMMIT_HASH "80fd23a"
+#define UPDATED_STR "Sep 23, 2020 4:39:12pm"
+#define GIT_COMMIT_HASH "4c3432c"
 #define COPYRIGHT_END_YEAR "2020"
