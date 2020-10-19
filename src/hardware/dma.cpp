fbee4009b34cb3cb5b98c3cc41f42a9c2069eba9
Wengier <wengierwu@yahoo.com>
2020-10-18 20:24:06 -0400

    diagnose

--- src/hardware/dma.cpp
+++ src/hardware/dma.cpp
@@ -423,7 +423,11 @@ Bitu DmaChannel::Read(Bitu want, uint8_t * buffer) {
     /* You cannot read a DMA channel configured for writing (to memory) */
     if (transfer_mode != DMAT_READ) {
         LOG(LOG_DMACONTROL,LOG_WARN)("BUG: Attempted DMA channel read when DMA channel is configured by guest for writing (to memory)");
-        return 0;
+        char psp_name[9];
+        DOS_MCB psp_mcb(dos.psp()-1);
+        psp_mcb.GetFileName(psp_name);
+        if (strcmp(psp_name, "DIAGNOSE")) // Wengier: Hack for Creative DIAGNOSE.EXE tool for now until the DMA recording function is implemented
+            return 0;
     }
 
     /* WARNING: "want" is expressed in DMA transfer units.
