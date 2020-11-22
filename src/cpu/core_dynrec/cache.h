aed412749c82c67c302de27db2236a2e9775ae56
Jonathan Campbell <jonathancampbell@Jonathans-MacBook-Pro-with-an-ARM.local>
2020-11-22 03:20:31 -0800

    dynrec: if mprotect fails, print errno string to say why

--- src/cpu/core_dynrec/cache.h
+++ src/cpu/core_dynrec/cache.h
@@ -632,7 +632,7 @@ static void cache_reset(void) {
 
 #if (C_HAVE_MPROTECT)
 			if(mprotect(cache_code_link_blocks,CACHE_TOTAL+CACHE_MAXSIZE+PAGESIZE_TEMP,PROT_WRITE|PROT_READ|PROT_EXEC))
-				LOG_MSG("Setting excute permission on the code cache has failed!");
+				LOG_MSG("Setting execute permission on the code cache has failed! err=%s",strerror(errno));
 #endif
 		}
 
@@ -706,7 +706,7 @@ static void cache_init(bool enable) {
 
 #if (C_HAVE_MPROTECT)
 			if(mprotect(cache_code_link_blocks,CACHE_TOTAL+CACHE_MAXSIZE+PAGESIZE_TEMP,PROT_WRITE|PROT_READ|PROT_EXEC))
-				LOG_MSG("Setting execute permission on the code cache has failed");
+				LOG_MSG("Setting execute permission on the code cache has failed! err=%s",strerror(errno));
 #endif
 			CacheBlockDynRec * block=cache_getblock();
 			cache.block.first=block;
