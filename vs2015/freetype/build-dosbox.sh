e4f592dd835ddd012fa13941c576d8dad98449c4
Jonathan Campbell <jonathan@castus.tv>
2020-11-27 11:07:51 -0800

    FreeType build script

---
+++ vs2015/freetype/build-dosbox.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+rm -Rfv linux-host || exit 1
+mkdir -p linux-host || exit 1
+
+rm -Rfv linux-build || exit 1
+mkdir -p linux-build || exit 1
+
+chmod +x configure || exit 1
+
+srcdir="`pwd`"
+instdir="`pwd`/linux-host"
+
+cd linux-build || exit 1
+
+opts=
+
+../configure "--srcdir=$srcdir" "--prefix=$instdir" --enable-static --disable-shared $opts || exit 1
+
+make -j || exit 1
+make install || exit 1  # will install into ./linux-host
+
