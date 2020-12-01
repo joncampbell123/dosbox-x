bfa4a1510b39ee19731bc02b42d23b508b494105
Jonathan Campbell <jonathan@castus.tv>
2020-12-01 01:21:16 -0800

    dynamic core, more methods

--- src/cpu/dynamic_alloc_common.h
+++ src/cpu/dynamic_alloc_common.h
@@ -13,6 +13,8 @@ static void cache_dynamic_common_alloc(Bitu allocsz) {
     assert(cache_code == NULL);
     assert(actualsz >= allocsz);
 
+    dyncore_flags = 0;
+    dyncore_method = DYNCOREM_RWX;
     dyncore_alloc = DYNCOREALLOC_NONE;
 
 #if defined (WIN32)
@@ -26,6 +28,26 @@ static void cache_dynamic_common_alloc(Bitu allocsz) {
         cache_code_start_ptr=(uint8_t*)mmap(NULL,actualsz,PROT_READ|PROT_WRITE|PROT_EXEC,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
         if (cache_code_start_ptr != MAP_FAILED) dyncore_alloc = DYNCOREALLOC_MMAP_ANON;
         else cache_code_start_ptr = NULL; /* MAP_FAILED is NOT NULL (or at least we cannot assume that) */
+    }
+#endif
+#if defined(C_HAVE_MMAP) /* try again, this time detect if read/write and read/execute are allowed (W^X) */
+    if (cache_code_start_ptr == NULL) {
+        cache_code_start_ptr=(uint8_t*)mmap(NULL,actualsz,PROT_READ|PROT_EXEC,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
+        if (cache_code_start_ptr != MAP_FAILED) {
+            if (munmap(cache_code_start_ptr,actualsz) == 0) {
+                cache_code_start_ptr=(uint8_t*)mmap(NULL,actualsz,PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
+                if (cache_code_start_ptr != MAP_FAILED) {
+                    dyncore_alloc = DYNCOREALLOC_MMAP_ANON;
+                    dyncore_method = DYNCOREM_MPROTECT_RW_RX;
+                    dyncore_flags |= DYNCOREF_W_XOR_X;
+                }
+            }
+            else {
+                cache_code_start_ptr = (uint8_t*)MAP_FAILED;
+            }
+        }
+        if (cache_code_start_ptr == MAP_FAILED)
+            cache_code_start_ptr = NULL;
     }
 #endif
     if (cache_code_start_ptr == NULL) {
@@ -44,6 +66,13 @@ static void cache_dynamic_common_alloc(Bitu allocsz) {
         case DYNCOREALLOC_MMAP_ANON:        LOG(LOG_MISC,LOG_DEBUG)("dyncore alloc: mmap using MAP_PRIVATE|MAP_ANONYMOUS"); break;
         case DYNCOREALLOC_MALLOC:           LOG(LOG_MISC,LOG_DEBUG)("dyncore alloc: malloc"); break;
         default:                            LOG(LOG_MISC,LOG_DEBUG)("dyncore alloc: ?"); break;
+    };
+
+    switch (dyncore_method) {
+        case DYNCOREM_NONE:                 LOG(LOG_MISC,LOG_DEBUG)("dyncore method: none"); break;
+        case DYNCOREM_RWX:                  LOG(LOG_MISC,LOG_DEBUG)("dyncore method: rwx"); break;
+        case DYNCOREM_MPROTECT_RW_RX:       LOG(LOG_MISC,LOG_DEBUG)("dyncore method: mprotect rw/rx"); break;
+        default:                            LOG(LOG_MISC,LOG_DEBUG)("dyncore method: ?"); break;
     };
 
     assert((cache_code+allocsz) <= (cache_code_start_ptr+actualsz));
