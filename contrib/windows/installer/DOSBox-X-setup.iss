2abf6700cb1608bbaed506b27bbe0801a1a6e277
Jonathan Campbell <jonathan@castus.tv>
2020-11-30 23:03:49 -0800

    Begin next version

--- contrib/windows/installer/DOSBox-X-setup.iss
+++ contrib/windows/installer/DOSBox-X-setup.iss
@@ -1,5 +1,5 @@
 #define MyAppName "DOSBox-X"
-#define MyAppVersion "0.83.8"
+#define MyAppVersion "0.83.9"
 #define MyAppPublisher "joncampbell123"
 #define MyAppURL "https://dosbox-x.com/"
 #define MyAppExeName "dosbox-x.exe"
--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,4 +1,4 @@
 /*auto-generated*/
-#define UPDATED_STR "Nov 30, 2020 6:44:18pm"
-#define GIT_COMMIT_HASH "6e395c0"
+#define UPDATED_STR "Nov 30, 2020 11:01:27pm"
+#define GIT_COMMIT_HASH "9e9d7ac"
 #define COPYRIGHT_END_YEAR "2020"
