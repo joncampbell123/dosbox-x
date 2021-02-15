88aa3331eb1590ed59d468324fd2c069e9a7f625
Jonathan Campbell <jonathan@castus.tv>
2021-02-15 15:56:49 -0800

    update build date and reference conf

--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,4 +1,4 @@
 /*auto-generated*/
-#define UPDATED_STR "Feb 5, 2021 12:40:24am"
-#define GIT_COMMIT_HASH "081d96b"
+#define UPDATED_STR "Feb 15, 2021 3:55:09pm"
+#define GIT_COMMIT_HASH "45fbbb9"
 #define COPYRIGHT_END_YEAR "2021"
