b1ee4e512810e13ca427442e48ab239a7144b843
Wengier <wengierwu@yahoo.com>
2021-03-15 13:33:25 -0400

    Update sdl_mapper.cpp

--- src/gui/sdl_mapper.cpp
+++ src/gui/sdl_mapper.cpp
@@ -738,7 +738,7 @@ class Typer {
 						if (bind_name == event->GetName()) {
 							found = true;
 							event->Active(true);
-							SDL_Delay(10);
+						        std::this_thread::sleep_for(std::chrono::milliseconds(50));
 							event->Active(false);
 							break;
 						}
