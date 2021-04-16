08ffdd4afa0fe0a8518db79d1ff44842dcc5c0e6
Jonathan Campbell <jonathan@castus.tv>
2021-04-15 20:48:23 -0700

    Stop packing all Windows VS2019 builds into one giant zip archive. Make one ZIP archive per platform and build

--- make-windows-release.pl
+++ make-windows-release.pl
@@ -31,13 +31,9 @@ mkdir "$subdir" unless -d "$subdir";
 
 die "bin directory not exist" unless -d "bin";
 
-my $zipname = "dosbox-x-$suffix-win-$datestr.zip";
-exit 0 if -f $zipname;
 die unless -f $ziptool;
 
 print "$zipname\n";
-
-my @filelist = ();
 
 my @platforms = ('ARM', 'ARM64', 'Win32', 'x64');
 my @builds = ('Release', 'Release SDL2');
@@ -45,15 +41,24 @@ my @files = ('dosbox-x.reference.conf', 'dosbox-x.reference.full.conf', 'dosbox-
 
 foreach $platform (@platforms) {
 	foreach $build (@builds) {
+		my @filelist = ();
+
+		# no spaces in filenames
+		my $p_build = $build;
+		$p_build =~ s/ /-/g;
+
+		my $zipname = "dosbox-x-$suffix-win-$platform-$p_build-$datestr.zip";
+		next if -f $zipname;
+
 		foreach $file (@files) {
 			$addfile = "bin/$platform/$build/$file";
 			die "Missing file $addfile" unless -e $addfile;
 
 			push(@filelist, $addfile);
 		}
+
+		# do it
+		$r = system($ziptool, '-9', '-r', "$subdir/$zipname", @filelist);
+		exit 1 unless $r == 0;
 	}
 }
-
-# do it
-$r = system($ziptool, '-9', '-r', "$subdir/$zipname", @filelist);
-exit 1 unless $r == 0;
