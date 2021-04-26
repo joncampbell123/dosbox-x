a9fa9a0ac18abfc5cd866b9e3a2dff73a8a8f152
Wengier <wengierwu@yahoo.com>
2021-04-26 19:57:30 -0400

    underline is valid too for DOS7/Win9x

--- src/shell/shell_misc.cpp
+++ src/shell/shell_misc.cpp
@@ -890,9 +890,9 @@ void DOS_Shell::ProcessCmdLineEnvVarStitution(char *line) {
 				 *
 				 * C:\>echo %PATH %
 				 * %PATH % */
-				if (isalpha(*r) || *r == ' ') { /* must start with a letter. space is apparently valid too. (Win95) */
+				if (isalpha(*r) || *r == '_' || *r == ' ') { /* must start with a letter. Space and underline are apparently valid too. (MS-DOS 7/Win9x) */
 					if (*r == ' ') spaces++;
-					else if (isalpha(*r)) chars++;
+					else if (isalpha(*r) || *r == '_') chars++;
 
 					r++;
 					while (*r != 0 && *r != '%') {
