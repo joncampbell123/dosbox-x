641dc150e9714201cdbb91bdfbb922acfedb7939
Wengier <wengierwu@yahoo.com>
2021-05-18 18:02:32 -0400

    fix translation for PC-98

--- src/gui/menu.cpp
+++ src/gui/menu.cpp
@@ -1172,11 +1172,12 @@ void* DOSBoxMenu::getNsMenu(void) const {
 #if defined(WIN32) && !defined(HX_DOS)
 LPWSTR getWString(std::string str, wchar_t *def, wchar_t*& buffer) {
     LPWSTR ret = def;
-    int reqsize = 0, cp = dos.loaded_codepage?dos.loaded_codepage:(IS_PC98_ARCH?932:0);
+    int reqsize = 0, cp = dos.loaded_codepage;
     Section_prop *section = static_cast<Section_prop *>(control->GetSection("config"));
-    if (!dos.loaded_codepage && !IS_PC98_ARCH && section!=NULL) {
+    if (!dos.loaded_codepage && section!=NULL) {
         char *countrystr = (char *)section->Get_string("country"), *r=strchr(countrystr, ',');
         if (r!=NULL && *(r+1)) cp = atoi(trim(r+1));
+        if (!cp && IS_PC98_ARCH) cp = 932;
     }
     uint16_t len=(uint16_t)str.size();
     if (cp) {
