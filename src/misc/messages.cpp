2061691db76fb26929338efc1b85dd032b35b74b
Wengier <wengierwu@yahoo.com>
2021-11-01 02:58:10 -0400

    last fix

--- src/misc/messages.cpp
+++ src/misc/messages.cpp
@@ -33,8 +33,8 @@
 #include <string>
 using namespace std;
 
-int msgcodepage = 0;
 extern bool dos_kernel_disabled, force_conversion;
+int msgcodepage = 0, FileDirExistUTF8(std::string &localname, const char *name);
 bool morelen = false, loadlang = false, systemmessagebox(char const * aTitle, char const * aMessage, char const * aDialogType, char const * aIconType, int aDefaultButton);
 bool isSupportedCP(int newCP), CodePageHostToGuestUTF8(char *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/), CodePageGuestToHostUTF8(char *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/);
 void InitFontHandle(void), ShutFontHandle(void), menu_update_autocycle(void), update_bindbutton_text(void), set_eventbutton_text(const char *eventname, const char *buttonname);
@@ -125,6 +125,11 @@ void LoadMessageFile(const char * fname) {
 	if (!mfile && exepath.size()) mfile=fopen((exepath + "languages/" + fname + ".lng").c_str(),"rt");
 	if (!mfile && config_path.size()) mfile=fopen((config_path + "languages/" + fname).c_str(),"rt");
 	if (!mfile && config_path.size()) mfile=fopen((config_path + "languages/" + fname + ".lng").c_str(),"rt");
+#if defined(WIN32) && defined(C_SDL2)
+    std::string localname = fname;
+    if (!mfile && FileDirExistUTF8(localname, fname) == 1) mfile=fopen(localname.c_str(),"rt");
+#endif
+
 	/* This should never happen and since other modules depend on this use a normal printf */
 	if (!mfile) {
 		std::string message="Could not load language message file '"+std::string(fname)+"'. The default language will be used.";
