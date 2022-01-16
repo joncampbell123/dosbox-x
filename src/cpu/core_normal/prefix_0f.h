c77f4a1eb8b679a5ea72887a116a25f08719b0fd
Jonathan Campbell <jonathan@castus.tv>
2022-01-16 00:14:05 -0800

    MOVLPS the other way

--- src/cpu/core_normal/prefix_0f.h
+++ src/cpu/core_normal/prefix_0f.h
@@ -251,6 +251,30 @@
 		break;
 #endif
 #if CPU_CORE >= CPU_ARCHTYPE_386
+	CASE_0F_B(0x13)												/* SSE instruction group */
+		if (CPU_ArchitectureType<CPU_ARCHTYPE_PENTIUMIII || !CPU_SSE()) goto illegal_opcode;
+		{
+			XMM_Reg xmmdst;
+			GetRM;
+			const unsigned char reg = (rm >> 3) & 7;
+
+			switch (last_prefix) {
+				case MP_NONE:									/* 0F 13 MOVUPS r/m, reg */
+					if (rm >= 0xc0) {
+						goto illegal_opcode;
+					} else {
+						GetEAa;
+						SSE_MOVLPS(xmmdst,fpu.xmmreg[reg]);
+						SaveMq(eaa,xmmdst.u64[0]);
+					}
+					break;
+				default:
+					goto illegal_opcode;
+			};
+		}
+		break;
+#endif
+#if CPU_CORE >= CPU_ARCHTYPE_386
 	CASE_0F_B(0x18)												/* SSE instruction /r group */
 		if (CPU_ArchitectureType<CPU_ARCHTYPE_PENTIUMIII) goto illegal_opcode;
 		{
