71f96e71b244f80c0f54e7519cec42f4a9408a76
Jonathan Campbell <jonathan@castus.tv>
2022-01-20 22:56:05 -0800

    Pentium 3 SSE: Add CVTTPS2PI/CVTTSS2SI/CVTPS2PI/CVTSS2SI to debugger

--- src/debug/debug_disasm.cpp
+++ src/debug/debug_disasm.cpp
@@ -288,7 +288,7 @@ static char const *second[] = {
   "mov %Rd,%Cd",      "mov %Rd,%Dd",     "mov %Cd,%Rd",    "mov %Dd,%Rd",
   "mov %Rd,%Td",      0,                 "mov %Td,%Rd",    0,
   "%x0",              "%x0",             "%x0",            "%x0",
-  0,                  0,                 0,                0,
+  "%x0",              "%x0",             0,                0,
 /* 3 */
   0,                  "rdtsc",           0,                0,
   "sysenter",         "sysexit",         0,                0,
@@ -379,7 +379,9 @@ static char const *mpgroups[][256][4] = { /* mandatory prefix groups SSE instruc
     /* 0x29 */ { "movaps %EX,%GX", "movapd %EX,%GX", 0, 0 },
     /* 0x2A */ { "cvtpi2ps %GX,%EM", 0, 0, "cvtsi2ss %GX,%Ed" },
     /* 0x2B */ { "movntps %EX,%GX", 0,0,0 },
-    /* 0x2C */ { 0,0,0,0 }, /* 0x2D */ { 0,0,0,0 }, /* 0x2E */ { 0,0,0,0 }, /* 0x2F */ { 0,0,0,0 },
+    /* 0x2C */ { "cvttps2pi %GM,%EX", 0, 0, "cvttss2si %Gd,%EX" },
+    /* 0x2D */ { "cvtps2pi %GM,%EX", 0, 0, "cvtss2si %Gd,%EX" },
+    /* 0x2E */ { 0,0,0,0 }, /* 0x2F */ { 0,0,0,0 },
 
     /* 0x30 */ { 0,0,0,0 }, /* 0x31 */ { 0,0,0,0 }, /* 0x32 */ { 0,0,0,0 }, /* 0x33 */ { 0,0,0,0 },
     /* 0x34 */ { 0,0,0,0 }, /* 0x35 */ { 0,0,0,0 }, /* 0x36 */ { 0,0,0,0 }, /* 0x37 */ { 0,0,0,0 },
