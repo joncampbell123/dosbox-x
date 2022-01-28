d7063703bdcf22bfdf4ff9cff8ed241d8ba69ddc
Jonathan Campbell <jonathan@castus.tv>
2022-01-27 21:56:43 -0800

    Pentium 3 SSE: Add ORPS to normal core

--- src/cpu/core_normal/prefix_0f.h
+++ src/cpu/core_normal/prefix_0f.h
@@ -994,6 +994,33 @@
 #endif
 
 #if CPU_CORE >= CPU_ARCHTYPE_386
+	CASE_0F_B(0x56)												/* SSE instruction group */
+		if (CPU_ArchitectureType<CPU_ARCHTYPE_PENTIUMIII || !CPU_SSE()) goto illegal_opcode;
+		{
+			XMM_Reg xmmsrc;
+			GetRM;
+			const unsigned char reg = (rm >> 3) & 7;
+
+			switch (last_prefix) {
+				case MP_NONE:									/* 0F 56 ORPS reg, r/m */
+					if (rm >= 0xc0) {
+						SSE_ORPS(fpu.xmmreg[reg],fpu.xmmreg[rm & 7]);
+					} else {
+						GetEAa;
+						if (!SSE_REQUIRE_ALIGNMENT(eaa)) SSE_ALIGN_EXCEPTION();
+						xmmsrc.u64[0] = LoadMq(eaa);
+						xmmsrc.u64[1] = LoadMq(eaa+8u);
+						SSE_ORPS(fpu.xmmreg[reg],xmmsrc);
+					}
+					break;
+				default:
+					goto illegal_opcode;
+			};
+		}
+		break;
+#endif
+
+#if CPU_CORE >= CPU_ARCHTYPE_386
 	CASE_0F_B(0x57)												/* SSE instruction group */
 		if (CPU_ArchitectureType<CPU_ARCHTYPE_PENTIUMIII || !CPU_SSE()) goto illegal_opcode;
 		{
--- src/cpu/core_normal/support.h
+++ src/cpu/core_normal/support.h
@@ -387,6 +387,21 @@ static INLINE void SSE_RCPSS(XMM_Reg &d,const XMM_Reg &s) {
 }
 #undef STEP
 
+////
+
+#define STEP(i) SSE_ORPS_i(d.f32[i],s.f32[i])
+static INLINE void SSE_ORPS_i(FPU_Reg_32 &d,const FPU_Reg_32 &s) {
+	d.raw |= s.raw;
+}
+
+static INLINE void SSE_ORPS(XMM_Reg &d,const XMM_Reg &s) {
+	STEP(0);
+	STEP(1);
+	STEP(2);
+	STEP(3);
+}
+#undef STEP
+
 #endif // 386+
 
 #define SETcc(cc)							\
