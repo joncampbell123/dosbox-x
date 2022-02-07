34e5848d1e328a44a96a25fe921ab41c0eadfc45
maron2000 <68574602+maron2000@users.noreply.github.com>
2022-02-07 18:27:33 +0900

    Fix conditions (avoid assertion failure)

--- src/gui/sdl_mapper.cpp
+++ src/gui/sdl_mapper.cpp
@@ -1468,7 +1468,10 @@ public:
 		//key = (event->key.keysym.sym ? GetKeyCode(event->key.keysym) : sdlkey_map[(Bitu)(event->key.keysym.scancode)]);
 		key = GetKeyCode(event->key.keysym);
 		//assert(key < keys);
-        if(key > keys) key = SDLK_UNKNOWN; // a test to avoid assertion failure (key < keys)
+        if(key >= keys) {
+            key = SDLK_UNKNOWN; // a test to avoid assertion failure (key < keys)
+            LOG_MSG("assertion failed: key: %x [keysym.sym:%x keysym.scancode: %x]", key, event->key.keysym.sym, event->key.keysym.scancode);
+        }
 #endif
 //      LOG_MSG("key type %i is %x [%x %x]",event->type,key,event->key.keysym.sym,event->key.keysym.scancode);
 
