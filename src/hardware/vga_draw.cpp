e2d4bcd6c9158990b1950c54632af06a594d0946
Jonathan Campbell <jonathan@castus.tv>
2022-02-24 18:40:55 -0800

    It turns out S3 Vision864/Vision868 cards also like to re-use the VGA cursor position register as a 256-color foreground/background cursor color according to Windows 95 behavior

--- src/hardware/vga_draw.cpp
+++ src/hardware/vga_draw.cpp
@@ -1274,11 +1274,15 @@ static uint8_t * VGA_Draw_VGA_Line_Xlat32_HWMouse( Bitu vidstart, Bitu /*line*/)
             // to set 256-color cursor colors using the foreground/background stack registers
             // of later cards. Truecolor/highcolor cards still use foreground/background
             // stack on 86C928 cards.
+	    //
+	    // Windows 95 behavior suggests that S3 Vision864 cards behave the same way in
+	    // 256-color mode. Perhaps it was there all along with Windows 3.1, but it happens
+	    // to write all the right values in a way that masks the issue.
             //
             // FIXME: On 86C928 cards, bits 2 & 3 of the Hardware Graphics Cursor Mode Register (CR45)
             //        enable horizontal stretch which also determines how the foreground/background
             //        stack is used to render the cursor.
-            if (svgaCard == SVGA_S3Trio && s3Card == S3_86C928) {
+            if (svgaCard == SVGA_S3Trio && (s3Card >= S3_86C928 && s3Card <= S3_Vision868)) {
                 fg = (vga.config.cursor_start >> 8u) & 0xFFu; // register 0Eh
                 bg = (vga.config.cursor_start & 0xFFu); // register 0Fh
             }
