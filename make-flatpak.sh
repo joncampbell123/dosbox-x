ccfff3e3422629d4d395a6461468da81896a644e
Robert de Rooy <robert.de.rooy@gmail.com>
2022-02-28 08:30:31 +0100

    remove SDL1 version and make SDL2 default
    this mirrors what is done on flathub

--- make-flatpak.sh
+++ make-flatpak.sh
@@ -11,17 +11,17 @@ if [[ ! $(flatpak list|grep org.freedesktop.Sdk|wc -l) -ge 1 ]]; then
 	exit
 fi
 
-flatpak-builder --repo=myrepo --force-clean build-flatpak contrib/linux/com.dosbox_x.DOSBox-X.yaml
+flatpak-builder --repo=myrepo --force-clean build-flatpak contrib/linux/com.dosbox_x.DOSBox-X-sdl2.yaml
 if [ $? -eq 0 ]; then
 	echo
 	echo You can now install the flatpak by running the following commands:
 	echo
 	echo  flatpak --user remote-add --no-gpg-verify myrepo myrepo
-	echo  flatpak --user install myrepo com.dosbox_x.DOSBox-X
+	echo  flatpak --user install myrepo com.dosbox_x.DOSBox-X-sdl2
 	echo
 	echo You can then run the flatpak as follows:
-	echo  flatpak run com.dosbox_x.DOSBox-X
+	echo  flatpak run com.dosbox_x.DOSBox-X-sdl2
 	echo
 	echo Or you can test it without installing by running:
-	echo  flatpak-builder --run build-flatpak contrib/linux/com.dosbox_x.DOSBox-X.yaml dosbox-x
+	echo  flatpak-builder --run build-flatpak contrib/linux/com.dosbox_x.DOSBox-X-sdl2.yaml dosbox-x
 fi
