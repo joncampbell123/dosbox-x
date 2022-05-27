92d7fb67f570d3d2ff1dac92dd230277e1387116
Wengier <wengierwu@yahoo.com>
2022-05-27 01:56:12 -0400

    update language

--- src/hardware/ide.cpp
+++ src/hardware/ide.cpp
@@ -1,8 +1,20 @@
 /*
- * IDE ATA/ATAPI and controller emulation for DOSBox-X
- * (C) 2012 Jonathan Campbell
+ *  IDE ATA/ATAPI and controller emulation for DOSBox-X
+ *  Copyright (C) 2012-2022 Jonathan Campbell
 
- * [insert open source license here]
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 /* $Id: ide.cpp,v 1.49 2009-04-10 09:53:04 c2woody Exp $ */
