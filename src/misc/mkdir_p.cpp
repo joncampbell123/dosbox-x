05851a4945435d5ecc5e7dc6c77f2b45e18b8a98
Jonathan Campbell <jonathan@castus.tv>
2022-05-29 11:31:30 -0700

    Copy-paste for Win32 _wmkdir_p

--- src/misc/mkdir_p.cpp
+++ src/misc/mkdir_p.cpp
@@ -16,7 +16,77 @@ using namespace std;
 /* Windows uses \\ (though since Windows 7 or so, / is also accepted apparently) */
 # define PSEP '\\'
 
+/* TODO: Maybe this could be combined with C++ templating and an #ifdef WIN32? */
 int _wmkdir_p(const wchar_t *pathname) {
+	const size_t pathlen = wcslen(pathname);
+	wchar_t *pc = new wchar_t[pathlen+1];
+	const wchar_t *ps = pathname;
+	wchar_t *pwf = pc + pathlen;
+	wchar_t *pw = pc;
+	struct stat st;
+	int result = 0;
+	errno = 0;
+
+	/* copy initial leading / if caller wants absolute paths */
+	while (*ps == PSEP) *pw++ = *ps++;
+	assert(pw <= pwf);
+
+	if (*ps != 0) {
+		/* for each path element, copy element and then mkdir */
+		do {
+			if (*ps == PSEP) {
+				*pw++ = *ps++; /* extra path separator? ok... */
+				assert(pw <= pwf);
+			}
+			else {
+				/* one element */
+				while (!(*ps == 0 || *ps == PSEP)) *pw++ = *ps++;
+				assert(pw <= pwf);
+
+				/* and mkdir() with it if needed */
+				*pw = 0; /* NUL terminate pc we copied so far */
+				if (_wstat(pc,&st) == 0) {
+					if (S_ISDIR(st.st_mode)) {
+						/* expected, do nothing */
+					}
+					else {
+						/* not a directory? stop and do not continue */
+						result = -1;
+						errno = ENOTDIR;
+						break; /* out of while (*ps != 0) */
+					}
+				}
+				else {
+					if (errno == ENOENT) {
+						/* expected, create directory */
+						if (_wmkdir(pc,mode)/*failed*/) {
+							/* create failed, stop and do not continue. */
+							result = -1;
+							/* errno already set by mkdir() */
+							break;
+						}
+					}
+					else {
+						/* not expected, stop and do not continue */
+						result = -1;
+						/* errno already set by stat() */
+						break;
+					}
+				}
+
+				/* skip path separator and continue */
+				if (*ps == PSEP) *pw++ = *ps++;
+				assert(pw <= pwf);
+			}
+		} while (*ps != 0);
+	}
+	else {
+		errno = ENOTSUP; /* trying to mkdir "" or "/", why?? */
+	}
+
+	delete[] pc;
+	return result;
+
 	errno = ENOSYS;
 	return -1;
 }
