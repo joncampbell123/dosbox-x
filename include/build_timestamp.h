36ee5351a1b0c40c6d2fc5e182910e20d2f38179
Jonathan Campbell <jonathan@castus.tv>
2022-05-31 20:33:06 -0700

    Bump to 0.84.0

--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,4 +1,4 @@
 /*auto-generated*/
-#define UPDATED_STR "May 31, 2022 7:31:25pm"
-#define GIT_COMMIT_HASH "1fceadc"
+#define UPDATED_STR "May 31, 2022 8:32:37pm"
+#define GIT_COMMIT_HASH "938011f"
 #define COPYRIGHT_END_YEAR "2022"
