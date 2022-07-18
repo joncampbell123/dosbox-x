f31ebe2de9ce9f0432ed08088e51db357e4a2102
Jonathan Campbell <jonathan@castus.tv>
2022-07-18 16:54:42 -0700

    SDL_EventState() polls and drops queued events during the function. That doesn't seem necessary and it's causing lost keystrokes. This was apparently the reason Windows SDL1 builds needed the user to type the fullscreen mapper shortcut twice.

--- vs/sdl/src/events/SDL_events.c
+++ vs/sdl/src/events/SDL_events.c
@@ -458,8 +458,10 @@ Uint8 SDL_EventState (Uint8 type, int state)
 				SDL_eventstate &= ~(0x00000001 << (type));
 			}
 		}
-		while ( SDL_PollEvent(&bitbucket) > 0 )
+#if 0 // This does not seem necessary, and in fact results in lost keystrokes when entering/leaving keystrokes. Disabled
+        while ( SDL_PollEvent(&bitbucket) > 0 )
 			;
+#endif
 		return(current_state);
 	}
 
@@ -475,8 +477,10 @@ Uint8 SDL_EventState (Uint8 type, int state)
 			} else {
 				SDL_eventstate &= ~(0x00000001 << (type));
 			}
-			while ( SDL_PollEvent(&bitbucket) > 0 )
+#if 0 // This does not seem necessary, and in fact results in lost keystrokes when entering/leaving keystrokes. Disabled
+            while ( SDL_PollEvent(&bitbucket) > 0 )
 				;
+#endif
 			break;
 		default:
 			/* Querying state? */
