joncampbell123/dosbox-x

commit bd7a7293194747ee9ae6492a1a5c5dec9a4b398a

Wengier patches [https://github.com/joncampbell123/dosbox-x/issues/1487]

src/shell/shell_cmds.cpp
@@ -96,7 +96,9 @@ static SHELL_Cmd cmd_list[]={
 {0,0,0,0}
 }; 
 
-extern bool date_host_forced, usecon;
+extern int faux;
+extern bool date_host_forced, usecon, rsize;
+extern unsigned long freec;
 
 /* support functions */
 static char empty_char = 0;
@@ -495,7 +497,13 @@ void DOS_Shell::CMD_HELP(char * args){
 	while (cmd_list[cmd_index].name) {
 		if (optall || !cmd_list[cmd_index].flags) {
 			WriteOut("<\033[34;1m%-8s\033[0m> %s",cmd_list[cmd_index].name,MSG_Get(cmd_list[cmd_index].help));
-			if(!(++write_count%22)) CMD_PAUSE(empty_string);
+			if(!(++write_count%22)) {
+				WriteOut(MSG_Get("SHELL_CMD_PAUSE"));
+				Bit8u c;Bit16u n=1;
+				DOS_ReadFile(STDIN,&c,&n);
+				if (c==3) {WriteOut("^C\r\n");break;}
+				if (c==0) DOS_ReadFile(STDIN,&c,&n); // read extended key
+			}
 		}
 		cmd_index++;
 	}
@@ -793,15 +801,21 @@ void DOS_Shell::CMD_RMDIR(char * args) {
 	}
 }
 
-static void FormatNumber(Bit32u num,char * buf) {
-	Bit32u numm,numk,numb,numg;
+static void FormatNumber(Bit64u num,char * buf) {
+	Bit32u numm,numk,numb,numg,numt;
 	numb=num % 1000;
 	num/=1000;
 	numk=num % 1000;
 	num/=1000;
 	numm=num % 1000;
 	num/=1000;
-	numg=num;
+	numg=num % 1000;
+	num/=1000;
+	numt=num;
+	if (numt) {
+		sprintf(buf,"%u,%03u,%03u,%03u,%03u",numt,numg,numm,numk,numb);
+		return;
+	}
 	if (numg) {
 		sprintf(buf,"%u,%03u,%03u,%03u",numg,numm,numk,numb);
 		return;
@@ -875,10 +889,14 @@ static bool doDir(DOS_Shell * shell, char * args, DOS_DTA dta, char * numformat,
     if (*(sargs+strlen(sargs)-1) != '\\') strcat(sargs,"\\");
 
 	Bit32u cbyte_count=0,cfile_count=0,w_count=0;
+	int fbak=faux;
+	faux=uselfn?255:256;
 	bool ret=DOS_FindFirst(args,0xffff & ~DOS_ATTR_VOLUME), found=true;
+	faux=fbak;
 	if (ret) {
 		std::vector<DtaResult> results;
 
+		faux=uselfn?255:256;
 		do {    /* File name and extension */
 			DtaResult result;
 			dta.GetResult(result.name,result.lname,result.size,result.date,result.time,result.attr);
@@ -899,6 +917,7 @@ static bool doDir(DOS_Shell * shell, char * args, DOS_DTA dta, char * numformat,
 			results.push_back(result);
 
 		} while ( (ret=DOS_FindNext()) );
+		faux=fbak;
 
 		if (optON) {
 			// Sort by name
@@ -1154,7 +1173,7 @@ void DOS_Shell::CMD_DIR(char * args) {
 		WriteOut(MSG_Get("SHELL_ILLEGAL_PATH"));
 		return;
 	}
-	if (!strrchr(sargs,'.'))
+	if (!(uselfn&&strchr(sargs,'*'))&&!strrchr(sargs,'.'))
 		strcat(sargs,".*");	// if no extension, get them all
     sprintf(args,"\"%s\"",sargs);
 
@@ -1205,8 +1224,11 @@ void DOS_Shell::CMD_DIR(char * args) {
 		Bitu free_space=1024u*1024u*100u;
 		if (Drives[drive]) {
 			Bit16u bytes_sector;Bit8u sectors_cluster;Bit16u total_clusters;Bit16u free_clusters;
+			rsize=true;
+			freec=0;
 			Drives[drive]->AllocationInfo(&bytes_sector,&sectors_cluster,&total_clusters,&free_clusters);
-			free_space=(Bitu)bytes_sector * (Bitu)sectors_cluster * (Bitu)free_clusters;
+			free_space=(Bitu)bytes_sector * (Bitu)sectors_cluster * (Bitu)(freec?freec:free_clusters);
+			rsize=false;
 		}
 		FormatNumber(free_space,numformat);
 		WriteOut(MSG_Get("SHELL_CMD_DIR_BYTES_FREE"),dir_count,numformat);
@@ -1291,6 +1313,7 @@ void DOS_Shell::CMD_COPY(char * args) {
 			size_t source_x_len = strlen(source_x);
 			if (source_x_len>0) {
 				if (source_x[source_x_len-1]==':') has_drive_spec = true;
+				else if (uselfn&&source_x[source_x_len-1]=='*'&&strchr(source_x, '.')==NULL) strcat(source_x, ".*");
 			}
 			if (!has_drive_spec  && !strpbrk(source_p,"*?") ) { //doubt that fu*\*.* is valid
                 char spath[DOS_PATHLENGTH];
@@ -2162,6 +2185,7 @@ void DOS_Shell::CMD_CHOICE(char * args){
 	Bit16u n=1;
 	do {
 		DOS_ReadFile (STDIN,&c,&n);
+		if (c==3) {WriteOut("^C\r\n");dos.return_code=0;return;}
 	} while (!c || !(ptr = strchr(rem,(optS?c:toupper(c)))));
 	c = optS?c:(Bit8u)toupper(c);
 	DOS_WriteFile (STDOUT,&c, &n);
@@ -2274,7 +2298,10 @@ void DOS_Shell::CMD_ATTRIB(char *args){
 		else if (!strcasecmp(arg1, "-S")) subs=true;
 		else if (!strcasecmp(arg1, "-H")) subh=true;
 		else if (!strcasecmp(arg1, "-R")) subr=true;
-		else if (*arg1) strcpy(sfull, strcmp(arg1, "*")?arg1:"*.*");
+		else if (*arg1) {
+			strcpy(sfull, arg1);
+			if (uselfn&&sfull[strlen(sfull)-1]=='*'&&strchr(sfull, '.')==NULL) strcat(sfull, ".*");
+		}
 	} while (*args);
 
 	char buffer[CROSS_LEN];

dosbox.reference.conf
@@ -369,7 +369,8 @@ showmenu          = true
 #                   pc-98 force ibm keyboard layout: Force to use a default keyboard layout like IBM US-English for PC-98 emulation.
 #                                                      Will only work with apps and games using BIOS for keyboard.
 #                                        nocachedir: If set, MOUNT commands will mount with -nocachedir by default.
-#                 leading colon write protect image: If set, BOOT and IMGMOUNT commands will put an image file name with a leading colon (:) in write-protect mode.
+#                                       freesizecap: If set (default), the value of MOUNT -freesize will be applied only if the actual free size is greater than the specified value (CAP mode).
+#                 leading colon write protect image: If set (default), BOOT and IMGMOUNT commands will put an image file name with a leading colon (:) in write-protect mode.
 #                            vga bios size override: VGA BIOS size override. Override the size of the VGA BIOS (normally 32KB in compatible or 12KB in non-compatible).
 # video bios dont duplicate cga first half rom font: If set, save 4KB of EGA/VGA ROM space by pointing to the copy in the ROM BIOS of the first 128 chars
 #    video bios always offer 14-pixel high rom font: If set, video BIOS will always carry the 14-pixel ROM font. If clear, 14-pixel rom font will not be offered except for EGA/VGA emulation.
@@ -576,6 +577,7 @@ pc-98 timer master frequency                      = 0
 pc-98 allow 4 display partition graphics          = -1
 pc-98 force ibm keyboard layout                   = false
 nocachedir                                        = false
+freesizecap                                       = true
 leading colon write protect image                 = true
 vga bios size override                            = 0
 video bios dont duplicate cga first half rom font = false

src/dos/dos.cpp
@@ -36,9 +36,9 @@
 #include "serialport.h"
 #include "dos_network.h"
 
-extern bool log_int21;
-extern bool log_fileio;
-
+extern bool log_int21, log_fileio;
+extern int faux;
+unsigned long totalc, freec;
 Bitu INT29_HANDLER(void);
 Bit32u BIOS_get_PC98_INT_STUB(void);
 
@@ -81,6 +81,7 @@ bool DOS_BreakFlag = false;
 bool enable_dbcs_tables = true;
 bool enable_filenamechar = true;
 bool enable_share_exe_fake = true;
+bool rsize = false;
 int dos_initial_hma_free = 34*1024;
 int dos_sda_size = 0x560;
 int dos_clipboard_device_access;
@@ -505,10 +506,10 @@ static Bitu DOS_21Handler(void) {
                 LOG(LOG_DOSMISC,LOG_DEBUG)("DOS:INT 20h/INT 21h AH=00h recovered CS segment %04x",f_cs);
 
                 DOS_Terminate(f_cs,false,0);
-            }
-            else {
+            } else if (reg_sp == 0xE224)
+                DOS_Terminate(dos.psp(),false,0);
+			else
                 DOS_Terminate(mem_readw(SegPhys(ss)+reg_sp+2),false,0);
-            }
 
             if (DOS_BreakINT23InProgress) throw int(0); /* HACK: Ick */
             dos_program_running = false;
@@ -1487,6 +1488,7 @@ static Bitu DOS_21Handler(void) {
             break;
         case 0x4e:                  /* FINDFIRST Find first matching file */
             MEM_StrCopy(SegPhys(ds)+reg_dx,name1,DOSNAMEBUF);
+			faux=256;
             if (DOS_FindFirst(name1,reg_cx)) {
                 CALLBACK_SCF(false);    
                 reg_ax=0;           /* Undocumented */
@@ -1995,6 +1997,8 @@ static Bitu DOS_21Handler(void) {
 			}
 			Bit16u bytes_per_sector,total_clusters,free_clusters;
 			Bit8u sectors_per_cluster;
+			rsize=true;
+			totalc=freec=0;
 			if (DOS_GetFreeDiskSpace(reg_dl,&bytes_per_sector,&sectors_per_cluster,&total_clusters,&free_clusters))
 				{
 				ext_space_info_t *info = new ext_space_info_t;
@@ -2002,12 +2006,12 @@ static Bitu DOS_21Handler(void) {
 				info->structure_version = 0;
 				info->sectors_per_cluster = sectors_per_cluster;
 				info->bytes_per_sector = bytes_per_sector;
-				info->available_clusters_on_drive = free_clusters;
-				info->total_clusters_on_drive = total_clusters;
-				info->available_sectors_on_drive = sectors_per_cluster * free_clusters;
-				info->total_sectors_on_drive = sectors_per_cluster * total_clusters;
-				info->available_allocation_units = free_clusters;
-				info->total_allocation_units = total_clusters;
+				info->available_clusters_on_drive = freec?freec:free_clusters;
+				info->total_clusters_on_drive = totalc?totalc:total_clusters;
+				info->available_sectors_on_drive = sectors_per_cluster * (freec?freec:free_clusters);
+				info->total_sectors_on_drive = sectors_per_cluster * (totalc?totalc:total_clusters);
+				info->available_allocation_units = freec?freec:free_clusters;
+				info->total_allocation_units = totalc?totalc:total_clusters;
 				MEM_BlockWrite(SegPhys(es)+reg_di,info,sizeof(ext_space_info_t));
 				delete(info);
 				reg_ax=0;
@@ -2018,6 +2022,7 @@ static Bitu DOS_21Handler(void) {
 				reg_ax=dos.errorcode;
 				CALLBACK_SCF(true);
 				}
+			rsize=false;
 			break;
 			}
 		case 0xE0:
@@ -3148,12 +3153,16 @@ void DOS_Int21_714e(char *name1, char *name2) {
 		}
 		if (strlen(name2)>2&&name2[strlen(name2)-2]=='\\'&&name2[strlen(name2)-1]=='*')
 			strcat(name2, ".*");
+		faux=handle;
 		bool b=DOS_FindFirst(name2,reg_cx,false);
+		faux=256;
 		int error=dos.errorcode;
 		Bit16u attribute = 0;
 		if (!b&&DOS_GetFileAttr(name2, &attribute) && (attribute&DOS_ATTR_DIRECTORY)) {
 			strcat(name2,"\\*.*");
+			faux=handle;
 			b=DOS_FindFirst(name2,reg_cx,false);
+			faux=256;
 			error=dos.errorcode;
 		}
 		if (b) {
@@ -3196,6 +3205,7 @@ void DOS_Int21_714f(char *name1, char *name2) {
 			CALLBACK_SCF(true);
 			return;
 		}
+		faux=handle;
 		if (DOS_FindNext()) {
 				DOS_DTA dta(dos.dta());
 				char finddata[CROSS_LEN];
@@ -3206,6 +3216,7 @@ void DOS_Int21_714f(char *name1, char *name2) {
 				reg_ax=dos.errorcode;
 				CALLBACK_SCF(true);
 		}
+		faux=256;
 }
 
 void DOS_Int21_7156(char *name1, char *name2) {

src/dos/dos_classes.cpp
@@ -24,8 +24,10 @@
 #include "dos_inc.h"
 #include "support.h"
 
-bool faux=false;
-char sname[LFN_NAMELENGTH+1],fname[LFN_NAMELENGTH+1],storect[CTBUF];
+Bit8u sattr[260], fattr;
+char sname[260][LFN_NAMELENGTH+1],storect[CTBUF];
+extern int faux;
+
 struct finddata {
        Bit8u attr;
        Bit8u fres1[19];
@@ -394,13 +396,15 @@ void DOS_DTA::SetupSearch(Bit8u _sdrive,Bit8u _sattr,char * pattern) {
 	sSave(sDTA,sdrive,_sdrive);
 	sSave(sDTA,sattr,_sattr);
 
-	/* Fill with char 0 */
-    int i;
-    for (i=0;i<LFN_NAMELENGTH;i++) {
-        if (pattern[i]==0) break;
-			(faux?fname:sname)[i]=pattern[i];
-    }
-    while (i<=LFN_NAMELENGTH) (faux?fname:sname)[i++]=0;
+	int i;
+	if (faux<256) {
+		sattr[faux]=_sattr;
+		for (i=0;i<LFN_NAMELENGTH;i++) {
+			if (pattern[i]==0) break;
+				sname[faux][i]=pattern[i];
+		}
+		while (i<=LFN_NAMELENGTH) sname[faux][i++]=0;
+	}
     for (i=0;i<11;i++) mem_writeb(pt+offsetof(sDTA,spname)+i,0);
 	
     char * find_ext;
@@ -462,11 +466,12 @@ Bit8u DOS_DTA::GetSearchDrive(void) {
 }
 
 void DOS_DTA::GetSearchParams(Bit8u & attr,char * pattern, bool lfn) {
-	attr=(Bit8u)sGet(sDTA,sattr);
-    if (lfn) {
-        memcpy(pattern,faux?fname:sname,LFN_NAMELENGTH);
-           pattern[LFN_NAMELENGTH]=0;
+    if (lfn&&faux<256) {
+		attr=sattr[faux];
+        memcpy(pattern,sname[faux],LFN_NAMELENGTH);
+        pattern[LFN_NAMELENGTH]=0;
     } else {
+		attr=(Bit8u)sGet(sDTA,sattr);
         char temp[11];
         MEM_BlockRead(pt+offsetof(sDTA,spname),temp,11);
         for (int i=0;i<13;i++) pattern[i]=0;

src/dos/dos_devices.cpp
@@ -101,9 +101,9 @@ public:
 };
 
 #if defined(WIN32)
-int captUsed = 0;
+bool lastwrite = false;
 Bit8u *clipAscii = NULL;
-Bit32u clipSize = 0, fPointer;
+Bit32u clipSize = 0, cPointer = 0, fPointer;
 
 void Unicode2Ascii(Bit16u *unicode)
 	{
@@ -171,8 +171,23 @@ class device_CLIP : public DOS_Device {
 private:
 	char	tmpAscii[20];
 	char	tmpUnicode[20];
-	std::string rawdata;				// the raw data sent to LPTx...
+	std::string rawdata;				// the raw data sent to CLIP$...
 	virtual void CommitData() {
+		if (cPointer>0) {
+			if (!clipSize)
+				getClipboard();
+			if (cPointer>clipSize)
+				cPointer=clipSize;
+			if (clipSize) {
+				if (rawdata.capacity() < 100000)
+					rawdata.reserve(100000);
+				std::string temp=std::string((char *)clipAscii).substr(0, cPointer);
+				if (temp[temp.length()-1]!='\n') temp+="\n";
+				rawdata.insert(0, temp);
+				clipSize=0;
+			}
+			cPointer=0;
+		}
 		FILE* fh = fopen(tmpAscii, "wb");			// Append or write to ASCII file
 		if (fh)
 			{
@@ -246,6 +261,7 @@ public:
 			*size = 0;
 			return true;
 		}
+		lastwrite=false;
 		if (!clipSize)																// If no data, we have to read the Windows CLipboard (clipSize gets reset on device close)
 			{
 			getClipboard();
@@ -266,6 +282,7 @@ public:
 			DOS_SetError(DOSERR_ACCESS_DENIED);
 			return false;
 		}
+		lastwrite=true;
 		Bit8u * datasrc = (Bit8u *) data;
 		Bit8u * datadst = (Bit8u *) data;
 
@@ -286,7 +303,6 @@ public:
 			}
 		while (numSpaces--)
 			*(datadst++) = ' ';
-		captUsed += *size;
 		if (Bit16u newsize = (Bit16u)(datadst - data))									// If data
 			{
 			if (rawdata.capacity() < 100000)											// Prevent repetive size allocations
@@ -300,6 +316,7 @@ public:
 			*pos = 0;
 			return true;
 		}
+		lastwrite=false;
 		if (clipSize == 0)																// No data yet
 			{
 			getClipboard();
@@ -328,6 +345,7 @@ public:
 		else if (newPos < 0)
 			newPos = 0;
 		*pos = newPos;
+		cPointer = newPos;
 		fPointer = newPos;
 		return true;
 	}
@@ -336,8 +354,9 @@ public:
 			return false;
 		clipSize = 0;																	// Reset clipboard read
 		rawdata.erase(rawdata.find_last_not_of(" \n\r\t")+1);							// Remove trailing white space
-		if (!rawdata.size())															// Nothing captured/to do
+		if (!rawdata.size()&&!lastwrite)												// Nothing captured/to do
 			return false;
+		lastwrite=false;
 		int len = (int)rawdata.size();
 		if (len > 2 && rawdata[len-3] == 0x0c && rawdata[len-2] == 27 && rawdata[len-1] == 64)	// <ESC>@ after last FF?
 			rawdata.erase(len-2, 2);

src/dos/dos_files.cpp
@@ -50,7 +50,7 @@ Bitu DOS_FILES = 127;
 DOS_File ** Files = NULL;
 DOS_Drive * Drives[DOS_DRIVES] = {NULL};
 bool force = false;
-int sdrive;
+int sdrive, faux = 256;
 
 bool shiftjis_lead_byte(int c);
 
@@ -217,13 +217,16 @@ bool DOS_GetSFNPath(char const * const path,char * SFNPath,bool LFN) {
     char pdir[LFN_NAMELENGTH], *p;
     Bit8u drive;char fulldir[DOS_PATHLENGTH],LFNPath[CROSS_LEN];
     char name[DOS_NAMELENGTH_ASCII], lname[LFN_NAMELENGTH];
-    DOS_DTA dta(dos.dta());
     Bit32u size;Bit16u date;Bit16u time;Bit8u attr;
     if (!DOS_MakeName(path,fulldir,&drive)) return false;
     sprintf(SFNPath,"%c:\\",drive+'A');
     strcpy(LFNPath,SFNPath);
     p = fulldir;
     if (*p==0) return true;
+	RealPt save_dta=dos.dta();
+	dos.dta(dos.tables.tempdta);
+	DOS_DTA dta(dos.dta());
+	int fbak=faux;
     for (char *s = strchr(p,'\\'); s != NULL; s = strchr(p,'\\')) {
 		*s = 0;
 		if (SFNPath[strlen(SFNPath)-1]=='\\')
@@ -233,7 +236,9 @@ bool DOS_GetSFNPath(char const * const path,char * SFNPath,bool LFN) {
 		if (!strrchr(p,'*') && !strrchr(p,'?')) {
 			*s = '\\';
 			p = s + 1;
+			faux=255;
 			if (DOS_FindFirst(pdir,0xffff & DOS_ATTR_DIRECTORY & ~DOS_ATTR_VOLUME,false)) {
+				faux=fbak;
 				dta.GetResult(name,lname,size,date,time,attr);
 				strcat(SFNPath,name);
 				strcat(LFNPath,lname);
@@ -241,7 +246,10 @@ bool DOS_GetSFNPath(char const * const path,char * SFNPath,bool LFN) {
 				strcat(LFNPath,"\\");
 			}
 			else {
-			return false;}
+				faux=fbak;
+				dos.dta(save_dta);
+				return false;
+			}
 		} else {
 			strcat(SFNPath,p);
 			strcat(LFNPath,p);
@@ -254,6 +262,7 @@ bool DOS_GetSFNPath(char const * const path,char * SFNPath,bool LFN) {
     }
     if (p != 0) {
 		sprintf(pdir,"\"%s%s\"",SFNPath,p);
+		faux=255;
 		if (!strrchr(p,'*')&&!strrchr(p,'?')&&DOS_FindFirst(pdir,0xffff & ~DOS_ATTR_VOLUME,false)) {
 			dta.GetResult(name,lname,size,date,time,attr);
 			strcat(SFNPath,name);
@@ -262,7 +271,9 @@ bool DOS_GetSFNPath(char const * const path,char * SFNPath,bool LFN) {
 			strcat(SFNPath,p);
 			strcat(LFNPath,p);
 		}
+		faux=fbak;
     }
+	dos.dta(save_dta);
     if (LFN) strcpy(SFNPath,LFNPath);
     return true;
 }
@@ -374,10 +385,25 @@ bool DOS_Rename(char const * const oldname,char const * const newname) {
 	if (!DOS_MakeName(oldname,fullold,&driveold)) return false;
 	if (!DOS_MakeName(newname,fullnew,&drivenew)) return false;
 	/* No tricks with devices */
+	bool clip=false;
 	if ( (DOS_FindDevice(oldname) != DOS_DEVICES) ||
 	     (DOS_FindDevice(newname) != DOS_DEVICES) ) {
-		DOS_SetError(DOSERR_FILE_NOT_FOUND);
-		return false;
+#if defined (WIN32)
+	if (!control->SecureMode()&&(dos_clipboard_device_access==3||dos_clipboard_device_access==4)) {
+		if (DOS_FindDevice(oldname) == DOS_DEVICES) {
+			char * find_last;
+			find_last=strrchr(fullnew,'\\');
+			if (find_last==NULL) find_last=fullnew;
+			else find_last++;
+			if (!strcasecmp(find_last, *dos_clipboard_device_name?dos_clipboard_device_name:"CLIP$"))
+				clip=true;
+		}
+	}
+#endif
+		if (!clip) {
+			DOS_SetError(DOSERR_FILE_NOT_FOUND);
+			return false;
+		}
 	}
 	/* Must be on the same drive */
 	if(driveold != drivenew) {
@@ -396,7 +422,21 @@ bool DOS_Rename(char const * const oldname,char const * const newname) {
 		return false;
 	}
 
-	if (Drives[drivenew]->Rename(fullold,fullnew)) return true;
+#if defined (WIN32)
+	if (clip) {
+		Bit16u sourceHandle, targetHandle, toread = 0x8000;
+		static Bit8u buffer[0x8000];
+		bool failed = false;
+		if (DOS_OpenFile(oldname,OPEN_READ,&sourceHandle) && DOS_OpenFile(newname,OPEN_WRITE,&targetHandle)) {
+			do {
+				if (!DOS_ReadFile(sourceHandle,buffer,&toread) || !DOS_WriteFile(targetHandle,buffer,&toread)) failed=true;
+			} while (toread == 0x8000);
+			if (!DOS_CloseFile(sourceHandle)||!DOS_CloseFile(targetHandle)) failed=true;
+		} else failed=true;
+		if (!failed&&Drives[drivenew]->FileUnlink(fullold)) return true;
+	} else
+#endif
+		if (Drives[drivenew]->Rename(fullold,fullnew)) return true;
 	/* If it still fails, which error should we give ? PATH NOT FOUND or EACCESS */
 	if (dos.errorcode!=DOSERR_ACCESS_DENIED&&dos.errorcode!=DOSERR_WRITE_PROTECTED) {
 		LOG(LOG_FILES,LOG_NORMAL)("Rename fails for %s to %s, no proper errorcode returned.",oldname,newname);
@@ -815,9 +855,10 @@ bool DOS_GetFileAttr(char const * const name,Bit16u * attr) {
 	if (!control->SecureMode()&&dos_clipboard_device_access) {
 		char * find_last;
 		find_last=strrchr(fullname,'\\');
-		if (find_last!=NULL)
-			if (!strcasecmp(find_last+1, *dos_clipboard_device_name?dos_clipboard_device_name:"CLIP$"))
-				return true;
+		if (find_last==NULL) find_last=fullname;
+		else find_last++;
+		if (!strcasecmp(find_last, *dos_clipboard_device_name?dos_clipboard_device_name:"CLIP$"))
+			return true;
 	}
 #endif
 	if (Drives[drive]->GetFileAttr(fullname,attr)) {

src/dos/dos_misc.cpp
@@ -244,6 +244,10 @@ static bool DOS_MultiplexFunctions(void) {
     case 0x1600:    /* Windows enhanced mode installation check */
         // Leave AX as 0x1600, indicating that neither Windows 3.x enhanced mode, Windows/386 2.x
         // nor Windows 95 are running, nor is XMS version 1 driver installed
+#ifdef WIN32
+		if (!control->SecureMode() && (reg_sp == 0xFFF6 && mem_readw(SegPhys(ss)+reg_sp) == 0x142A || reg_sp >= 0xFF7A && reg_sp <= 0xFF8F && mem_readw(SegPhys(ss)+reg_sp) == reg_sp + 21))
+			reg_ax = 0x301;
+#endif
         return true;
 	case 0x1605:	/* Windows init broadcast */
 		if (enable_a20_on_windows_init) {
@@ -344,6 +348,15 @@ static bool DOS_MultiplexFunctions(void) {
 			return true;
 		}
 		else return false;
+	case 0x160A:
+		// Report Windows version 4.0 (95) to NESTICLE x.xx so that it uses LFN when available
+		if (uselfn && reg_sp == 0x220A && mem_readw(SegPhys(ss)+reg_sp) == 0x1FBB) {
+			reg_ax = 0;
+			reg_bx = 0x400;
+			reg_cx = 2;
+			return true;
+		}
+		return false;
 	case 0x1680:	/*  RELEASE CURRENT VIRTUAL MACHINE TIME-SLICE */
 		//TODO Maybe do some idling but could screw up other systems :)
 		return true; //So no warning in the debugger anymore
@@ -359,7 +372,11 @@ static bool DOS_MultiplexFunctions(void) {
 		return true;
 	case 0x1701:
 		if(control->SecureMode()) return false;
-		reg_ax=OpenClipboard(NULL)?1:0;
+		reg_ax=0;
+		if (OpenClipboard(NULL)) {
+			reg_ax=1;
+			CloseClipboard();
+		}
 		return true;
 	case 0x1702:
 		if(control->SecureMode()) return false;
@@ -401,6 +418,8 @@ static bool DOS_MultiplexFunctions(void) {
 				reg_ax=(Bit16u)strlen((char *)text)+1;
 				reg_dx=(Bit16u)((strlen((char *)text)+1)/65536);
 				}
+			else
+				reg_dx=0;
 			CloseClipboard();
 			}
 		return true;
@@ -419,7 +438,8 @@ static bool DOS_MultiplexFunctions(void) {
 		return true;
 	case 0x1708:
 		if(control->SecureMode()) return false;
-		reg_ax=CloseClipboard()?1:0;
+		reg_ax=1;
+		CloseClipboard();
 		return true;
 #endif
     case 0x1a00:    /* ANSI.SYS installation check (MS-DOS 4.0 or higher) */

src/dos/dos_programs.cpp
@@ -49,6 +49,7 @@
 bool Mouse_Drv=true;
 bool Mouse_Vertical = false;
 bool force_nocachedir = false;
+bool freesizecap = true;
 bool wpcolon = true;
 
 void DOS_EnableDriveMenu(char drv);
@@ -314,20 +315,15 @@ public:
             Bit8u mediaid;
             std::string str_size;
             if (type=="floppy") {
-                str_size="512,1,2880,2880";/* All space free */
+                str_size="512,1,2880,2880";
                 mediaid=0xF0;       /* Floppy 1.44 media */
             } else if (type=="dir") {
                 // 512*32*32765==~500MB total size
                 // 512*32*16000==~250MB total free size
-#if defined(__WIN32__) && !defined(C_SDL2) && !defined(HX_DOS)
-                GetDefaultSize();
-                str_size=hdd_size;
-#else
-                str_size="512,32,32765,16000";
-#endif
+                str_size="512,32,0,0";
                 mediaid=0xF8;       /* Hard Disk */
             } else if (type=="cdrom") {
-                str_size="2048,1,65535,0";
+                str_size="2048,1,32765,0";
                 mediaid=0xF8;       /* Hard Disk */
             } else {
                 WriteOut(MSG_Get("PROGAM_MOUNT_ILL_TYPE"),type.c_str());
@@ -340,14 +336,18 @@ public:
                 Bit16u freesize = static_cast<Bit16u>(atoi(mb_size.c_str()));
                 if (type=="floppy") {
                     // freesize in kb
-                    sprintf(teststr,"512,1,2880,%d",freesize*1024/(512*1));
+                    sprintf(teststr,"512,1,2880,%d",freesize*1024/(512*1)>2880?2880:freesize*1024/(512*1));
                 } else {
+					if (freesize>1919) freesize=1919;
+					Bit16u numc=type=="cdrom"?1:32;
                     Bit32u total_size_cyl=32765;
-                    Bit32u free_size_cyl=(Bit32u)freesize*1024*1024/(512*32);
+					Bit32u tmp=(Bit32u)freesize*1024*1024/(type=="cdrom"?2048*1:512*32);
+					if (tmp>65534) numc=type=="cdrom"?(tmp+65535)/65536:64;
+                    Bit32u free_size_cyl=(Bit32u)freesize*1024*1024/(numc*(type=="cdrom"?2048:512));
                     if (free_size_cyl>65534) free_size_cyl=65534;
                     if (total_size_cyl<free_size_cyl) total_size_cyl=free_size_cyl+10;
                     if (total_size_cyl>65534) total_size_cyl=65534;
-                    sprintf(teststr,"512,32,%u,%u",total_size_cyl,free_size_cyl);
+                    sprintf(teststr,type=="cdrom"?"2048,%u,%u,%u":"512,%u,%u,%u",numc,total_size_cyl,free_size_cyl);
                 }
                 str_size=teststr;
             }
@@ -517,7 +517,11 @@ public:
                 if (is_physfs) {
                     LOG_MSG("ERROR:This build does not support physfs");
                 } else {
-                    newdrive  = new cdromDrive(drive,temp_line.c_str(),sizes[0],bit8size,sizes[2],0,mediaid,error);
+					if (Drives[drive-'A']) {
+						WriteOut(MSG_Get("PROGRAM_MOUNT_ALREADY_MOUNTED"),drive,Drives[drive-'A']->GetInfo());
+						return;
+					}
+                    newdrive  = new cdromDrive(drive,temp_line.c_str(),sizes[0],bit8size,sizes[2],sizes[3],mediaid,error);
                 }
                 // Check Mscdex, if it worked out...
                 switch (error) {
@@ -593,9 +597,9 @@ public:
         return;
 showusage:
 #if defined (WIN32) || defined(OS2)
-       WriteOut(MSG_Get("PROGRAM_MOUNT_USAGE"),"d:\\dosprogs","d:\\dosprogs","d:\\dosprogs","d:\\dosprogs");
+       WriteOut(MSG_Get("PROGRAM_MOUNT_USAGE"),"d:\\dosprogs","d:\\dosprogs","d:\\dosprogs","d:\\dosprogs","d:\\dosprogs");
 #else
-       WriteOut(MSG_Get("PROGRAM_MOUNT_USAGE"),"~/dosprogs","~/dosprogs","~/dosprogs","~/dosprogs");         
+       WriteOut(MSG_Get("PROGRAM_MOUNT_USAGE"),"~/dosprogs","~/dosprogs","~/dosprogs","~/dosprogs","~/dosprogs");
 #endif
         return;
     }
@@ -4530,9 +4534,10 @@ void DOS_SetupPrograms(void) {
         "This makes the directory %s act as the C: drive inside DOSBox-X.\n"
         "The directory has to exist.\n"
 		"Options are accepted. For example:\n"
-		"MOUNT -nocachedir c %s will mount C: without caching the drive.\n"
-		"MOUNT -ro c %s will mount the C: drive in read-only mode.\n"
-		"MOUNT -u c will unmount the C: drive.\n");
+		"MOUNT -nocachedir c %s mounts C: without caching the drive.\n"
+		"MOUNT -freesize 128 c %s mounts C: with the specified free disk space.\n"
+		"MOUNT -ro c %s mounts the C: drive in read-only mode.\n"
+		"MOUNT -u c unmounts the C: drive.\n");
     MSG_Add("PROGRAM_MOUNT_UMOUNT_NOT_MOUNTED","Drive %c isn't mounted.\n");
     MSG_Add("PROGRAM_MOUNT_UMOUNT_SUCCESS","Drive %c has successfully been removed.\n");
     MSG_Add("PROGRAM_MOUNT_UMOUNT_NO_VIRTUAL","Virtual Drives can not be unMOUNTed.\n");

commit a302de3d24ffb886698a9530e66acbba994f92bf

shell_cmds: Fix number overflow
For an output of any number of 1000GB or more 16 bytes is not enough to
represent it: "1,000,000,000,000" takes 18 bytes including the NULL
terminator. Increase the buffer size to something overkill like 64 so we
never have to think of this situation again.

Strangely this was found to crash MinGW builds on Windows but not on
other compilers or platforms. Shortening the numformat to 5 easily
reproduces the crash on my system.

Closes #3644 https://github.com/joncampbell123/dosbox-x/issues/3644

Reported-by: gkswngh05 (https://github.com/gkswngh05)
Fixes: bd7a7293194747ee9ae6492a1a5c5dec9a4b398a

src/shell/shell_cmds.cpp
@@ -1823,7 +1823,7 @@ static bool doDir(DOS_Shell * shell, char * args, DOS_DTA dta, char * numformat,
 
 void DOS_Shell::CMD_DIR(char * args) {
 	HELP("DIR");
-	char numformat[16];
+	char numformat[64];
 	char path[DOS_PATHLENGTH];
 	char sargs[CROSS_LEN];
 