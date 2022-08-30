51d9169883e5f02f24407200aacc26791a01eafe
Wengier <wengierwu@yahoo.com>
2022-08-29 23:42:58 -0400

    update version and timestamp

--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,4 +1,4 @@
 /*auto-generated*/
-#define UPDATED_STR "Aug 24, 2022 11:23:02pm"
-#define GIT_COMMIT_HASH "0789813"
+#define UPDATED_STR "Aug 29, 2022 11:36:13pm"
+#define GIT_COMMIT_HASH "5b5f099"
 #define COPYRIGHT_END_YEAR "2022"
