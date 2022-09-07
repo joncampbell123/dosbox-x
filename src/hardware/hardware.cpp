6f34005f73fa056334206abad011072e4be31658
Jonathan Campbell <jonathan@castus.tv>
2022-09-06 21:47:30 -0700

    Despite FFMPEG API changes, make it work with FFMPEG 4.3

--- src/hardware/hardware.cpp
+++ src/hardware/hardware.cpp
@@ -1157,6 +1157,7 @@ skip_shot:
 			}
 			ffmpeg_vid_ctx = avcodec_alloc_context3(ffmpeg_vid_codec);
 			if (ffmpeg_vid_ctx == NULL) E_Exit("Error: Unable to open vid context");
+			ffmpeg_vid_stream->codec = ffmpeg_vid_ctx; // NTS: This is required in FFMPEG 4.3 to make the encoder work
 			ffmpeg_vid_ctx->bit_rate = 25000000; // TODO: make configuration option!
 			ffmpeg_vid_ctx->keyint_min = 15; // TODO: make configuration option!
 			ffmpeg_vid_ctx->time_base.num = 1000000;
@@ -1204,6 +1205,7 @@ skip_shot:
 			}
 			ffmpeg_aud_ctx = avcodec_alloc_context3(ffmpeg_aud_codec);
 			if (ffmpeg_aud_ctx == NULL) E_Exit("Error: Unable to open aud context");
+			ffmpeg_aud_stream->codec = ffmpeg_aud_ctx; // NTS: This is required in FFMPEG 4.3 to make the encoder work
 			ffmpeg_aud_ctx->sample_rate = (int)capture.video.audiorate;
 			ffmpeg_aud_ctx->channels = 2;
 			ffmpeg_aud_ctx->flags = 0; // do not use global headers
