67a21f928b1e0dcfc0461f8ea209fb306451065d
Jookia <contact@jookia.org>
2022-09-10 07:56:34 +1000

    sdl2: Disable libudev support outside Linux
    The SDL2 code assumes these are only on Linux.

--- vs/sdl2/build-dosbox.sh
+++ vs/sdl2/build-dosbox.sh
@@ -40,7 +40,8 @@ if [ "$1" == "hx-dos" ]; then
 opts="--disable-video-opengl"
 fi
 if [ "$sys" != "Linux" ]; then
-opts="--disable-video-wayland"
+# These are supported on BSDs but the SDL2 code assumes Linux
+opts="--disable-video-wayland --disable-libudev"
 fi
 
 ac_cv_header_iconv_h=no ac_cv_func_iconv=no ac_cv_lib_iconv_libiconv_open=no ../configure "--srcdir=$srcdir" "--prefix=$instdir" --enable-static --disable-shared --disable-x11-shared --disable-video-x11-xrandr --disable-video-x11-vm --disable-video-x11-xv $opts || exit 1
