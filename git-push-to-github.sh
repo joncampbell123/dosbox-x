054b9e6686a5864594606cecf50bc4d58d80e1d1
Torinde <97228894+Torinde@users.noreply.github.com>
2022-12-09 09:45:29 +0100

    BSD

--- git-push-to-github.sh
+++ git-push-to-github.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 git remote add github https://joncampbell123@github.com/joncampbell123/dosbox-x.git # might fail
 git remote set-url github https://joncampbell123@github.com/joncampbell123/dosbox-x.git # might fail
 git push github master
