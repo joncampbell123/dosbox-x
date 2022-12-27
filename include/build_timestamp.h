da59770c52df2768523432a1d368735ea7c42140
Jonathan Campbell <jonathan@castus.tv>
2022-12-26 16:59:12 -0800

    Prepare for release

--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,4 +1,4 @@
 /*auto-generated*/
-#define UPDATED_STR "Dec 25, 2022 1:15:16pm"
-#define GIT_COMMIT_HASH "1736c3b"
+#define UPDATED_STR "Dec 26, 2022 4:51:47pm"
+#define GIT_COMMIT_HASH "abbadb3"
 #define COPYRIGHT_END_YEAR "2022"
