f87a3b83373e0ef276a9942dbadee81d2c6b55f7
maron2000 <68574602+maron2000@users.noreply.github.com>
2023-02-15 21:10:40 +0900

    Fix int13 AH=02h broken conditional branch

--- src/ints/bios_disk.cpp
+++ src/ints/bios_disk.cpp
@@ -1866,7 +1866,7 @@ static Bitu INT13_DiskHandler(void) {
             CALLBACK_SCF(true);
             return CBRET_NONE;
         }
-        if (drivenum >= MAX_DISK_IMAGES && imageDiskList[drivenum] == NULL) {
+        if (drivenum >= MAX_DISK_IMAGES || imageDiskList[drivenum] == NULL) {
             if (drivenum >= DOS_DRIVES || !Drives[drivenum] || Drives[drivenum]->isRemovable()) {
                 reg_ah = 0x01;
                 CALLBACK_SCF(true);
