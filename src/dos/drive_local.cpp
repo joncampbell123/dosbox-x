e4aa05fddc3c82f7fc13f3e5e60aa6744d8bac22
nanshiki <support@nanshiki.co.jp>
2023-02-18 12:31:45 +0900

    Fixed a bug that a list of files in a folder with Japanese name which has 0x5c,0x61-0x7a at the second byte of the character could not be referred to.

--- src/dos/drive_local.cpp
+++ src/dos/drive_local.cpp
@@ -1688,14 +1688,25 @@ bool localDrive::FindFirst(const char * _dir,DOS_DTA & dta,bool fcb_findfirst) {
 	strcat(tempDir,_dir);
 	CROSS_FILENAME(tempDir);
 
-	for (unsigned int i=0;i<strlen(tempDir);i++) tempDir[i]=toupper(tempDir[i]);
+	size_t len = strlen(tempDir);
+#if defined (WIN32)
+	bool lead = false;
+#endif
+	for (unsigned int i=0;i<len;i++) {
+#if defined (WIN32)
+		if(lead) lead = false;
+		else if((IS_PC98_ARCH || isDBCSCP()) && isKanji1(tempDir[i])) lead = true;
+		else 
+#endif
+		tempDir[i]=toupper(tempDir[i]);
+	}
     if (nocachedir) EmptyCache();
 
 	if (allocation.mediaid==0xF0 ) {
 		EmptyCache(); //rescan floppie-content on each findfirst
 	}
 
-	if (tempDir[strlen(tempDir)-1]!=CROSS_FILESPLIT) {
+	if (!check_last_split_char(tempDir, len, CROSS_FILESPLIT)) {
 		char end[2]={CROSS_FILESPLIT,0};
 		strcat(tempDir,end);
 	}
@@ -1754,7 +1765,7 @@ bool localDrive::FindNext(DOS_DTA & dta) {
     char full_name[CROSS_LEN], lfull_name[LFN_NAMELENGTH+1];
     char dir_entcopy[CROSS_LEN], ldir_entcopy[CROSS_LEN];
 
-    uint8_t srch_attr;char srch_pattern[LFN_NAMELENGTH];
+    uint8_t srch_attr;char srch_pattern[LFN_NAMELENGTH+1];
 	uint8_t find_attr;
 
     dta.GetSearchParams(srch_attr,srch_pattern,false);
@@ -2162,7 +2173,7 @@ bool localDrive::TestDir(const char * dir) {
 
 	// Skip directory test, if "\"
 	size_t len = strlen(newdir);
-	if (len && (newdir[len-1]!='\\')) {
+	if (len && !check_last_split_char(newdir, len, '\\')) {
 		// It has to be a directory !
 		ht_stat_t test;
 		if (ht_stat(host_name,&test))		return false;
