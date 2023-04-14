f11408c5a4d2e95d62329c7e37fdee6b79eddf1d
Jonathan Campbell <jonathan@castus.tv>
2023-04-14 13:03:31 -0700

    Fix info block size and make sure SDA and a specific member of the SDA struct are placed at exactly the offset expected by Microsoft software

--- src/dos/dos.cpp
+++ src/dos/dos.cpp
@@ -4211,8 +4211,23 @@ public:
         DOS_IHSEG = DOS_GetMemory(1,"DOS_IHSEG");
 
         /* DOS_INFOBLOCK_SEG contains the entire List of Lists, though the INT 21h call returns seg:offset with offset nonzero */
-        DOS_INFOBLOCK_SEG = DOS_GetMemory(0xC0,"DOS_INFOBLOCK_SEG");	// was 0x80
-
+	/* NTS: DOS_GetMemory() allocation sizes are in PARAGRAPHS (16-byte units) not bytes */
+	/* NTS: DOS_INFOBLOCK_SEG must be 0x20 paragraphs, CONDRV_SEG 0x08 paragraphs, and CONSTRING_SEG 0x0A paragraphs.
+	 *      The total combined size of INFOBLOCK+CONDRV+CONSTRING must be 0x32 paragraphs.
+	 *      SDA_SEG must be located at INFOBLOCK_SEG+0x32, so that the current PSP segment parameter is exactly at
+	 *      memory location INFOBLOCK_SEG:0x330. The reason for this has to do with Microsoft "Genuine MS-DOS detection"
+	 *      code in QuickBasic 7.1 and other programs designed to thwart DR-DOS at the time.
+	 *
+	 *      This is probably why DOSBox SVN hardcoded segments in the first place.
+	 *
+	 *      See also:
+	 *
+	 *      [https://www.os2museum.com/wp/how-to-void-your-valuable-warranty/]
+         *      [https://www.os2museum.com/files/drdos_detect.txt]
+         *      [https://www.os2museum.com/wp/about-that-warranty/]
+         *      [https://github.com/joncampbell123/dosbox-x/issues/3626]
+	 */
+        DOS_INFOBLOCK_SEG = DOS_GetMemory(0x20,"DOS_INFOBLOCK_SEG");	// was 0x80
         DOS_CONDRV_SEG = DOS_GetMemory(0x08,"DOS_CONDRV_SEG");		// was 0xA0
         DOS_CONSTRING_SEG = DOS_GetMemory(0x0A,"DOS_CONSTRING_SEG");	// was 0xA8
         DOS_SDA_SEG = DOS_GetMemory(DOS_SDA_SEG_SIZE>>4,"DOS_SDA_SEG");		// was 0xB2  (0xB2 + 0x56 = 0x108)
