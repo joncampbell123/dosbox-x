f3a112322412ac761a30d676fd9277fa8fe7bbb7
maron2000 <68574602+maron2000@users.noreply.github.com>
2023-12-30 20:00:59 +0900

    Fix a typo in log

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -8719,7 +8719,7 @@ int main(int argc, char* argv[]) SDL_MAIN_NOEXCEPT {
 #if defined(C_SDL2)
         SDL_version sdl_version;
         SDL_GetVersion(&sdl_version);
-        LOG_MSG("SDL: version %d.%d.%d, Video %s, Audio %s)",
+        LOG_MSG("SDL: version %d.%d.%d, Video %s, Audio %s",
             sdl_version.major, sdl_version.minor, sdl_version.patch,
             SDL_GetCurrentVideoDriver(), SDL_GetCurrentAudioDriver());
 #endif //defined (C_SDL2)
