a8efb95a721ba52ae702035f690e9d5b5f47767e
Jonathan Campbell <jonathan@castus.tv>
2024-03-01 00:04:45 -0800

    Prepare for release

--- contrib/windows/installer/DOSBox-X-installer.iss
+++ contrib/windows/installer/DOSBox-X-installer.iss
@@ -1,5 +1,5 @@
 ﻿#define MyAppName "DOSBox-X"
-#define MyAppVersion "2023.10.06"
+#define MyAppVersion "2024.03.01"
 #define MyAppBit "(32/64bit for Windows)"
 #define MyAppPublisher "joncampbell123 [DOSBox-X Team]"
 #define MyAppURL "https://dosbox-x.com/"
--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,4 +1,4 @@
 /*auto-generated*/
-#define UPDATED_STR "Oct 6, 2023 3:33:20pm"
-#define GIT_COMMIT_HASH "59744fe"
-#define COPYRIGHT_END_YEAR "2023"
+#define UPDATED_STR "Mar 1, 2024 12:00:26am"
+#define GIT_COMMIT_HASH "a1610c0"
+#define COPYRIGHT_END_YEAR "2024"
