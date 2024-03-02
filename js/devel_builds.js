d19c33e343aabad8b5ee9a4a2d33e027fb54aa7e
Liu Wenyuan <15816141883@163.com>
2024-03-02 20:45:27 +0800

    Cleanup devel_builds to get rid of then/catch hell

--- js/devel_builds.js
+++ js/devel_builds.js
@@ -1,25 +1,25 @@
 /*
-  *  SPDX-License-Identifier: CC-BY-SA-4.0
+  * SPDX-License-Identifier: CC-BY-SA-4.0
   *
-  *  Copyright (C) 2022-2023 The DOSBox Staging Team
-  *  Copyright (C) 2023 The DOSBox-X Team
+  * Copyright (C) 2022-2023 The DOSBox Staging Team
+  * Copyright (C) 2023-2024 The DOSBox-X Team
 */
 
 /*
-  * Helper functions for populating the contents of
-  * the build status table at devel-build.html
+  * Helper functions for populating the contents of the build
+  * status table in devel-build.html
 */
 
-function report_error(msg, e, show_alert=true, show_trace=false) {
-    let stack = e ? "\n" + (e instanceof Error ? e.stack : e) : "";
-    if (e)
-        console.error(e);
+function report_error(msg, err, show_alert=false, show_trace=false) {
+    let stack = err ? "\n" + (err instanceof Error ? err.stack : err) : "";
+    if (err)
+        console.error(err);
     console.info(msg);
     if (show_alert)
         alert(msg + (show_trace ? stack : ""));
 }
 
-function add_ci_build_entry(repo, workflow_id, description) {
+async function add_ci_build_entry(repo, workflow_id, description) {
     let builds = document.querySelector("#builds");
     let build_entry_id = "build-" + workflow_id;
     let build_entry = builds.querySelector("#" + build_entry_id);
@@ -38,82 +38,80 @@ function add_ci_build_entry(repo, workflow_id, description) {
         build_status_el.innerText = text;
     }
 
-    let page = 1;
-    let per_page = 1;
+    const query_url = new URL("https://api.github.com/repos/" + repo + "/" + "actions/workflows/" + workflow_id + ".yml" + "/runs");
+    query_url.searchParams.set("page", 1);
+    query_url.searchParams.set("per_page", 1);
+    query_url.searchParams.set("branch", "master");
+    query_url.searchParams.set("event", "push");
+    query_url.searchParams.set("status", "success");
 
-    let filter_branch = "master";
-    let filter_event = "push";
-    let filter_status = "success";
-
-        const queryParams = new URLSearchParams();
-    queryParams.set("page", page);
-    queryParams.set("per_page", per_page);
-    queryParams.set("branch", filter_branch);
-    queryParams.set("event", filter_event);
-    queryParams.set("status", filter_status);
-
-    const gh_api_url = "https://api.github.com/repos/" + repo + "/";
-
-    fetch(gh_api_url + "actions/workflows/" + workflow_id + ".yml" + "/runs?" +
-        queryParams.toString())
-    .then((response) => {
-        // Handle HTTP error
-        if (!response.ok) {
-            function show_generic_error(e) {
-                if (e)
-                    console.error(e);
-                report_error(`Looks like there was a problem while getting the info of "${description}". ` +
-                `Status code: ${response.status}`, null, false);
-                report_error_as_build_link(`Got error (status code: ${response.status})`);
-            }
-            if (response.status == 403) {
-                response.json()
-                .then((data) => {
-                    if (data.message && data.message.includes("API rate limit exceeded")) {
-                        report_error(`GitHub API rate limit while getting the info of "${description}".`, data, false);
-                        report_error_as_build_link("API rate limit exceeded");
-                    } else {
-                        show_generic_error();
-                    }
-                })
-                .catch(show_generic_error);
-            } else {
-                show_generic_error();
-            }
+    let response;
+    try {
+        response = await fetch(query_url.href);
+    } catch (err) {
+        report_error(`Error ocurred for "${description}" :-S`, err);
+        report_error_as_build_link("Network error");
+        return;
+    }
+    let data;
+    async function parse_response(handle_err) {
+        try {
+            data = await response.json();
+        } catch (err) {
+            if (!handle_err) throw err;
+            report_error(`Response to query of "${description}" can't be parsed as JSON`, err);
+            report_error_as_build_link("Response invalid");
             return;
         }
+    }
 
-        response.json()
-        .then((data) => {
-            const status = data.workflow_runs.length && data.workflow_runs[0];
-
-            // If result not found, query the next page
-            if (!status) {
-                show_generic_error("Result not found");
-                return;
+    // Handle HTTP error
+    if (!response.ok) {
+        function show_generic_error(err) {
+            report_error(`Looks like there was a problem while getting the info of "${description}". ` +
+            `Status code: ${response.status}`, err);
+            report_error_as_build_link(`HTTP error (status code: ${response.status})`);
+        }
+        if (response.status == 403) {
+            try {
+                await parse_response(false);
+                if (data.message && data.message.includes("API rate limit exceeded")) {
+                    report_error(`GitHub API rate limit while getting the info of "${description}".`, data);
+                    report_error_as_build_link("API rate limit exceeded");
+                } else {
+                    show_generic_error(data);
+                }
+            } catch (err) {
+                show_generic_error(err);
             }
+        } else {
+            show_generic_error();
+        }
+        return;
+    }
 
-            // Update HTML elements
-            let build_link = document.createElement("a");
-            build_link.setAttribute("href", status.html_url);
-            build_link.textContent = description;
-            let build_link_el = build_entry.querySelector(".build-link");
-            build_link_el.replaceChildren(build_link);
+    await parse_response();
+    const run = data.workflow_runs.length && data.workflow_runs[0];
+    if (!run) {
+        report_error(`No builds found for "${description}"`, data);
+        report_error_as_build_link("No builds found");
+        return;
+    }
 
-            let build_commit = status.head_commit.id;
-            let build_commit_link = document.createElement("a");
-            build_commit_link.setAttribute("href", `https://github.com/${repo}/commit/${build_commit}`);
-            build_commit_link.textContent = build_commit.substring(0, 7);
-            let commit_el = build_entry.querySelector(".build-commit");
-            commit_el.replaceChildren(build_commit_link);
+    // Setup row contents
+    let build_link = document.createElement("a");
+    build_link.setAttribute("href", run.html_url);
+    build_link.textContent = description;
+    build_entry.querySelector(".build-link")
+        .replaceChildren(build_link);
 
-            let build_date = new Date(status.updated_at);
-            let date_el = build_entry.querySelector(".build-date");
-            date_el.textContent = build_date.toUTCString();
-        });
-    })
-    .catch((err) => {
-        report_error(`Error ocurred for "${description}" :-S`, err, false);
-        report_error_as_build_link("Network error");
-    });
+    let build_commit = run.head_commit.id;
+    let build_commit_link = document.createElement("a");
+    build_commit_link.setAttribute("href", `https://github.com/${repo}/commit/${build_commit}`);
+    build_commit_link.textContent = build_commit.substring(0, 7);
+    build_entry.querySelector(".build-commit")
+        .replaceChildren(build_commit_link);
+
+    build_entry.querySelector(".build-date")
+        .textContent = new Date(run.updated_at).toUTCString();
 }
