3edb631b385681d81700bac747fcc05cd004f3e3
Liu Wenyuan <15816141883@163.com>
2024-03-02 21:16:02 +0800

    Generate devel-build table in Liquid
    So at least NoScripted users can have something to look at

--- js/devel_builds.js
+++ js/devel_builds.js
@@ -6,39 +6,36 @@
 */
 
 /*
-  * Helper functions for populating the contents of the build
-  * status table in devel-build.html
+  * Code for populating the contents of the build status table
+  * in devel-build.html
 */
 
 function report_error(msg, err, show_alert=false, show_trace=false) {
     let stack = err ? "\n" + (err instanceof Error ? err.stack : err) : "";
     if (err)
-        console.error(err);
-    console.info(msg);
+        console.error(msg, err);
+    else
+        console.error(msg);
     if (show_alert)
         alert(msg + (show_trace ? stack : ""));
 }
 
-async function add_ci_build_entry(repo, workflow_id, description) {
-    let builds = document.querySelector("#builds");
-    let build_entry_id = "build-" + workflow_id;
-    let build_entry = builds.querySelector("#" + build_entry_id);
-
-    if (!build_entry) {
-        build_entry = document.importNode(document.querySelector("#build-template").content.querySelector("tr"), true);
-        build_entry.setAttribute("id", build_entry_id);
-        builds.appendChild(build_entry);
-        let build_name_el = build_entry.querySelector(".build-loading-name");
-        build_name_el.innerText = description;
-    }
+async function populate_workflow_entry(builds, build_entry) {
+    let build_status_el = build_entry.querySelector(".build-link").appendChild(
+        document.querySelector("#build-loading-status-template").content
+            .querySelector(".build-loading-status").cloneNode(true)
+    );
 
     function report_error_as_build_link(text) {
-        let build_status_el = build_entry.querySelector(".build-loading-status");
         build_status_el.style.color = "red";
         build_status_el.innerText = text;
     }
 
-    const query_url = new URL("https://api.github.com/repos/" + repo + "/" + "actions/workflows/" + workflow_id + ".yml" + "/runs");
+    let repo = builds.dataset.repo,
+        workflow = build_entry.dataset.workflow,
+        description = build_entry.dataset.description;
+
+    const query_url = new URL(`https://api.github.com/repos/${repo}/actions/workflows/${workflow}.yml/runs`);
     query_url.searchParams.set("page", 1);
     query_url.searchParams.set("per_page", 1);
     query_url.searchParams.set("branch", "master");
@@ -59,7 +56,7 @@ async function add_ci_build_entry(repo, workflow_id, description) {
             data = await response.json();
         } catch (err) {
             if (!handle_err) throw err;
-            report_error(`Response to query of "${description}" can't be parsed as JSON`, err);
+            report_error(`Response to query of "${description}" (${workflow}) can't be parsed as JSON`, err);
             report_error_as_build_link("Response invalid");
             return;
         }
@@ -68,7 +65,7 @@ async function add_ci_build_entry(repo, workflow_id, description) {
     // Handle HTTP error
     if (!response.ok) {
         function show_generic_error(err) {
-            report_error(`Looks like there was a problem while getting the info of "${description}". ` +
+            report_error(`Looks like there was a problem while getting the info of "${description}" (${workflow}). ` +
             `Status code: ${response.status}`, err);
             report_error_as_build_link(`HTTP error (status code: ${response.status})`);
         }
@@ -76,8 +73,8 @@ async function add_ci_build_entry(repo, workflow_id, description) {
             try {
                 await parse_response(false);
                 if (data.message && data.message.includes("API rate limit exceeded")) {
-                    report_error(`GitHub API rate limit while getting the info of "${description}".`, data);
-                    report_error_as_build_link("API rate limit exceeded");
+                    report_error(`API rate limit reached while getting the info of "${description}" (${workflow}).`, data);
+                    report_error_as_build_link("API rate limit reached");
                 } else {
                     show_generic_error(data);
                 }
@@ -93,7 +90,7 @@ async function add_ci_build_entry(repo, workflow_id, description) {
     await parse_response();
     const run = data.workflow_runs.length && data.workflow_runs[0];
     if (!run) {
-        report_error(`No builds found for "${description}"`, data);
+        report_error(`No builds found for "${description}" (${workflow})`, data);
         report_error_as_build_link("No builds found");
         return;
     }
@@ -114,4 +111,10 @@ async function add_ci_build_entry(repo, workflow_id, description) {
 
     build_entry.querySelector(".build-date")
         .textContent = new Date(run.updated_at).toUTCString();
+}
+
+let builds = document.querySelector("#builds");
+for (let build_entry of builds.querySelectorAll("tr")) {
+    if (build_entry.dataset.workflow)
+        populate_workflow_entry(builds, build_entry);
 }
