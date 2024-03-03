a137271e26f1998951da4cd4c05368c8a873fff0
Liu Wenyuan <15816141883@163.com>
2024-03-03 11:32:45 +0800

    Fake SPA cleanup 1

--- js/wiki_fake_spa.js
+++ js/wiki_fake_spa.js
@@ -2,45 +2,41 @@
 "use strict";
 
 (function() {
-
-    if (localStorage.getItem("wiki_fake_spa_opt_out")) return;
+    if (localStorage && localStorage.getItem("wiki_fake_spa_opt_out")) return;
 
     let contentFrame = document.getElementsByName("bottom")[0];
 
     let myDir = location.pathname;
     myDir = myDir.substring(0, myDir.lastIndexOf("/")) + "/";
 
-    function changeHash(e) {
-        let theirWindow = contentFrame.contentWindow;
-        if (theirWindow.location.host != location.host) return;
-        if (!theirWindow.location.pathname.startsWith(myDir)) return;
-        let relUrl = "#" + theirWindow.location.pathname.substring(myDir.length) + theirWindow.location.search + theirWindow.location.hash;
-        if (location.hash != relUrl) {
+    function changeHash() {
+        let bottomLocation = contentFrame.contentWindow.location;
+        if (bottomLocation.host != location.host || !bottomLocation.pathname.startsWith(myDir))
+            return;
+        let relUrl = "#" + bottomLocation.pathname.substring(myDir.length) + bottomLocation.search + bottomLocation.hash;
+        if (decodeURI(location.hash) != relUrl) {
             const url = new URL(location);
             url.hash = relUrl;
             history.pushState({}, "", url);
         }
     }
-    function changeBottomUrl(e) {
-        if (e && contentFrame.contentDocument.readyState != "complete") return;
+    function changeBottomUrl() {
         let here = location.protocol + "//" + location.host + myDir;
         let bottomLocation = contentFrame.contentWindow.location;
         let dest = location.hash.substring(1);
         let what = new URL(dest, here), whatMutated = new URL(dest, here);
         if (!dest.includes("/") && !whatMutated.pathname.endsWith(".html")) whatMutated.pathname += ".html";
-        if (what.href != bottomLocation.href && whatMutated.href != bottomLocation.href) bottomLocation.replace(what);
+        if (what.href != bottomLocation.href && whatMutated.href != bottomLocation.href)
+            bottomLocation.replace(what);
     }
 
-    if (location.hash) {
+    if (location.hash)
         changeBottomUrl();
-    }
     window.addEventListener("hashchange", changeBottomUrl);
     contentFrame.addEventListener("load", () => {
-        if (contentFrame.contentDocument) {
-            contentFrame.contentWindow.addEventListener("popstate", changeHash);
-            contentFrame.contentWindow.addEventListener("hashchange", changeHash);
-            changeHash();
-        }
+        if (!contentFrame.contentDocument)
+            return;
+        changeHash();
+        contentFrame.contentWindow.addEventListener("popstate", changeHash);
     });
-
 })();
