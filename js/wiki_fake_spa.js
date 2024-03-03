d8c81fb70b6fcda04f3c641aa8d962515ae23acc
Liu Wenyuan <15816141883@163.com>
2024-03-03 12:01:51 +0800

    Sloppy antiflash

--- js/wiki_fake_spa.js
+++ js/wiki_fake_spa.js
@@ -4,16 +4,17 @@
 (function() {
     if (localStorage && localStorage.getItem("wiki_fake_spa_opt_out")) return;
 
-    let contentFrame = document.getElementsByName("bottom")[0];
+    let frame = document.querySelector("main").appendChild(document.querySelector("#empty-iframe").content.querySelector("iframe"));
 
-    let myDir = location.pathname;
-    myDir = myDir.substring(0, myDir.lastIndexOf("/")) + "/";
+    let contentDir = location.pathname;
+    contentDir = contentDir.substring(0, contentDir.lastIndexOf("/") + 1);
+    let contentDirUrl = location.protocol + "//" + location.host + contentDir;
 
     function changeHash() {
-        let bottomLocation = contentFrame.contentWindow.location;
-        if (bottomLocation.host != location.host || !bottomLocation.pathname.startsWith(myDir))
+        let bottomLocation = frame.contentWindow.location;
+        if (bottomLocation.host != location.host || !bottomLocation.pathname.startsWith(contentDir))
             return;
-        let relUrl = "#" + bottomLocation.pathname.substring(myDir.length) + bottomLocation.search + bottomLocation.hash;
+        let relUrl = "#" + bottomLocation.pathname.substring(contentDir.length) + bottomLocation.search + bottomLocation.hash;
         if (decodeURI(location.hash) != relUrl) {
             const url = new URL(location);
             url.hash = relUrl;
@@ -21,22 +22,22 @@
         }
     }
     function changeBottomUrl() {
-        let here = location.protocol + "//" + location.host + myDir;
-        let bottomLocation = contentFrame.contentWindow.location;
+        let bottomLocation = frame.contentWindow.location;
         let dest = location.hash.substring(1);
-        let what = new URL(dest, here), whatMutated = new URL(dest, here);
-        if (!dest.includes("/") && !whatMutated.pathname.endsWith(".html")) whatMutated.pathname += ".html";
-        if (what.href != bottomLocation.href && whatMutated.href != bottomLocation.href)
-            bottomLocation.replace(what);
+        let destUrl = new URL(dest, contentDirUrl), destUrlWithHtmlExt = new URL(dest, contentDirUrl);
+        if (!dest.includes("/") && !destUrlWithHtmlExt.pathname.endsWith(".html"))
+            destUrlWithHtmlExt.pathname += ".html";
+        if (destUrl.href != bottomLocation.href && destUrlWithHtmlExt.href != bottomLocation.href)
+            bottomLocation.replace(destUrl);
     }
 
     if (location.hash)
         changeBottomUrl();
     window.addEventListener("hashchange", changeBottomUrl);
-    contentFrame.addEventListener("load", () => {
-        if (!contentFrame.contentDocument)
+    frame.addEventListener("load", () => {
+        if (!frame.contentDocument)
             return;
         changeHash();
-        contentFrame.contentWindow.addEventListener("popstate", changeHash);
+        frame.contentWindow.addEventListener("popstate", changeHash);
     });
 })();
