c3b3f905569af43ecf55ef863cdeedada92251ab
Liu Wenyuan <15816141883@163.com>
2024-03-09 18:34:53 +0800

    Fix bugs in fake SPA

--- js/wiki_fake_spa.js
+++ js/wiki_fake_spa.js
@@ -2,45 +2,54 @@
 "use strict";
 
 (function() {
-    if (localStorage && localStorage.getItem("wiki_fake_spa_opt_out")) return;
-
     let frame = document.querySelector("main").appendChild(document.querySelector("#empty-iframe").content.querySelector("iframe"));
 
-    let contentDir = location.pathname;
-    contentDir = contentDir.substring(0, contentDir.lastIndexOf("/") + 1);
+    if (localStorage && localStorage.getItem("wiki_fake_spa_opt_out")) {
+        frame.contentWindow.location.href = "./Home";
+        return;
+    }
+
+    const serverRoot = location.protocol + "//" + location.host;
+    const contentDir = location.pathname.substring(0, location.pathname.lastIndexOf("/") + 1);
 
     function changeHash() {
         let bottomLocation = frame.contentWindow.location;
         if (bottomLocation.host != location.host || !bottomLocation.pathname.startsWith(contentDir))
             return;
+        let strippedPathname = bottomLocation.pathname.substring(contentDir.length)
+            .replace(/(?:.html)?(?:\/)?$/, "");
         // FIXME: Is this even correct?
         let relUrl = "#" + encodeURI(decodeURIComponent(
-            bottomLocation.pathname.substring(contentDir.length) + bottomLocation.search + bottomLocation.hash
+            strippedPathname + bottomLocation.search + bottomLocation.hash
         ));
+        document.title = decodeURIComponent(strippedPathname).replaceAll("-", " ") + " - DOSBox-X Wiki";
         if (location.hash != relUrl) {
             const url = new URL(location);
             url.hash = relUrl;
             history.pushState({}, "", url);
         }
-        document.title = decodeURIComponent(
-            bottomLocation.pathname.substring(contentDir.length)
-            .replace(/(?:.html)?(?:\/)?$/, "")
-        ).replaceAll("-", " ") + " - DOSBox-X Wiki";
     }
     function changeBottomUrl() {
         let bottomLocation = frame.contentWindow.location;
         let dest = contentDir + location.hash.substring(1);
-        let serverRoot = location.protocol + "//" + location.host;
         let destUrl = new URL(dest, serverRoot), destUrlWithHtmlExt = destUrl;
-        if (!dest.includes("/") && !destUrlWithHtmlExt.pathname.endsWith(".html"))
+        if (destUrl.host != location.host || !destUrl.pathname.startsWith(contentDir)) {
+            console.error(`Did not navigate to ${dest} as it escapes ${serverRoot}${contentDir}.`);
+            return false;
+        }
+        if (!dest.includes(".") && !destUrlWithHtmlExt.pathname.endsWith(".html"))
             destUrlWithHtmlExt.pathname += ".html";
-        if (destUrl.href != bottomLocation.href && destUrlWithHtmlExt.href != bottomLocation.href)
-            bottomLocation.replace(destUrl);
+        try {
+            if (destUrl.href != bottomLocation.href && destUrlWithHtmlExt.href != bottomLocation.href)
+                bottomLocation.replace(destUrl);
+        } catch (err) {
+            console.error(`Could not navigate to ${dest}.`, err);
+            return false;
+        }
+        return true;
     }
 
-    if (location.hash)
-        changeBottomUrl();
-    else
+    if (!location.hash || !changeBottomUrl())
         frame.contentWindow.location.href = "./Home";
     window.addEventListener("hashchange", changeBottomUrl);
     frame.addEventListener("load", () => {
