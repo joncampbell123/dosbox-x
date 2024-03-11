0b6e7f3147088e4353ecff99088e53daeb853330
donmor <donmor3000@hotmail.com>
2024-03-11 13:39:56 +0800

    Update version_string.h
    Revert

--- include/version_string.h
+++ include/version_string.h
@@ -57,7 +57,7 @@
 #define OS_PLATFORM_LONG ""
 #endif
 
-#if defined(_M_X64) || defined (_M_AMD64) || defined (_M_ARM64) || defined (_M_IA64) || defined(__ia64__) || defined(__LP64__) || defined(_WIN64) || defined(__x86_64__) || defined(__aarch64__) || defined(__powerpc64__) || defined(__loongarch64__)
+#if defined(_M_X64) || defined (_M_AMD64) || defined (_M_ARM64) || defined (_M_IA64) || defined(__ia64__) || defined(__LP64__) || defined(_WIN64) || defined(__x86_64__) || defined(__aarch64__) || defined(__powerpc64__)
 #define OS_BIT "64"
 #define OS_BIT_INT 64
 #else
