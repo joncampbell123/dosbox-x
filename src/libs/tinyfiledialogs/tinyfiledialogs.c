29d31300c0bc26221010145284952462c01cef20
maron2000 <68574602+maron2000@users.noreply.github.com>
2024-03-12 18:38:22 +0900

    tinyfiledialogs: Reapply patch for Haiku OS

--- src/libs/tinyfiledialogs/tinyfiledialogs.c
+++ src/libs/tinyfiledialogs/tinyfiledialogs.c
@@ -3284,6 +3284,15 @@ int tfd_isDarwin(void)
 }
 
 
+int tdf_isHaikuOS(void)
+{
+#if defined(__HAIKU__)
+    return 1;
+#else
+    return 0;
+#endif
+}
+
 static int dirExists( char const * aDirPath )
 {
 		DIR * lDir ;
@@ -3645,7 +3654,7 @@ static int whiptailPresent(void)
 static int graphicMode(void)
 {
 		return !( tinyfd_forceConsole && (isTerminalRunning() || terminalName()) )
-						&& ( getenvDISPLAY()
+						&& ( getenvDISPLAY() || tdf_isHaikuOS()
 						|| (tfd_isDarwin() && (!getenv("SSH_TTY") || getenvDISPLAY() ) ) ) ;
 }
 
