7dd5c1b1f0a595f56442427ddab9ff5a81ab8984
maxpat78 <maxpat78@yahoo.it>
2024-04-15 12:13:57 +0200

    Ehanced DetectGeometry
    - avoids duplicating imageDisk for Fixed VHDs by splitting DetectGeometry
    - removed unused code

--- include/bios_disk.h
+++ include/bios_disk.h
@@ -358,6 +358,7 @@ public:
     static uint32_t CreateDifferencing(const char* filename, const char* basename);
     uint32_t CreateSnapshot();
     void DetectGeometry(Bitu sizes[]);
+    static void DetectGeometry(uint8_t* buf, Bitu sizes[], uint64_t currentSize);
     static uint64_t scanMBR(uint8_t* mbr, Bitu sizes[], uint64_t disksize=0);
     bool MergeSnapshot(uint32_t* totalSectorsMerged, uint32_t* totalBlocksUpdated);
     static void SizeToCHS(uint64_t size, uint16_t* c, uint8_t* h, uint8_t* s);
