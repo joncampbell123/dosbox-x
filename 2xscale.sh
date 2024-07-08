50e8eb516ab9a6272fc0761f5a38a48eddda536c
Jonathan Campbell <jonathan@castus.tv>
2024-07-07 20:07:49 -0700

    more

--- 2xscale.sh
+++ 2xscale.sh
@@ -2,5 +2,5 @@
 src="${1}"
 dst="${1}.2x.mp4"
 
-ffmpeg -i "${src}" -bsf:a aac_adtstoasc -acodec copy -vcodec libx264 -g 15 -bf 2 -vf "scale=iw*2:ih*2:flags=neighbor" -vb 15000000 -preset superfast -pix_fmt yuv420p -y -f mp4 "${dst}"
+ffmpeg -i "${src}" -acodec copy -vcodec libx264 -g 15 -bf 2 -vf "scale=iw*2:ih*2:flags=neighbor" -vb 15000000 -preset superfast -g 15 -bf 2 -crf 8 -pix_fmt yuv422p -y -f mov "${dst}"
 
