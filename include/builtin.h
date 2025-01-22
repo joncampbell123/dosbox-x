c02c9902e05a2dec189b1edee98376f0c6cbf7f8
Jonathan Campbell <jonathan@castus.tv>
2025-01-22 12:19:04 -0800

    For PC-98 mode, add a patched version of CWSDPMI.EXE made for PC-98 systems

--- include/builtin.h
+++ include/builtin.h
@@ -19,6 +19,7 @@ extern char autoexec_data[AUTOEXEC_SIZE];
 extern struct BuiltinFileBlob bfb_DSXMENU_EXE_PC;		// DSXMENU.EXE
 extern struct BuiltinFileBlob bfb_DSXMENU_EXE_PC98;		// DSXMENU.EXE
 
+extern struct BuiltinFileBlob bfb_CWSDPMI_PC98_EXE;  // CWSDPMI.EXE (PC-98 version)
 extern struct BuiltinFileBlob bfb_CWSDPMI_EXE;  // CWSDPMI.EXE
 extern struct BuiltinFileBlob bfb_DOS32A_EXE;   // DOS32A.EXE
 extern struct BuiltinFileBlob bfb_DOS4GW_EXE;   // DOS4GW.EXE
