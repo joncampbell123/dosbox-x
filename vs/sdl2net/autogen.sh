34db23493270c3e78330cd1e3ad2ee718d7a1cbb
maron2000 <68574602+maron2000@users.noreply.github.com>
2025-12-20 23:22:01 +0900

    SDL2_net: Fix build scripts to build static libraries

--- vs/sdl2net/autogen.sh
+++ vs/sdl2net/autogen.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+# Initialize libtool
+if command -v libtoolize >/dev/null 2>&1; then
+    libtoolize --force --copy
+elif command -v glibtoolize >/dev/null 2>&1; then
+    glibtoolize --force --copy
+else
+    echo "libtoolize not found"
+    exit 1
+fi
+
 "${ACLOCAL:-aclocal}" -I acinclude
 "${AUTOMAKE:-automake}" --foreign --include-deps --add-missing --copy
 "${AUTOCONF:-autoconf}"
---
+++ vs/sdl2net/build-dosbox.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+rm -Rfv linux-host || exit 1
+mkdir -p linux-host || exit 1
+
+rm -Rfv linux-build || exit 1
+mkdir -p linux-build || exit 1
+
+./autogen.sh
+
+chmod +x configure || exit 1
+
+srcdir="`pwd`"
+instdir="`pwd`/linux-host"
+
+cd linux-build || exit 1
+
+opts=
+
+sys=`uname -s`
+
+sdldirspec="--with-sdl-prefix=$srcdir/../sdl2/linux-host"
+
+autoreconf -fi
+../configure "--srcdir=$srcdir" "--prefix=$instdir" --disable-examples "$sdldirspec" $opts || exit 1
+
+mkdir -p linux-host || exit 1
+mkdir -p linux-build || exit 1
+mkdir -p linux-build/build || exit 1
+mkdir -p linux-build/include || exit 1
+
+# Um, what?
+#chmod +x "$srcdir/install-sh" || exit 1
+
+# Proceed
+make -j3 || exit 1
+make install || exit 1  # will install into ./linux-host
