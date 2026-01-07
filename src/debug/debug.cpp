b094d97aee05af6e942face42578d25dac6c28b7
Jonathan Campbell <jonathan@castus.tv>
2026-01-06 17:32:43 -0800

    Change LOGL to print PIC_FullIndex() to allow debugging timing issues

--- src/debug/debug.cpp
+++ src/debug/debug.cpp
@@ -5417,20 +5417,20 @@ static void LogInstruction(uint16_t segValue, uint32_t eipValue,  ofstream& out)
 	char dline[200];Bitu size;
 	size = DasmI386(dline, start, reg_eip, cpu.code.big);
 	char* res = empty;
-	if (showExtend && (cpuLogType > 0) ) {
+	if (showExtend && (cpuLogType > 0)) {
 		res = AnalyzeInstruction(dline,false);
 		if (!res || !(*res)) res = empty;
 		Bitu reslen = strlen(res);
-        if (reslen < 22) {
-            memset(res + reslen, ' ', 22 - reslen);
-            res[22] = 0;
-        }
+		if (reslen < 22) {
+			memset(res + reslen, ' ', 22 - reslen);
+			res[22] = 0;
+		}
 	}
 	Bitu len = strlen(dline);
-    if (len < 30) {
-        memset(dline + len, ' ', 30 - len);
-        dline[30] = 0;
-    }
+	if (len < 30) {
+		memset(dline + len, ' ', 30 - len);
+		dline[30] = 0;
+	}
 
 	// Get register values
 
@@ -5439,7 +5439,9 @@ static void LogInstruction(uint16_t segValue, uint32_t eipValue,  ofstream& out)
 	} else if (cpuLogType == 1) {
 		out << setw(4) << SegValue(cs) << ":" << setw(8) << reg_eip << "  " << dline << "  " << res;
 	} else if (cpuLogType == 2) {
-		char ibytes[200]="";	char tmpc[200];
+		char ibytes[200]="";
+		char tstamp[128];
+		char tmpc[200];
 		for (Bitu i=0; i<size; i++) {
 			uint8_t value;
 			if (mem_readb_checked((PhysPt)(start+i),&value)) sprintf(tmpc,"%s","?? ");
@@ -5447,32 +5449,33 @@ static void LogInstruction(uint16_t segValue, uint32_t eipValue,  ofstream& out)
 			strcat(ibytes,tmpc);
 		}
 		len = strlen(ibytes);
-        if (len < 21) {
-            for (Bitu i = 0; i < 21 - len; i++) ibytes[len + i] = ' ';
-            ibytes[21] = 0;
-        }
-		out << setw(4) << SegValue(cs) << ":" << setw(8) << reg_eip << "  " << dline << "  " << res << "  " << ibytes;
+		if (len < 21) {
+			for (Bitu i = 0; i < 21 - len; i++) ibytes[len + i] = ' ';
+			ibytes[21] = 0;
+		}
+		sprintf(tstamp,"%.6f",(double)PIC_FullIndex());
+		out << tstamp << " " << setw(4) << SegValue(cs) << ":" << setw(8) << reg_eip << "  " << dline << "  " << res << "  " << ibytes;
 	}
 
 	out << " EAX:" << setw(8) << reg_eax << " EBX:" << setw(8) << reg_ebx
-	    << " ECX:" << setw(8) << reg_ecx << " EDX:" << setw(8) << reg_edx
-	    << " ESI:" << setw(8) << reg_esi << " EDI:" << setw(8) << reg_edi
-	    << " EBP:" << setw(8) << reg_ebp << " ESP:" << setw(8) << reg_esp
-	    << " DS:"  << setw(4) << SegValue(ds)<< " ES:"  << setw(4) << SegValue(es);
+		<< " ECX:" << setw(8) << reg_ecx << " EDX:" << setw(8) << reg_edx
+		<< " ESI:" << setw(8) << reg_esi << " EDI:" << setw(8) << reg_edi
+		<< " EBP:" << setw(8) << reg_ebp << " ESP:" << setw(8) << reg_esp
+		<< " DS:"  << setw(4) << SegValue(ds)<< " ES:"  << setw(4) << SegValue(es);
 
 	if(cpuLogType == 0) {
 		out << " SS:"  << setw(4) << SegValue(ss) << " C"  << (get_CF()>0)  << " Z"   << (get_ZF()>0)
-		    << " S" << (get_SF()>0) << " O"  << (get_OF()>0) << " I"  << GETFLAGBOOL(IF);
+			<< " S" << (get_SF()>0) << " O"  << (get_OF()>0) << " I"  << GETFLAGBOOL(IF);
 	} else {
 		out << " FS:"  << setw(4) << SegValue(fs) << " GS:"  << setw(4) << SegValue(gs)
-		    << " SS:"  << setw(4) << SegValue(ss)
-		    << " CF:"  << (get_CF()>0)  << " ZF:"   << (get_ZF()>0)  << " SF:"  << (get_SF()>0)
-		    << " OF:"  << (get_OF()>0)  << " AF:"   << (get_AF()>0)  << " PF:"  << (get_PF()>0)
-		    << " IF:"  << GETFLAGBOOL(IF);
+			<< " SS:"  << setw(4) << SegValue(ss)
+			<< " CF:"  << (get_CF()>0)  << " ZF:"   << (get_ZF()>0)  << " SF:"  << (get_SF()>0)
+			<< " OF:"  << (get_OF()>0)  << " AF:"   << (get_AF()>0)  << " PF:"  << (get_PF()>0)
+			<< " IF:"  << GETFLAGBOOL(IF);
 	}
 	if(cpuLogType == 2) {
 		out << " TF:" << GETFLAGBOOL(TF) << " VM:" << GETFLAGBOOL(VM) <<" FLG:" << setw(8) << reg_flags
-		    << " CR0:" << setw(8) << cpu.cr0;
+			<< " CR0:" << setw(8) << cpu.cr0;
 	}
 	out << endl;
 }
