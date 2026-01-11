1eea2c3bb5a6d28f19cc484e54ab704da600f205
1abcd <bb0738@gmail.com>
2026-01-11 10:43:05 +0800

    Use more ChinaSea extended characters
    Use more ChinaSea/Big5-2003 extended characters for code page 951 when ChinaSea is set to true.

--- include/cp951_uni.h
+++ include/cp951_uni.h
@@ -6859,11 +6859,12 @@ void makeseacp951table() {
         cp951sea_to_unicode_raw[i] = cp950ext_to_unicode_raw[i];
         cp951uaosea_to_unicode_raw[i] = cp950ext_to_unicode_raw[i];
     }
-    for (int i=64*98; i<64*365; i++) {
+    for (int i=64*98; i<64*210; i++) {
         cp951sea_to_unicode_raw[i] = cp951_to_unicode_raw[i];
         cp951uaosea_to_unicode_raw[i] = cp951uao_to_unicode_raw[i];
     }
-    for (int i=64*365; i<64*380; i++) {
+    for (int i=64*210; i<64*380; i++) {
+        /* 0xC6A1-0xFEFE */
         cp951sea_to_unicode_raw[i] = cp950ext_to_unicode_raw[i];
         cp951uaosea_to_unicode_raw[i] = cp950ext_to_unicode_raw[i];
     }
