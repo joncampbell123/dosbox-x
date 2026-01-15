b0a37cabcc2602c6453704bbfccaaa67f3850df6
Jochen Schfer <josch1710@live.de>
2026-01-15 10:10:29 +0100

    Deleted build scripts

--- build-os2-sdl2.cmd
+++
@@ -1,9 +0,0 @@
-echo off
-bash autogen.sh
-dash configure --enable-sdl2 --disable-gamelink
-REM since cpu cores need a lot of memory,
-REM we don't want to build parallel
-cd src\cpu
-make
-cd ..\..
-make -j5
--- build-os2.cmd
+++
@@ -1,9 +0,0 @@
-echo off
-bash autogen.sh
-dash configure
-REM since cpu cores need a lot of memory,
-REM we don't want to build parallel
-cd src\cpu
-make
-cd ..\..
-make -j4
