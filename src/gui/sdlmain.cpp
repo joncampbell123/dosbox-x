5ed3b45793dbf2a5bd47b7090bb1f23da4d5c5c5
maron2000 <68574602+maron2000@users.noreply.github.com>
2026-01-19 22:12:25 +0900

    D3D11: Clean-up some codes

--- src/gui/sdlmain.cpp
+++ src/gui/sdlmain.cpp
@@ -2121,7 +2121,7 @@ Bitu GFX_SetSize(Bitu width, Bitu height, Bitu flags, double scalex, double scal
 {
 
 #if C_DIRECT3D && defined(C_SDL2)
-    if(sdl.desktop.type == SCREEN_DIRECT3D11) sdl.desktop.want_type = SCREEN_DIRECT3D11;
+    //if(sdl.desktop.type == SCREEN_DIRECT3D11) sdl.desktop.want_type = SCREEN_DIRECT3D11;
 #endif
 
     if ((width == 0 || height == 0) && !TTF_using()) {
