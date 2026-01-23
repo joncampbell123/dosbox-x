988eff7048f0b2368cb5baf4133a262dddc23705
Allofich <19624336+Allofich@users.noreply.github.com>
2026-01-23 20:54:54 +0900

    Revert e272c6b and 3454420

--- src/dos/drive_fat.cpp
+++ src/dos/drive_fat.cpp
@@ -63,7 +63,6 @@ extern char * DBCS_upcase(char * str), sfn[DOS_NAMELENGTH_ASCII];
 extern bool gbk, isDBCSCP(), isKanji1_gbk(uint8_t chr), shiftjis_lead_byte(int c);
 extern bool CodePageGuestToHostUTF16(uint16_t *d/*CROSS_LEN*/,const char *s/*CROSS_LEN*/);
 extern bool CodePageHostToGuestUTF16(char *d/*CROSS_LEN*/,const uint16_t *s/*CROSS_LEN*/);
-extern bool wild_match(const char* haystack, char* needle);
 bool systemmessagebox(char const * aTitle, char const * aMessage, char const * aDialogType, char const * aIconType, int aDefaultButton);
 extern bool dos_kernel_disabled;
 std::string formatString(const char* format, ...);
@@ -2900,18 +2899,11 @@ nextfile:
 	}
 	memset(find_name,0,DOS_NAMELENGTH_ASCII);
 	memset(extension,0,4);
-
-	if (sectbuf[entryoffset].attrib & DOS_ATTR_VOLUME)
-		memcpy(find_name, &sectbuf[entryoffset].entryname[0], 11);
-	else
-	{
-		memcpy(find_name, &sectbuf[entryoffset].entryname[0], 8);
-		memcpy(extension, &sectbuf[entryoffset].entryname[8], 3);
-	}
-
+	memcpy(find_name,&sectbuf[entryoffset].entryname[0],8);
 	// recover the SFN initial E5, which was converted to 05
 	// to distinguish with a free directory entry
 	if (find_name[0] == 0x05) find_name[0] = 0xe5;
+	memcpy(extension,&sectbuf[entryoffset].entryname[8],3);
 
 	if (!(sectbuf[entryoffset].attrib & DOS_ATTR_VOLUME)) {
 		trimString(&find_name[0]);
@@ -2919,9 +2911,14 @@ nextfile:
 	}
 
 	if (extension[0]!=0) {
-		strcat(find_name, ".");
+		if (!(sectbuf[entryoffset].attrib & DOS_ATTR_VOLUME)) {
+			strcat(find_name, ".");
+		}
 		strcat(find_name, extension);
 	}
+	
+	if (sectbuf[entryoffset].attrib & DOS_ATTR_VOLUME)
+        trimString(find_name);
 
 	/* Compare attributes to search attributes */
 
@@ -3059,19 +3056,12 @@ nextfile:
 	}
 
 	/* Compare name to search pattern. Skip long filename match if no long filename given. */
-	if(attrs == DOS_ATTR_VOLUME) {
-		if (!(wild_match(find_name, srch_pattern)))
-			goto nextfile;
-	}
-	else if (!(WildFileCmp(find_name,srch_pattern) || (lfn_max_ord != 0 && lfind_name[0] != 0 && LWildFileCmp(lfind_name,srch_pattern)))) {
+	if (!(WildFileCmp(find_name,srch_pattern) || (lfn_max_ord != 0 && lfind_name[0] != 0 && LWildFileCmp(lfind_name,srch_pattern)))) {
 		lfind_name[0] = 0; /* LFN code will memset() it in full upon next dirent */
 		lfn_max_ord = 0;
 		lfnRange.clear();
 		goto nextfile;
 	}
-
-	if(sectbuf[entryoffset].attrib == DOS_ATTR_VOLUME)
-		trimString(find_name);
 
 	// Drive emulation does not need to require a LFN in case there is no corresponding 8.3 names.
 	if (lfind_name[0] == 0) strcpy(lfind_name,find_name);
