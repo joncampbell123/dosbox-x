cefbf5c14cdb9f4d33466bee2e203cfc6563d3f3
1abcd <bb0738@gmail.com>
2026-02-08 15:00:42 +0800

    Adjust ChinaSea/Big5-2003 extended ranges

--- include/cp951_uni.h
+++ include/cp951_uni.h
@@ -6851,15 +6851,16 @@ bool madecp951 = false;
 uint16_t cp951sea_to_unicode_raw[24320], cp951uaosea_to_unicode_raw[24320];
 void makeseacp951table() {
     if (madecp951) return;
-    for (int i=0; i<64*41; i++) {
+    for (int i=0; i<64*50; i++) {
         cp951sea_to_unicode_raw[i] = cp951_to_unicode_raw[i];
         cp951uaosea_to_unicode_raw[i] = cp951uao_to_unicode_raw[i];
     }
-    for (int i=64*41; i<64*98; i++) {
+    for (int i=64*50; i<64*107; i++) {
+        /* 0x9140-0xA3FE */
         cp951sea_to_unicode_raw[i] = cp950ext_to_unicode_raw[i];
         cp951uaosea_to_unicode_raw[i] = cp950ext_to_unicode_raw[i];
     }
-    for (int i=64*98; i<64*210; i++) {
+    for (int i=64*107; i<64*210; i++) {
         cp951sea_to_unicode_raw[i] = cp951_to_unicode_raw[i];
         cp951uaosea_to_unicode_raw[i] = cp951uao_to_unicode_raw[i];
     }
