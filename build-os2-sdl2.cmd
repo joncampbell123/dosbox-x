b3748c645f631d450834943597647d8777b8bfd5
Jochen Schfer <josch1710@live.de>
2026-02-10 16:06:37 +0100

    Added OS/2 build scripts

---
+++ build-os2-sdl2.cmd
@@ -0,0 +1,10 @@
+echo off
+cd dosbox-x
+bash autogen.sh
+dash configure --enable-sdl2 --disable-gamelink
+REM since cpu cores need a lot of memory,
+REM we don't want to build parallel
+cd src\cpu
+make
+cd ..\..
+make -j4
