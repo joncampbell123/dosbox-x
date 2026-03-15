4ec1ab5c4d751b3941e18a2abbf7b0a421f1bd99
Jonathan Campbell <jonathan@castus.tv>
2026-03-15 11:48:41 -0700

    EXEPACK decompression

--- src/dos/exepackv1.h
+++ src/dos/exepackv1.h
@@ -26,3 +26,20 @@ static const unsigned char EXEPACKv1[] = {
   0x63, 0x6f, 0x72, 0x72, 0x75, 0x70, 0x74
 };
 static const unsigned int EXEPACKv1_len = 283;
+
+/* EXEPACK variables [https://moddingwiki.shikadi.net/wiki/Microsoft_EXEPACK#Games_using_EXEPACK] */
+#pragma pack(push,1)
+typedef struct EXEPACKVARSv1 {
+	uint16_t	real_IP;
+	uint16_t	real_CS;
+	uint16_t	mem_start;
+	uint16_t	exepack_size;
+	uint16_t	real_SP;
+	uint16_t	real_SS;
+	uint16_t	dest_len;
+	uint16_t	skip_len;
+	uint16_t	signature; /* "RB" */
+};
+static_assert( sizeof(EXEPACKVARSv1) == 0x12, "oops" );
+#pragma pack(pop)
+
