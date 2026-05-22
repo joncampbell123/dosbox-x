267218d9cd83ca15e24b507e862c816b8081e45f
Jonathan Campbell <jonathan@castus.tv>
2026-05-21 23:16:40 -0700

    Apparently the reason OS/2 Warp was not acceptable, was a bug fix in 2025 that attempted to emulate a wraparound quirk for 16-bit LDS/LES instructions, however that fix was not quite done correctly. Correct that emulation. OS/2 Warp boots now

--- src/cpu/core_normal/table_ea_8086.h
+++ src/cpu/core_normal/table_ea_8086.h
@@ -17,32 +17,32 @@
  */
 
 /* The MOD/RM Decoder for EA for this decoder's addressing modes */
-static PhysPt EA86_16_00_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_bx+(int16_t)reg_si))); }
-static PhysPt EA86_16_01_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_bx+(int16_t)reg_di))); }
-static PhysPt EA86_16_02_n(void) { return BaseSS+(last_ea86_offset=((uint16_t)(reg_bp+(int16_t)reg_si))); }
-static PhysPt EA86_16_03_n(void) { return BaseSS+(last_ea86_offset=((uint16_t)(reg_bp+(int16_t)reg_di))); }
-static PhysPt EA86_16_04_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_si))); }
-static PhysPt EA86_16_05_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_di))); }
-static PhysPt EA86_16_06_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(Fetchw())));}
-static PhysPt EA86_16_07_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_bx))); }
+static PhysPt EA86_16_00_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_bx+reg_si)); }
+static PhysPt EA86_16_01_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_bx+reg_di)); }
+static PhysPt EA86_16_02_n(void) { return BaseSS+(last_ea86_offset=(uint16_t)(reg_bp+reg_si)); }
+static PhysPt EA86_16_03_n(void) { return BaseSS+(last_ea86_offset=(uint16_t)(reg_bp+reg_di)); }
+static PhysPt EA86_16_04_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_si)); }
+static PhysPt EA86_16_05_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_di)); }
+static PhysPt EA86_16_06_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(Fetchw()));}
+static PhysPt EA86_16_07_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_bx)); }
 
-static PhysPt EA86_16_40_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_bx+(int16_t)reg_si+Fetchbs()))); }
-static PhysPt EA86_16_41_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_bx+(int16_t)reg_di+Fetchbs()))); }
-static PhysPt EA86_16_42_n(void) { return BaseSS+(last_ea86_offset=((uint16_t)(reg_bp+(int16_t)reg_si+Fetchbs()))); }
-static PhysPt EA86_16_43_n(void) { return BaseSS+(last_ea86_offset=((uint16_t)(reg_bp+(int16_t)reg_di+Fetchbs()))); }
-static PhysPt EA86_16_44_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_si+Fetchbs()))); }
-static PhysPt EA86_16_45_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_di+Fetchbs()))); }
-static PhysPt EA86_16_46_n(void) { return BaseSS+(last_ea86_offset=((uint16_t)(reg_bp+Fetchbs()))); }
-static PhysPt EA86_16_47_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_bx+Fetchbs()))); }
+static PhysPt EA86_16_40_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_bx+reg_si+(uint16_t)Fetchbs())); }
+static PhysPt EA86_16_41_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_bx+reg_di+(uint16_t)Fetchbs())); }
+static PhysPt EA86_16_42_n(void) { return BaseSS+(last_ea86_offset=(uint16_t)(reg_bp+reg_si+(uint16_t)Fetchbs())); }
+static PhysPt EA86_16_43_n(void) { return BaseSS+(last_ea86_offset=(uint16_t)(reg_bp+reg_di+(uint16_t)Fetchbs())); }
+static PhysPt EA86_16_44_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_si+(uint16_t)Fetchbs())); }
+static PhysPt EA86_16_45_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_di+(uint16_t)Fetchbs())); }
+static PhysPt EA86_16_46_n(void) { return BaseSS+(last_ea86_offset=(uint16_t)(reg_bp+(uint16_t)Fetchbs())); }
+static PhysPt EA86_16_47_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_bx+(uint16_t)Fetchbs())); }
 
-static PhysPt EA86_16_80_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_bx+(int16_t)reg_si+Fetchws()))); }
-static PhysPt EA86_16_81_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_bx+(int16_t)reg_di+Fetchws()))); }
-static PhysPt EA86_16_82_n(void) { return BaseSS+(last_ea86_offset=((uint16_t)(reg_bp+(int16_t)reg_si+Fetchws()))); }
-static PhysPt EA86_16_83_n(void) { return BaseSS+(last_ea86_offset=((uint16_t)(reg_bp+(int16_t)reg_di+Fetchws()))); }
-static PhysPt EA86_16_84_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_si+Fetchws()))); }
-static PhysPt EA86_16_85_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_di+Fetchws()))); }
-static PhysPt EA86_16_86_n(void) { return BaseSS+(last_ea86_offset=((uint16_t)(reg_bp+Fetchws()))); }
-static PhysPt EA86_16_87_n(void) { return BaseDS+(last_ea86_offset=((uint16_t)(reg_bx+Fetchws()))); }
+static PhysPt EA86_16_80_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_bx+reg_si+(uint16_t)Fetchws())); }
+static PhysPt EA86_16_81_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_bx+reg_di+(uint16_t)Fetchws())); }
+static PhysPt EA86_16_82_n(void) { return BaseSS+(last_ea86_offset=(uint16_t)(reg_bp+reg_si+(uint16_t)Fetchws())); }
+static PhysPt EA86_16_83_n(void) { return BaseSS+(last_ea86_offset=(uint16_t)(reg_bp+reg_di+(uint16_t)Fetchws())); }
+static PhysPt EA86_16_84_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_si+(uint16_t)Fetchws())); }
+static PhysPt EA86_16_85_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_di+(uint16_t)Fetchws())); }
+static PhysPt EA86_16_86_n(void) { return BaseSS+(last_ea86_offset=(uint16_t)(reg_bp+(uint16_t)Fetchws())); }
+static PhysPt EA86_16_87_n(void) { return BaseDS+(last_ea86_offset=(uint16_t)(reg_bx+(uint16_t)Fetchws())); }
 
 static GetEAHandler EATable8086[512]={
 /* 00 */
@@ -108,7 +108,7 @@ static GetEAHandler EATable8086[512]={
 	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
 	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
 	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
-/* 11 These are illegal so make em 0 */
+/* 11 These are illegal so make em nullptr */
 	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
 	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
 	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
@@ -120,8 +120,8 @@ static GetEAHandler EATable8086[512]={
 };
 
 #if CPU_CORE <= CPU_ARCHTYPE_8086
-#define GetEADirect(sz)							\
-	PhysPt eaa;								\
-	eaa=BaseDS+Fetchw();
+#define GetEADirect(sz)						\
+	PhysPt eaa;						\
+	eaa=Fetchw();						\
+	eaa += BaseDS;
 #endif
-
