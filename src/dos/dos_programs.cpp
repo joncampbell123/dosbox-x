bd6c9f53acd49b72567355af874e186aa32e4ded
Jonathan Campbell <jonathan@castus.tv>
2026-06-07 16:03:46 -0700

    IMGMOUNT: use tdr and not temp_line for fstype auto and "*" drive

--- src/dos/dos_programs.cpp
+++ src/dos/dos_programs.cpp
@@ -5953,7 +5953,7 @@ class IMGMOUNT : public Program {
 			cmd->FindString("-chs", str_chs, true);
 			if (!ReadSizeParameter(str_size, str_chs, type, sizes)) return;
 
-			if (!rfstype&&isdigit(temp_line[0])) fstype="none";
+			if (!rfstype&&isdigit(tdr)) fstype="none";
 
 			//for floppies, hard drives, and cdroms, require a drive letter
 			//for -fs none, require a number indicating where to mount at
@@ -5965,7 +5965,7 @@ class IMGMOUNT : public Program {
 				}
 
 				/* if fs type if "fat" and we're asked to mount *: then check for that here */
-				if (tdr == '*' && temp_line[1] == ':') {
+				if (tdr == '*') {
 					/* What drives are available? */
 					int i_drive = IS_PC98_ARCH ? 'A' : 'C';
 					while (i_drive <= 'Z' && Drives[i_drive-'A'] != NULL && (i_drive-'A') < DOS_DRIVES) i_drive++;
